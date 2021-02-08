18806a2259c1a94002db748ec04b40f84136019a
Daniel Izdebski <spidiritsu@gmail.com>
2021-02-08 14:49:47 +0100

    Use unnamed returns

--- api/send_transaction.go
+++ api/send_transaction.go
@@ -18,20 +18,21 @@ type IncomingTransaction struct {
 	Signature []byte
 }
 
-func (a *Api) SendTransaction(tx IncomingTransaction) (h common.Hash, err error) {
+func (a *Api) SendTransaction(tx IncomingTransaction) (common.Hash, error) {
 	fmt.Printf("%+v\n", tx)
-	h, err = rlpHash(tx)
+	h, err := rlpHash(tx)
 	if err != nil {
-		return
+		return common.Hash{}, err
 	}
 	return h, nil
 }
 
-func rlpHash(x interface{}) (h common.Hash, err error) {
+func rlpHash(x interface{}) (common.Hash, error) {
 	hw := sha3.NewLegacyKeccak256()
-	if err = rlp.Encode(hw, x); err != nil {
-		return
+	if err := rlp.Encode(hw, x); err != nil {
+		return common.Hash{}, err
 	}
+	h := common.Hash{}
 	hw.Sum(h[:0])
 	return h, nil
 }
