039efff8d3f7a4b00b0dde9974bfb8dd155f8852
Daniel Izdebski <spidiritsu@gmail.com>
2021-02-11 13:05:50 +0100

    Update db package to handle new config

--- db/db_test.go
+++ db/db_test.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"os"
 	"testing"
 
 	sq "github.com/Masterminds/squirrel"
@@ -9,32 +8,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMain(m *testing.M) {
-	if os.Getenv("HUBBLE_VERSION") == "" {
-		os.Setenv("HUBBLE_VERSION", "dev-0.1.0")
-	}
-
-	if os.Getenv("HUBBLE_PORT") == "" {
-		os.Setenv("HUBBLE_PORT", "8080")
-	}
-	
-	if os.Getenv("HUBBLE_DBNAME") == "" {
-		os.Setenv("HUBBLE_DBNAME", "hubble_test")
-	}
-
-	if os.Getenv("HUBBLE_DBUSER") == "" {
-		os.Setenv("HUBBLE_DBUSER", "hubble")
-	}
-	
-	if os.Getenv("HUBBLE_DBPASSWORD") == "" {
-		os.Setenv("HUBBLE_DBPASSWORD", "root")
-	}
-	
-	os.Exit(m.Run())
-}
-
 func TestGetDB(t *testing.T) {
-	cfg := config.GetConfig()
+	cfg := config.GetTestConfig()
 	db, err := GetTestDB(cfg)
 	assert.NoError(t, err)
 	defer db.Close()
@@ -43,7 +18,7 @@ func TestGetDB(t *testing.T) {
 }
 
 func TestMigrations(t *testing.T) {
-	cfg := config.GetConfig()
+	cfg := config.GetTestConfig()
 	db, err := GetTestDB(cfg)
 	assert.NoError(t, err)
 	defer db.Close()
