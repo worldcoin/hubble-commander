928323b46663d1b33e7f9a03f24b09a6e1e0c4e4
Michał Sieczkowski <michal@ethworks.io>
2021-02-16 13:05:47 +0100

    Use require instead of assert in tests to get the fail-fast behaviour

--- config/config_test.go
+++ config/config_test.go
@@ -5,12 +5,12 @@ import (
 	"testing"
 
 	"github.com/Worldcoin/hubble-commander/utils"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetEnvOrDefault(t *testing.T) {
-	os.Setenv("HUBBLE_FOO", "foo")
+	_ = os.Setenv("HUBBLE_FOO", "foo")
 
-	assert.Equal(t, "foo", *getEnvOrDefault("HUBBLE_FOO", utils.String("bar")))
-	assert.Equal(t, "bar", *getEnvOrDefault("HUBBLE_BAR", utils.String("bar")))
+	require.Equal(t, "foo", *getEnvOrDefault("HUBBLE_FOO", utils.String("bar")))
+	require.Equal(t, "bar", *getEnvOrDefault("HUBBLE_BAR", utils.String("bar")))
 }
--- db/db_test.go
+++ db/db_test.go
@@ -5,36 +5,36 @@ import (
 
 	sq "github.com/Masterminds/squirrel"
 	"github.com/Worldcoin/hubble-commander/config"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetDB(t *testing.T) {
 	cfg := config.GetTestConfig()
 	db, err := GetTestDB(&cfg)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	defer db.Close()
-	assert.NoError(t, db.Ping())
+	require.NoError(t, db.Ping())
 }
 
 func TestMigrations(t *testing.T) {
 	cfg := config.GetTestConfig()
 	db, err := GetTestDB(&cfg)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	defer db.Close()
 
 	migrator, err := GetMigrator(&cfg)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 
-	assert.NoError(t, migrator.Up())
-
-	_, err = sq.Select("*").From("person").
-		RunWith(db).Query()
-	assert.NoError(t, err)
-
-	assert.NoError(t, migrator.Down())
+	require.NoError(t, migrator.Up())
 
 	_, err = sq.Select("*").From("person").
 		RunWith(db).Query()
-	assert.Error(t, err)
+	require.NoError(t, err)
+
+	require.NoError(t, migrator.Down())
+
+	_, err = sq.Select("*").From("person").
+		RunWith(db).Query()
+	require.Error(t, err)
 
 }
