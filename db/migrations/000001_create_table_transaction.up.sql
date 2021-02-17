42ae433e6c10be34bb0ebe0c3ddfe58d5e40b04e
Michał Sieczkowski <michal@ethworks.io>
2021-02-17 12:48:48 +0100

    Regenerate migration files using golang-migrate cli v4

---
+++ db/migrations/000001_create_table_transaction.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "transaction" (
+    txHash    bytea,
+    fromIndex NUMERIC(78),
+    toIndex   NUMERIC(78),
+    amount    NUMERIC(78),
+    fee       NUMERIC(78),
+    nonce     NUMERIC(78),
+    signature bytea
+);
--- db/migrations/00_create_table_transaction.up.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "transaction" (
-	txHash bytea,
-	fromIndex NUMERIC(78),
-	toIndex NUMERIC(78),
-	amount NUMERIC(78),
-	fee NUMERIC(78),
-	nonce NUMERIC(78),
-	signature bytea
-);
