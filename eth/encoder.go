87f3cb7267ce82682186942903686949100e1219
Michał Sieczkowski <michal@ethworks.io>
2021-02-18 16:43:55 +0100

    Create transaction encoder (#11)

---
+++ eth/encoder.go
@@ -0,0 +1,41 @@
+package eth
+
+import (
+	"log"
+
+	"github.com/Worldcoin/hubble-commander/contracts/transfer"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+)
+
+var tUint256 abi.Type
+
+func init() {
+	t, err := abi.NewType("uint256", "", nil)
+	if err != nil {
+		log.Fatal("Failed to create tUint256 constant")
+	}
+	tUint256 = t
+}
+
+func EncodeTransfer(tx transfer.OffchainTransfer) ([]uint8, error) {
+	arguments := abi.Arguments{
+		{Name: "txType", Type: tUint256},
+		{Name: "fromIndex", Type: tUint256},
+		{Name: "toIndex", Type: tUint256},
+		{Name: "amount", Type: tUint256},
+		{Name: "fee", Type: tUint256},
+		{Name: "nonce", Type: tUint256},
+	}
+	encodedBytes, err := arguments.Pack(
+		tx.TxType,
+		tx.FromIndex,
+		tx.ToIndex,
+		tx.Amount,
+		tx.Fee,
+		tx.Nonce,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return encodedBytes, nil
+}
---
+++ eth/encoder_test.go
@@ -0,0 +1,74 @@
+package eth
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/Worldcoin/hubble-commander/contracts/transfer"
+	"github.com/Worldcoin/hubble-commander/testutils"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type EncoderTestSuite struct {
+	*require.Assertions
+	suite.Suite
+	sim      *testutils.Simulator
+	contract *transfer.Transfer
+}
+
+func (s *EncoderTestSuite) SetupSuite() {
+	s.Assertions = require.New(s.T())
+}
+
+func (s *EncoderTestSuite) SetupTest() {
+	sim, err := testutils.NewSimulator()
+	s.NoError(err)
+	s.sim = sim
+
+	_, _, contract, err := transfer.DeployTransfer(sim.Account, sim.Backend)
+	s.NoError(err)
+	sim.Backend.Commit()
+	s.contract = contract
+}
+
+func (s *EncoderTestSuite) TearDownTest() {
+	s.sim.Close()
+}
+
+func (s *EncoderTestSuite) TestEncodeTransferZero() {
+	tx := transfer.OffchainTransfer{
+		TxType:    big.NewInt(0),
+		FromIndex: big.NewInt(0),
+		ToIndex:   big.NewInt(0),
+		Amount:    big.NewInt(0),
+		Fee:       big.NewInt(0),
+		Nonce:     big.NewInt(0),
+	}
+	bytes, err := EncodeTransfer(tx)
+	s.NoError(err)
+	expected, err := s.contract.Encode(&bind.CallOpts{Pending: false}, tx)
+	s.NoError(err)
+	s.Equal(expected, bytes)
+}
+
+func (s *EncoderTestSuite) TestEncodeTransferNonZero() {
+	tx := transfer.OffchainTransfer{
+		TxType:    big.NewInt(1),
+		FromIndex: big.NewInt(2),
+		ToIndex:   big.NewInt(3),
+		Amount:    big.NewInt(4),
+		Fee:       big.NewInt(5),
+		Nonce:     big.NewInt(6),
+	}
+	bytes, err := EncodeTransfer(tx)
+	s.NoError(err)
+	expected, err := s.contract.Encode(&bind.CallOpts{Pending: false}, tx)
+	s.NoError(err)
+	s.Equal(expected, bytes)
+}
+
+func TestEncoderTestSuite(t *testing.T) {
+	suite.Run(t, new(EncoderTestSuite))
+}
