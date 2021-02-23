3963f2e1980f1a214550c2784d8ac677917d1324
Michał Sieczkowski <michal@ethworks.io>
2021-02-23 15:27:39 +0100

    Create Bytes32 model type

---
+++ models/Byte32.go
@@ -0,0 +1,23 @@
+package models
+
+import (
+	"encoding/hex"
+	"errors"
+)
+
+type Bytes32 struct {
+	Bytes [32]byte
+}
+
+func MakeBytes32(hexString string) (Bytes32, error) {
+	var bytes [32]byte
+	bytesSlice, err := hex.DecodeString(hexString)
+	if err != nil {
+		return Bytes32{}, err
+	}
+	if len(bytesSlice) != 32 {
+		return Bytes32{}, errors.New("invalid hex string length")
+	}
+	copy(bytes[:], bytesSlice)
+	return Bytes32{Bytes: bytes}, nil
+}
