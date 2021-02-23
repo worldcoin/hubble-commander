f5d7ca90495a2f6159a59b0a8544e78ae116d78a
Dmytro Maretskyi <maretskii@gmail.com>
2021-02-23 12:05:07 +0100

    Use uint256 in data models

--- models/transaction.go
+++ models/transaction.go
@@ -1,18 +1,16 @@
 package models
 
 import (
-	"math/big"
-
 	"github.com/ethereum/go-ethereum/common"
 )
 
 type Transaction struct {
-	Hash      common.Hash
-	FromIndex *big.Int
-	ToIndex   *big.Int
-	Amount    *big.Int
-	Fee       *big.Int
-	Nonce     *big.Int
+	Hash      common.Hash `db:"tx_hash"`
+	FromIndex Uint256     `db:"from_index"`
+	ToIndex   Uint256     `db:"to_index"`
+	Amount    Uint256
+	Fee       Uint256
+	Nonce     Uint256
 	// TODO: Right now decoder expects a base64 string here, we could define a custom type with interface implementation to expect a hex string
 	Signature []byte
 }
---
+++ models/uint256.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"database/sql/driver"
+	"fmt"
+	"math/big"
+)
+
+type Uint256 struct {
+	big.Int
+}
+
+func MakeUint256(value int64) Uint256 {
+	return Uint256{*big.NewInt(value)}
+}
+
+func MakeUint256FromBig(value big.Int) Uint256 {
+	return Uint256{value}
+}
+
+// Scan implements Scanner for database/sql.
+func (a *Uint256) Scan(src interface{}) error {
+	value, ok := src.([]uint8)
+	if !ok {
+		return fmt.Errorf("can't scan %T into Uint256", src)
+	}
+
+	a.SetString(string(value), 10)
+	return nil
+}
+
+// Value implements valuer for database/sql.
+func (a Uint256) Value() (driver.Value, error) {
+	return a.Text(10), nil
+}
