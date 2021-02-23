31b680d5dfef78dade1c92d0a04e0ee95e9ef303
Dmytro Maretskyi <maretskii@gmail.com>
2021-02-23 11:24:09 +0100

    Kill other db connections during test run

--- db/recreate_database.go
+++ db/recreate_database.go
@@ -14,14 +14,17 @@ func recreateDatabase(cfg *config.Config) error {
 		return err
 	}
 
-	query := fmt.Sprintf("DROP DATABASE IF EXISTS %s", cfg.DBName)
-	_, err = dbInstance.Exec(query)
+	_, err = dbInstance.Exec(fmt.Sprintf("SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = '%s' AND pid <> pg_backend_pid();", cfg.DBName))
 	if err != nil {
 		return err
 	}
 
-	query = fmt.Sprintf("CREATE DATABASE %s", cfg.DBName)
-	_, err = dbInstance.Exec(query)
+	_, err = dbInstance.Exec(fmt.Sprintf("DROP DATABASE IF EXISTS %s", cfg.DBName))
+	if err != nil {
+		return err
+	}
+
+	_, err = dbInstance.Exec(fmt.Sprintf("CREATE DATABASE %s", cfg.DBName))
 	if err != nil {
 		return err
 	}
