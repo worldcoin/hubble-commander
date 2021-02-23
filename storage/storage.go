61014ad77cbdf0aa9260e82974151648c060ff8c
Dmytro Maretskyi <maretskii@gmail.com>
2021-02-23 15:09:30 +0100

    Move transaction storage methods

--- storage/storage.go
+++ storage/storage.go
@@ -1,11 +1,8 @@
 package storage
 
 import (
-	sq "github.com/Masterminds/squirrel"
 	"github.com/Worldcoin/hubble-commander/config"
 	"github.com/Worldcoin/hubble-commander/db"
-	"github.com/Worldcoin/hubble-commander/models"
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/jmoiron/sqlx"
 )
 
@@ -19,32 +16,4 @@ func NewStorage(cfg *config.Config) (*Storage, error) {
 		return nil, err
 	}
 	return &Storage{DB: dbInstance}, nil
-}
-
-func (storage *Storage) AddTransaction(tx *models.Transaction) error {
-	_, err := sq.
-		Insert("transaction").
-		Values(
-			tx.Hash,
-			tx.FromIndex,
-			tx.ToIndex,
-			tx.Amount,
-			tx.Fee,
-			tx.Nonce,
-			tx.Signature,
-		).
-		RunWith(storage.DB).
-		PlaceholderFormat(sq.Dollar).
-		Exec()
-
-	return err
-}
-
-func (storage *Storage) GetTransaction(hash common.Hash) (*models.Transaction, error) {
-	res := make([]models.Transaction, 0, 1)
-	err := storage.DB.Select(&res, "SELECT * FROM transaction WHERE tx_hash = $1", hash)
-	if err != nil {
-		return nil, err
-	}
-	return &res[0], nil
 }
---
+++ storage/transaction.go
@@ -0,0 +1,34 @@
+package storage
+
+import (
+	"github.com/Masterminds/squirrel"
+	"github.com/Worldcoin/hubble-commander/models"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+func (storage *Storage) AddTransaction(tx *models.Transaction) error {
+	_, err := squirrel.Insert("transaction").
+		Values(
+			tx.Hash,
+			tx.FromIndex,
+			tx.ToIndex,
+			tx.Amount,
+			tx.Fee,
+			tx.Nonce,
+			tx.Signature,
+		).
+		RunWith(storage.DB).
+		PlaceholderFormat(squirrel.Dollar).
+		Exec()
+
+	return err
+}
+
+func (storage *Storage) GetTransaction(hash common.Hash) (*models.Transaction, error) {
+	res := make([]models.Transaction, 0, 1)
+	err := storage.DB.Select(&res, "SELECT * FROM transaction WHERE tx_hash = $1", hash)
+	if err != nil {
+		return nil, err
+	}
+	return &res[0], nil
+}
