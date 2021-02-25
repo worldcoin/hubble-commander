5eb7165c394d705628da9a72e4aec7ab8e6efbf7
Daniel Izdebski <spidiritsu@gmail.com>
2021-02-25 17:35:03 +0100

    Add GetStateNodeByPath and move tests to their respective files

--- storage/state_node.go
+++ storage/state_node.go
@@ -18,11 +18,31 @@ func (s *Storage) AddStateNode(node *models.StateNode) error {
 	return err
 }
 
-func (s *Storage) GetStateNode(hash common.Hash) (*models.StateNode, error) {
+func (s *Storage) GetStateNodeByHash(hash common.Hash) (*models.StateNode, error) {
 	res := make([]models.StateNode, 0, 1)
 	sql, args, err := s.QB.Select("*").
 		From("state_node").
 		Where(squirrel.Eq{"data_hash": hash}).
+		ToSql()
+	if err != nil {
+		return nil, err
+	}
+	err = s.DB.Select(&res, sql, args...)
+	if err != nil {
+		return nil, err
+	}
+	return &res[0], nil
+}
+
+func (s *Storage) GetStateNodeByPath(path *models.MerklePath) (*models.StateNode, error) {
+	res := make([]models.StateNode, 0, 1)
+	pathValue, err := path.Value()
+	if err != nil {
+		return nil, err
+	}
+	sql, args, err := s.QB.Select("*").
+		From("state_node").
+		Where(squirrel.Eq{"merkle_path": pathValue}).
 		ToSql()
 	if err != nil {
 		return nil, err
--- storage/transaction_test.go
+++ storage/transaction_test.go
@@ -52,55 +52,6 @@ func (s *TransactionTestSuite) TestAddTransaction() {
 	s.Equal(tx, res)
 }
 
-func (s *TransactionTestSuite) TestAddStateNode() {
-	node := &models.StateNode{
-		MerklePath: "0000111",
-		DataHash:   common.BytesToHash([]byte{1, 2, 3, 4, 5}),
-	}
-	err := s.storage.AddStateNode(node)
-	s.NoError(err)
-
-	res, err := s.storage.GetStateNode(node.DataHash)
-	s.NoError(err)
-
-	s.Equal(node, res)
-}
-
-func (s *TransactionTestSuite) TestAddStateLeaf() {
-	leaf := &models.StateLeaf{
-		DataHash:     common.BytesToHash([]byte{1, 2, 3, 4, 5}),
-		AccountIndex: models.MakeUint256(1),
-		TokenIndex:   models.MakeUint256(1),
-		Balance:      models.MakeUint256(420),
-		Nonce:        models.MakeUint256(0),
-	}
-	err := s.storage.AddStateLeaf(leaf)
-	s.NoError(err)
-
-	res, err := s.storage.GetStateLeaf(leaf.DataHash)
-	s.NoError(err)
-
-	s.Equal(leaf, res)
-}
-
-func (s *TransactionTestSuite) TestAddStateUpdate() {
-	update := &models.StateUpdate{
-		ID:          1,
-		MerklePath:  "11111111111111111111111111111111",
-		CurrentHash: common.BytesToHash([]byte{1, 2}),
-		CurrentRoot: common.BytesToHash([]byte{1, 2, 3}),
-		PrevHash:    common.BytesToHash([]byte{1, 2, 3, 4}),
-		PrevRoot:    common.BytesToHash([]byte{1, 2, 3, 4, 5}),
-	}
-	err := s.storage.AddStateUpdate(update)
-	s.NoError(err)
-
-	res, err := s.storage.GetStateUpdate(1)
-	s.NoError(err)
-
-	s.Equal(update, res)
-}
-
 func TestTransactionTestSuite(t *testing.T) {
 	suite.Run(t, new(TransactionTestSuite))
 }
