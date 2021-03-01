dfa0b3bceeb8646a8ec74f67697f64f0f594e0c8
Michał Sieczkowski <michal@ethworks.io>
2021-03-01 17:46:14 +0100

    Create StateTree with basic Set method

---
+++ storage/merkle_tree.go
@@ -0,0 +1,62 @@
+package storage
+
+import (
+	"github.com/Worldcoin/hubble-commander/contracts/frontend/generic"
+	"github.com/Worldcoin/hubble-commander/encoder"
+	"github.com/Worldcoin/hubble-commander/models"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+type StateTree struct {
+	storage *Storage
+}
+
+func NewStateTree(storage *Storage) *StateTree {
+	return &StateTree{storage}
+}
+
+func (s *StateTree) Set(index uint32, state models.UserState) error {
+	leaf, err := NewStateLeaf(&state)
+	if err != nil {
+		return err
+	}
+
+	err = s.storage.AddStateLeaf(leaf)
+	if err != nil {
+		return err
+	}
+
+	err = s.storage.AddStateNode(&models.StateNode{
+		MerklePath: models.MerklePath{
+			Path:  index,
+			Depth: 32,
+		},
+		DataHash: leaf.DataHash,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewStateLeaf(state *models.UserState) (*models.StateLeaf, error) {
+	encodedState, err := encoder.EncodeUserState(toContractUserState(state))
+	if err != nil {
+		return nil, err
+	}
+	dataHash := crypto.Keccak256Hash(encodedState)
+	return &models.StateLeaf{
+		DataHash:  dataHash,
+		UserState: *state,
+	}, nil
+}
+
+func toContractUserState(state *models.UserState) generic.TypesUserState {
+	return generic.TypesUserState{
+		PubkeyID: &state.AccountIndex.Int,
+		TokenID:  &state.TokenIndex.Int,
+		Balance:  &state.Balance.Int,
+		Nonce:    &state.Nonce.Int,
+	}
+}
---
+++ storage/merkle_tree_test.go
@@ -0,0 +1,76 @@
+package storage
+
+import (
+	"testing"
+
+	"github.com/Worldcoin/hubble-commander/db"
+	"github.com/Worldcoin/hubble-commander/models"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type StateTreeTestSuite struct {
+	*require.Assertions
+	suite.Suite
+	db      *db.TestDB
+	storage *Storage
+	tree    *StateTree
+	leaf    *models.StateLeaf
+}
+
+func (s *StateTreeTestSuite) SetupSuite() {
+	s.Assertions = require.New(s.T())
+}
+
+func (s *StateTreeTestSuite) SetupTest() {
+	testDB, err := db.GetTestDB()
+	s.NoError(err)
+	s.db = testDB
+	s.storage = NewTestStorage(testDB.DB)
+	s.tree = NewStateTree(s.storage)
+
+	state := models.UserState{
+		AccountIndex: models.MakeUint256(1),
+		TokenIndex:   models.MakeUint256(1),
+		Balance:      models.MakeUint256(420),
+		Nonce:        models.MakeUint256(0),
+	}
+	leaf, err := NewStateLeaf(&state)
+	s.NoError(err)
+	s.leaf = leaf
+}
+
+func (s *StateTreeTestSuite) TearDownTest() {
+	err := s.db.Teardown()
+	s.NoError(err)
+}
+
+func (s *StateTreeTestSuite) Test_Set_StoresStateLeafRecord() {
+	err := s.tree.Set(0, s.leaf.UserState)
+	s.NoError(err)
+
+	actualLeaf, err := s.storage.GetStateLeaf(s.leaf.DataHash)
+	s.NoError(err)
+	s.Equal(s.leaf, actualLeaf)
+}
+
+func (s *StateTreeTestSuite) Test_Set_StoresStateNodeRecord() {
+	err := s.tree.Set(0, s.leaf.UserState)
+	s.NoError(err)
+
+	expectedNode := &models.StateNode{
+		MerklePath: models.MerklePath{
+			Path:  0,
+			Depth: 32,
+		},
+		DataHash: s.leaf.DataHash,
+	}
+
+	node, err := s.storage.GetStateNodeByHash(s.leaf.DataHash)
+	s.NoError(err)
+	s.Equal(expectedNode, node)
+}
+
+func TestMerkleTreeTestSuite(t *testing.T) {
+	suite.Run(t, new(StateTreeTestSuite))
+}
