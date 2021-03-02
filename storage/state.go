d4b5ebb34c8081da8cd28f28ddf8033373ccd01d
Daniel Izdebski <spidiritsu@gmail.com>
2021-03-02 12:53:52 +0100

    Add hashTo method

--- storage/state.go
+++ storage/state.go
@@ -13,11 +13,15 @@ func init() {
 	// Same as keccak256(abi.encode(0))
 	zeroHashes[0] = common.HexToHash("0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563")
 	for i := 1; i < 32; i++ {
-		buf := make([]byte, 64)
-		copy(buf[0:32], zeroHashes[i-1].Bytes())
-		copy(buf[32:64], zeroHashes[i-1].Bytes())
-		zeroHashes[i] = crypto.Keccak256Hash(buf)
+		zeroHashes[i] = hashTwo(zeroHashes[i-1], zeroHashes[i-1])
 	}
+}
+
+func hashTwo(a common.Hash, b common.Hash) common.Hash {
+	buf := make([]byte, 64)
+	copy(buf[0:32], a.Bytes())
+	copy(buf[32:64], b.Bytes())
+	return crypto.Keccak256Hash(buf)
 }
 
 func GetZeroHash(level uint) common.Hash {
