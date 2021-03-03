f1e2ddb7dd80edca56d3a8cb25ebd4a2d772eda7
Dmytro Maretskyi <maretskii@gmail.com>
2021-03-03 14:09:16 +0100

    Add tx encoding

--- encoder/encoder_test.go
+++ encoder/encoder_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/Worldcoin/hubble-commander/testutils/deployer"
 	"github.com/Worldcoin/hubble-commander/testutils/simulator"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
@@ -103,6 +104,32 @@ func (s *EncoderTestSuite) TestDecimalEncoding() {
 	s.NoError(err)
 
 	s.Equal(uint16(expected.Uint64()), encoded)
+}
+
+func (s *EncoderTestSuite) TestTransactionEncoding() {
+	tx := models.Transaction{
+		Hash:      common.Hash{},
+		FromIndex: models.MakeUint256(1),
+		ToIndex:   models.MakeUint256(2),
+		Amount:    models.MakeUint256(50),
+		Fee:       models.MakeUint256(10),
+		Nonce:     models.MakeUint256(22),
+		Signature: nil,
+	}
+
+	encoded, err := EncodeTransaction(tx)
+	s.NoError(err)
+
+	txTransfer := testtx.TxTransfer{
+		FromIndex: &tx.FromIndex.Int,
+		ToIndex:   &tx.ToIndex.Int,
+		Amount:    &tx.Amount.Int,
+		Fee:       &tx.Fee.Int,
+	}
+	expected, err := s.testTx.TransferSerialize(&bind.CallOpts{Pending: false}, []testtx.TxTransfer{txTransfer})
+	s.NoError(err)
+
+	s.Equal(expected, encoded)
 }
 
 func TestEncoderTestSuite(t *testing.T) {
