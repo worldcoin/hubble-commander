e04eb06142863a40daee42aa4fcf327581057c24
Daniel Izdebski <spidiritsu@gmail.com>
2021-03-03 11:50:14 +0100

    Fix error in zeroHashes calculation

--- storage/state.go
+++ storage/state.go
@@ -6,13 +6,13 @@ import (
 )
 
 var (
-	zeroHashes [32]common.Hash
+	zeroHashes [33]common.Hash
 )
 
 func init() {
 	// Same as keccak256(abi.encode(0))
 	zeroHashes[0] = common.HexToHash("0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563")
-	for i := 1; i < 32; i++ {
+	for i := 1; i <= 32; i++ {
 		zeroHashes[i] = hashTwo(zeroHashes[i-1], zeroHashes[i-1])
 	}
 }
@@ -25,8 +25,8 @@ func hashTwo(a common.Hash, b common.Hash) common.Hash {
 }
 
 func GetZeroHash(level uint) common.Hash {
-	if level > 31 {
-		panic("level >= 32")
+	if level > 32 {
+		panic("level > 32")
 	}
 
 	return zeroHashes[level]
