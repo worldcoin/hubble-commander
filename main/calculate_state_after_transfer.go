443a631286e5e49d03d540d6034a0e580a80cbed
Daniel Izdebski <spidiritsu@gmail.com>
2021-03-09 13:17:04 +0100

    Add validation

--- main/calculate_state_after_transfer.go
+++ main/calculate_state_after_transfer.go
@@ -1,21 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"math/big"
 
 	"github.com/Worldcoin/hubble-commander/models"
 )
 
-func CalculateStateAfterTransfer(senderState, receiverState *models.UserState, tx *models.Transaction) (*models.UserState, *models.UserState, error) {
-	// TODO: VALIDATION
+func CalculateStateAfterTransfer(
+	senderState,
+	receiverState *models.UserState,
+	tx *models.Transaction,
+) (
+	newSenderState models.UserState,
+	newReceiverState models.UserState,
+	err error,
+) {
+	// TODO: Signature validation
+	// TODO: Do we check if sender and receiver states exist?
 
-	senderState.Nonce.Add(&senderState.Nonce.Int, big.NewInt(1))
+	if tx == nil {
+		err = fmt.Errorf("transaction cannot be nil")
+		return
+	}
+
+	if senderState == nil || receiverState == nil {
+		err = fmt.Errorf("sender/receiver cannot be nil")
+		return
+	}
+
+	if senderState.Nonce.Cmp(&tx.Nonce.Int) != 0 {
+		err = fmt.Errorf("incorrect nonce")
+		return
+	}
 
 	totalAmount := big.NewInt(0)
 	totalAmount.Add(&tx.Amount.Int, &tx.Fee.Int)
-	senderState.Balance.Sub(&senderState.Balance.Int, totalAmount)
 
-	receiverState.Balance.Add(&receiverState.Balance.Int, &tx.Amount.Int)
+	if senderState.Balance.Cmp(totalAmount) < 0 {
+		err = fmt.Errorf("amount exceeds balance")
+		return
+	}
 
-	return senderState, receiverState, nil
+	newSenderState = *senderState
+	newReceiverState = *receiverState
+
+	newSenderState.Nonce.Add(&senderState.Nonce.Int, big.NewInt(1))
+
+	newSenderState.Balance.Sub(&senderState.Balance.Int, totalAmount)
+
+	newReceiverState.Balance.Add(&receiverState.Balance.Int, &tx.Amount.Int)
+
+	return newSenderState, newReceiverState, nil
 }
--- main/calculate_state_after_transfer_test.go
+++ main/calculate_state_after_transfer_test.go
@@ -7,20 +7,22 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_CalculateStateAfterTransfer_UpdatesSenderAndReceiverStates(t *testing.T) {
-	senderState := models.UserState{
+var (
+	senderState = models.UserState{
 		AccountIndex: models.MakeUint256(1),
 		TokenIndex:   models.MakeUint256(1),
 		Balance:      models.MakeUint256(420),
 		Nonce:        models.MakeUint256(0),
 	}
-	receiverState := models.UserState{
+	receiverState = models.UserState{
 		AccountIndex: models.MakeUint256(2),
 		TokenIndex:   models.MakeUint256(1),
 		Balance:      models.MakeUint256(0),
 		Nonce:        models.MakeUint256(0),
 	}
+)
 
+func Test_CalculateStateAfterTransfer_UpdatesSenderAndReceiverStates(t *testing.T) {
 	tx := models.Transaction{
 		FromIndex: models.MakeUint256(1),
 		ToIndex:   models.MakeUint256(2),
@@ -37,4 +39,48 @@ func Test_CalculateStateAfterTransfer_UpdatesSenderAndReceiverStates(t *testing.
 
 	require.Equal(t, newReceiverState.Nonce, models.MakeUint256(0))
 	require.Equal(t, newReceiverState.Balance, models.MakeUint256(100))
+
+	require.NotEqual(t, &newSenderState, &senderState)
+	require.NotEqual(t, &newReceiverState, &receiverState)
+}
+
+func Test_CalculateStateAfterTransfer_Validation_Nonce(t *testing.T) {
+	tx := models.Transaction{
+		FromIndex: models.MakeUint256(1),
+		ToIndex:   models.MakeUint256(2),
+		Amount:    models.MakeUint256(100),
+		Fee:       models.MakeUint256(10),
+		Nonce:     models.MakeUint256(1),
+	}
+
+	_, _, err := CalculateStateAfterTransfer(&senderState, &receiverState, &tx)
+	require.Error(t, err)
+}
+
+func Test_CalculateStateAfterTransfer_Validation_Balance(t *testing.T) {
+	tx := models.Transaction{
+		FromIndex: models.MakeUint256(1),
+		ToIndex:   models.MakeUint256(2),
+		Amount:    models.MakeUint256(400),
+		Fee:       models.MakeUint256(50),
+		Nonce:     models.MakeUint256(0),
+	}
+
+	_, _, err := CalculateStateAfterTransfer(&senderState, &receiverState, &tx)
+	require.Error(t, err)
+}
+
+func Test_CalculateStateAfterTransfer_Validation_Account(t *testing.T) {
+	tx := models.Transaction{
+		Nonce: models.MakeUint256(0),
+	}
+
+	_, _, err := CalculateStateAfterTransfer(nil, &receiverState, &tx)
+	require.Error(t, err)
+
+	_, _, err = CalculateStateAfterTransfer(&senderState, nil, &tx)
+	require.Error(t, err)
+
+	_, _, err = CalculateStateAfterTransfer(&senderState, &receiverState, nil)
+	require.Error(t, err)
 }
