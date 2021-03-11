23c7d4b38dc74f7842fb9148554efacae3cb4eed
Michał Sieczkowski <michal@ethworks.io>
2021-03-11 14:07:42 +0100

    Create PadLeft utility function

---
+++ utils/bytes_test.go
@@ -0,0 +1,13 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_PadLeft(t *testing.T) {
+	require.Equal(t, []byte{0, 0, 1}, PadLeft([]byte{1}, 3))
+	require.Equal(t, []byte{0, 0, 1}, PadLeft([]byte{0, 0, 1}, 3))
+	require.Equal(t, []byte{0, 0, 1}, PadLeft([]byte{0, 0, 1}, 2))
+}
