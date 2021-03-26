b551d725475ad5d247b8f3126cbd35c2a46e97d6
Daniel Izdebski <spidiritsu@gmail.com>
2021-03-26 21:30:42 +0100

    Add Wallet model

---
+++ models/wallet.go
@@ -0,0 +1,75 @@
+package models
+
+import (
+	"crypto/rand"
+
+	"github.com/kilic/bn254/bls"
+)
+
+var DefaultDomain = [32]byte{0x00, 0x00, 0x00, 0x00}
+
+type Wallet struct {
+	signer bls.BLSSigner
+}
+
+func BytesToSignature(b []byte) (*bls.Signature, error) {
+	sig, err := bls.SignatureFromBytes(b)
+	if err != nil {
+		return nil, err
+	}
+	return sig, nil
+}
+
+func NewWallet(domain [32]byte) (*Wallet, error) {
+	newAccount, err := bls.NewKeyPair(rand.Reader)
+	if err != nil {
+		return nil, err
+	}
+	signer := bls.BLSSigner{Account: newAccount, Domain: domain[:]}
+	return &Wallet{signer: signer}, nil
+}
+
+func (w *Wallet) Bytes() (secretKey, pubkey []byte) {
+	accountBytes := w.signer.Account.ToBytes()
+	secretBytes := accountBytes[128:]
+	pubkeyBytes := accountBytes[:128]
+	return secretBytes, pubkeyBytes
+}
+
+func SecretToWallet(secretKey []byte, domain [32]byte) (*Wallet, error) {
+	keyPair, err := bls.NewKeyPairFromSecret(secretKey)
+	if err != nil {
+		return nil, err
+	}
+	signer := bls.BLSSigner{Account: keyPair, Domain: domain[:]}
+	return &Wallet{signer: signer}, nil
+}
+
+func (w *Wallet) Sign(data []byte) (*bls.Signature, error) {
+	signature, err := w.signer.Sign(data)
+	if err != nil {
+		return nil, err
+	}
+	return signature, nil
+}
+
+func (w *Wallet) VerifySignature(signature *bls.Signature, data []byte, pubkey *bls.PublicKey) (bool, error) {
+	verifier := bls.NewBLSVerifier(w.signer.Domain)
+	valid, err := verifier.Verify(data, signature, pubkey)
+	return valid, err
+}
+
+func VerifyAggregatedSignature(
+	aggregateSignature bls.Signature,
+	data []bls.Message,
+	pubkeys []*bls.PublicKey,
+	domain [32]byte,
+) (bool, error) {
+	verifier := bls.NewBLSVerifier(domain[:])
+	return verifier.VerifyAggregate(data, pubkeys, &aggregateSignature)
+}
+
+func NewAggregateSignature(signatures []*bls.Signature) bls.Signature {
+	aggregatedSig := bls.AggregateSignatures(signatures)
+	return *aggregatedSig
+}
---
+++ models/wallet_test.go
@@ -0,0 +1,53 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/kilic/bn254/bls"
+	r "github.com/stretchr/testify/require"
+)
+
+func Test_Wallet_SignAndVerify(t *testing.T) {
+	bytesToSign := []byte("0x123221")
+
+	wallet, err := NewWallet(DefaultDomain)
+	r.NoError(t, err)
+
+	signature, err := wallet.Sign(bytesToSign)
+	r.NoError(t, err)
+
+	isValid, err := wallet.VerifySignature(signature, bytesToSign, wallet.signer.Account.Public)
+	r.NoError(t, err)
+	r.True(t, isValid)
+}
+
+func Test_Wallet_SignAndVerifyAggregatedSignature(t *testing.T) {
+	totalSigners := 3
+	bytesToSign := [3][]byte{
+		[]byte("0x111111"),
+		[]byte("0x222222"),
+		[]byte("0x333333"),
+	}
+
+	publicKeys := make([]*bls.PublicKey, 0)
+	messages := make([]bls.Message, 0)
+	signatures := make([]*bls.Signature, 0)
+
+	for i := 0; i < totalSigners; i++ {
+		wallet, err := NewWallet(DefaultDomain)
+		r.NoError(t, err)
+
+		signature, err := wallet.Sign(bytesToSign[i])
+		r.NoError(t, err)
+
+		publicKeys = append(publicKeys, wallet.signer.Account.Public)
+		messages = append(messages, bytesToSign[i])
+		signatures = append(signatures, signature)
+	}
+
+	aggregatedSignature := NewAggregateSignature(signatures)
+
+	isValid, err := VerifyAggregatedSignature(aggregatedSignature, messages, publicKeys, DefaultDomain)
+	r.NoError(t, err)
+	r.True(t, isValid)
+}
