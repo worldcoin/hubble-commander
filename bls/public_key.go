1ff350881713edb7af53253be576fa67cff8957b
Michał Sieczkowski <michal@ethworks.io>
2021-03-31 16:27:52 +0200

    Fix byte order in PublicKey.ToBigInts()

--- bls/public_key.go
+++ bls/public_key.go
@@ -13,9 +13,9 @@ type PublicKey struct {
 func (p *PublicKey) ToBigInts() [4]*big.Int {
 	bytes := p.key.ToBytes()
 	return [4]*big.Int{
-		new(big.Int).SetBytes(bytes[:32]),
 		new(big.Int).SetBytes(bytes[32:64]),
-		new(big.Int).SetBytes(bytes[64:96]),
+		new(big.Int).SetBytes(bytes[:32]),
 		new(big.Int).SetBytes(bytes[96:]),
+		new(big.Int).SetBytes(bytes[64:96]),
 	}
 }
