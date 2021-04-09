77258d751b954655ac93833969810b8bb64d3007
Daniel Izdebski <spidiritsu@gmail.com>
2021-04-09 15:53:42 +0200

    Add rollup loop tests

--- commander/rollup.go
+++ commander/rollup.go
@@ -9,6 +9,7 @@ import (
 	"github.com/Worldcoin/hubble-commander/eth"
 	"github.com/Worldcoin/hubble-commander/models"
 	st "github.com/Worldcoin/hubble-commander/storage"
+	"github.com/ethereum/go-ethereum/common"
 )
 
 func RollupEndlessLoop(storage *st.Storage, client *eth.Client, cfg *config.RollupConfig) error {
@@ -25,7 +26,7 @@ func RollupLoop(storage *st.Storage, client *eth.Client, cfg *config.RollupConfi
 			ticker.Stop()
 			return nil
 		case <-ticker.C:
-			err := submitBatch(storage, client, cfg)
+			err := XXX(storage, client, cfg)
 			if err != nil {
 				var e *BatchError
 				if errors.As(err, &e) {
@@ -38,7 +39,7 @@ func RollupLoop(storage *st.Storage, client *eth.Client, cfg *config.RollupConfi
 	}
 }
 
-func submitBatch(storage *st.Storage, client *eth.Client, cfg *config.RollupConfig) (err error) {
+func XXX(storage *st.Storage, client *eth.Client, cfg *config.RollupConfig) (err error) {
 	tx, txStorage, err := storage.BeginTransaction()
 	if err != nil {
 		return
@@ -55,6 +56,20 @@ func submitBatch(storage *st.Storage, client *eth.Client, cfg *config.RollupConf
 		return
 	}
 
+	err = SubmitBatch(commitments, txStorage, client, cfg)
+	if err != nil {
+		return err
+	}
+	
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func SubmitBatch(commitments []models.Commitment, storage *st.Storage, client *eth.Client, cfg *config.RollupConfig) (err error) {
 	if len(commitments) < int(cfg.MinCommitmentsPerBatch) {
 		return
 	}
@@ -66,20 +81,15 @@ func submitBatch(storage *st.Storage, client *eth.Client, cfg *config.RollupConf
 
 	err = storage.AddBatch(batch)
 	if err != nil {
-		return err
+		return
 	}
 
-	err = markCommitmentsAsIncluded(txStorage, commitments, &batch.Hash, accountRoot)
+	err = markCommitmentsAsIncluded(storage, commitments, &batch.Hash, accountRoot)
 	if err != nil {
-		return err
+		return
 	}
 
 	log.Printf("Submitted %d commitment(s) on chain. Batch ID: %d. Batch Hash: %v", len(commitments), batch.ID.Uint64(), batch.Hash)
-
-	err = tx.Commit()
-	if err != nil {
-		return err
-	}
 
 	return nil
 }
@@ -148,4 +158,14 @@ func commitmentsLoop(
 	}
 
 	return commitments, nil
+}
+
+func markCommitmentsAsIncluded(storage *st.Storage, commitments []models.Commitment, batchHash, accountRoot *common.Hash) error {
+	for i := range commitments {
+		err := storage.MarkCommitmentAsIncluded(commitments[i].ID, batchHash, accountRoot)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
---
+++ commander/rollup_test.go
@@ -0,0 +1,166 @@
+package commander
+
+import (
+	"math/big"
+	"testing"
+
+	"github.com/Worldcoin/hubble-commander/config"
+	"github.com/Worldcoin/hubble-commander/db"
+	"github.com/Worldcoin/hubble-commander/eth"
+	"github.com/Worldcoin/hubble-commander/models"
+	st "github.com/Worldcoin/hubble-commander/storage"
+	"github.com/Worldcoin/hubble-commander/utils"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+var (
+	baseTx = models.Transaction{
+		Hash:                 common.BigToHash(big.NewInt(1234)),
+		FromIndex:            1,
+		ToIndex:              2,
+		Amount:               models.MakeUint256(50),
+		Fee:                  models.MakeUint256(10),
+		Nonce:                models.MakeUint256(0),
+		Signature:            []byte{1, 2, 3, 4, 5},
+		IncludedInCommitment: nil,
+	}
+	baseCommitment = models.Commitment{
+		Transactions:      utils.RandomBytes(24),
+		FeeReceiver:       1,
+		CombinedSignature: models.MakeSignature(1, 2),
+		PostStateRoot:     utils.RandomHash(),
+	}
+)
+
+type RollupLoopTestSuite struct {
+	*require.Assertions
+	suite.Suite
+	db         *db.TestDB
+	storage    *st.Storage
+	tree       *st.StateTree
+	cfg        *config.RollupConfig
+	testClient *eth.TestClient
+}
+
+func (s *RollupLoopTestSuite) SetupSuite() {
+	s.Assertions = require.New(s.T())
+}
+
+func (s *RollupLoopTestSuite) SetupTest() {
+	testDB, err := db.NewTestDB()
+	s.NoError(err)
+	s.db = testDB
+	s.storage = st.NewTestStorage(testDB.DB)
+	s.tree = st.NewStateTree(s.storage)
+	s.cfg = &config.RollupConfig{
+		MinCommitmentsPerBatch: 1,
+		MaxCommitmentsPerBatch: 32,
+	}
+
+	s.testClient, err = eth.NewTestClient()
+	s.NoError(err)
+
+	userState := models.UserState{
+		AccountIndex: 1,
+		TokenIndex:   models.MakeUint256(1),
+		Balance:      models.MakeUint256(1000),
+		Nonce:        models.MakeUint256(0),
+	}
+
+	err = s.tree.Set(1, &userState)
+	s.NoError(err)
+}
+
+func (s *RollupLoopTestSuite) TearDownTest() {
+	s.testClient.Close()
+	err := s.db.Teardown()
+	s.NoError(err)
+}
+
+func (s *RollupLoopTestSuite) Test_SubmitBatch_SubmitsCommitmentsOnChain() {
+	commitmentID, err := s.storage.AddCommitment(&baseCommitment)
+	s.NoError(err)
+
+	commitment, err := s.storage.GetCommitment(*commitmentID)
+	s.NoError(err)
+
+	err = SubmitBatch([]models.Commitment{*commitment}, s.storage, s.testClient.Client, s.cfg)
+	s.NoError(err)
+
+	nextBatchID, err := s.testClient.Rollup.NextBatchID(nil)
+	s.NoError(err)
+	s.Equal(big.NewInt(2), nextBatchID)
+}
+
+func (s *RollupLoopTestSuite) Test_SubmitBatch_StoresBatchRecord() {
+	commitmentID, err := s.storage.AddCommitment(&baseCommitment)
+	s.NoError(err)
+
+	commitment, err := s.storage.GetCommitment(*commitmentID)
+	s.NoError(err)
+
+	err = SubmitBatch([]models.Commitment{*commitment}, s.storage, s.testClient.Client, s.cfg)
+	s.NoError(err)
+
+	batch, err := s.storage.GetBatchByID(models.MakeUint256(1))
+	s.NoError(err)
+	s.NotNil(batch)
+}
+
+func (s *RollupLoopTestSuite) addCommitments(count int) ([]int32, []models.Commitment) {
+	ids := make([]int32, 0, count)
+	commitments := make([]models.Commitment, 0, count)
+	for i := 0; i < count; i++ {
+		id, err := s.storage.AddCommitment(&baseCommitment)
+		s.NoError(err)
+		ids = append(ids, *id)
+
+		commitment, err := s.storage.GetCommitment(*id)
+		s.NoError(err)
+		commitments = append(commitments, *commitment)
+	}
+	return ids, commitments
+}
+
+func (s *RollupLoopTestSuite) Test_SubmitBatch_MarksCommitmentsAsIncluded() {
+	ids, commitments := s.addCommitments(2)
+
+	err := SubmitBatch(commitments, s.storage, s.testClient.Client, s.cfg)
+	s.NoError(err)
+
+	batch, err := s.storage.GetBatchByID(models.MakeUint256(1))
+	s.NoError(err)
+
+	for _, id := range ids {
+		commit, err := s.storage.GetCommitment(id)
+		s.NoError(err)
+		s.Equal(batch.Hash, *commit.IncludedInBatch)
+	}
+}
+
+func (s *RollupLoopTestSuite) Test_SubmitBatch_MarksCommitmentsAsIncluded_UnsavedCommitment() {
+	err := SubmitBatch([]models.Commitment{baseCommitment}, s.storage, s.testClient.Client, s.cfg)
+	s.EqualError(err, "no rows were affected by the update")
+}
+
+func (s *RollupLoopTestSuite) Test_SubmitBatch_UpdatesCommitmentsAccountRoot() {
+	ids, commitments := s.addCommitments(2)
+
+	err := SubmitBatch(commitments, s.storage, s.testClient.Client, s.cfg)
+	s.NoError(err)
+
+	accountRoot, err := s.testClient.AccountRegistry.Root(nil)
+	s.NoError(err)
+
+	for _, id := range ids {
+		commit, err := s.storage.GetCommitment(id)
+		s.NoError(err)
+		s.Equal(common.BytesToHash(accountRoot[:]), *commit.AccountTreeRoot)
+	}
+}
+
+func TestRollupLoopTestSuite(t *testing.T) {
+	suite.Run(t, new(RollupLoopTestSuite))
+}
