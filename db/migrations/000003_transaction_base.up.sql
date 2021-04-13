32f68ad2b4ac917a81cfdc1dcc09306cf9d28f2b
Daniel Izdebski <spidiritsu@gmail.com>
2021-04-13 15:49:37 +0200

    Rename columns

--- db/migrations/000003_transaction_base.up.sql
+++ db/migrations/000003_transaction_base.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "transaction_base" (
     tx_hash                BYTEA PRIMARY KEY,
     tx_type                SMALLINT NOT NULL,
-    from_id                BIGINT NOT NULL,
+    from_state_id          BIGINT NOT NULL,
     amount                 BIGINT NOT NULL,
     fee                    BIGINT NOT NULL,
     nonce                  BIGINT NOT NULL
--- db/migrations/000005_create2transfer.up.sql
+++ db/migrations/000005_create2transfer.up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE "create2transfer" (
     tx_hash                BYTEA PRIMARY KEY,
     tx_type                SMALLINT NOT NULL,
-    to_id                  BIGINT NOT NULL,
+    to_state_id            BIGINT NOT NULL,
     to_pubkey_id           BIGINT NOT NULL,
-    signature              BYTEA       NOT NULL,
+    signature              BYTEA NOT NULL,
     included_in_commitment INTEGER REFERENCES commitment,
     error_message          TEXT
 );
