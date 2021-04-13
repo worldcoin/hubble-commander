71e2a9ff3b311087bd379179e86f7318e0bdbdc9
Daniel Izdebski <spidiritsu@gmail.com>
2021-04-13 15:50:15 +0200

    Add Create2Transfer dto model

---
+++ models/dto/create2transfer.go
@@ -0,0 +1,61 @@
+package dto
+
+import (
+	"encoding/json"
+
+	"github.com/Worldcoin/hubble-commander/models"
+)
+
+type Create2Transfer struct {
+	FromStateID *uint32
+	ToStateID   *uint32
+	ToPubkeyID  *uint32
+	Amount      *models.Uint256
+	Fee         *models.Uint256
+	Nonce       *models.Uint256
+	Signature   HexString
+}
+
+type create2transferWithType struct {
+	Type        uint8
+	FromStateID *uint32
+	ToStateID   *uint32
+	ToPubkeyID  *uint32
+	Amount      *models.Uint256
+	Fee         *models.Uint256
+	Nonce       *models.Uint256
+	Signature   HexString
+}
+
+func (t Create2Transfer) MarshalJSON() ([]byte, error) {
+	transfer := create2transferWithType{
+		Type:        3,
+		FromStateID: t.FromStateID,
+		ToStateID:   t.ToStateID,
+		ToPubkeyID:  t.ToPubkeyID,
+		Amount:      t.Amount,
+		Fee:         t.Fee,
+		Nonce:       t.Nonce,
+		Signature:   t.Signature,
+	}
+	return json.Marshal(transfer)
+}
+
+func (t *Create2Transfer) UnmarshalJSON(bytes []byte) error {
+	var transfer create2transferWithType
+	err := json.Unmarshal(bytes, &transfer)
+	if err != nil {
+		return err
+	}
+
+	*t = Create2Transfer{
+		FromStateID: transfer.FromStateID,
+		ToStateID:   transfer.ToStateID,
+		ToPubkeyID:  transfer.ToPubkeyID,
+		Amount:      transfer.Amount,
+		Fee:         transfer.Fee,
+		Nonce:       transfer.Nonce,
+		Signature:   transfer.Signature,
+	}
+	return nil
+}
---
+++ models/dto/create2transfer_test.go
@@ -0,0 +1,52 @@
+package dto
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"testing"
+
+	"github.com/Worldcoin/hubble-commander/models"
+	"github.com/Worldcoin/hubble-commander/utils"
+	"github.com/Worldcoin/hubble-commander/utils/ref"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreate2Transfer_JSONMarshaling(t *testing.T) {
+	transfer := Create2Transfer{
+		FromStateID: ref.Uint32(1),
+		ToStateID:   ref.Uint32(2),
+		ToPubkeyID:  ref.Uint32(3),
+		Amount:      models.NewUint256(50),
+		Fee:         models.NewUint256(10),
+		Nonce:       models.NewUint256(0),
+		Signature:   utils.RandomBytes(12),
+	}
+	data, err := json.Marshal(transfer)
+	require.NoError(t, err)
+
+	var unmarshalled Create2Transfer
+	err = json.Unmarshal(data, &unmarshalled)
+	require.NoError(t, err)
+
+	require.Equal(t, transfer, unmarshalled)
+}
+
+func TestCreate2Transfer_MarshalJSON(t *testing.T) {
+	sig, err := hex.DecodeString("deadbeef")
+	require.NoError(t, err)
+
+	transfer := Create2Transfer{
+		FromStateID: ref.Uint32(1),
+		ToStateID:   ref.Uint32(2),
+		ToPubkeyID:   ref.Uint32(3),
+		Amount:      models.NewUint256(50),
+		Fee:         models.NewUint256(10),
+		Nonce:       models.NewUint256(0),
+		Signature:   sig,
+	}
+	data, err := json.Marshal(transfer)
+	require.NoError(t, err)
+
+	expected := `{"Type":3,"FromStateID":1,"ToStateID":2,"ToPubkeyID":3,"Amount":"50","Fee":"10","Nonce":"0","Signature":"0xdeadbeef"}`
+	require.Equal(t, expected, string(data))
+}
