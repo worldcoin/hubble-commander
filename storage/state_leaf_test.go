197f8200a4d56cc8655f251c598dce725f5d3a66
Michał Sieczkowski <michal@ethworks.io>
2021-04-13 16:42:18 +0200

    Extract errors to vars

--- storage/state_leaf_test.go
+++ storage/state_leaf_test.go
@@ -57,13 +57,13 @@ func (s *StateLeafTestSuite) Test_AddStateLeaf_AddAndRetrieve() {
 func (s *StateLeafTestSuite) Test_GetStateLeaf_NonExistentLeaf() {
 	hash := common.BytesToHash([]byte{1, 2, 3, 4, 5})
 	res, err := s.storage.GetStateLeaf(hash)
-	s.Equal(err, NewNotFoundError("state leaf"))
+	s.Equal(NewNotFoundError("state leaf"), err)
 	s.Nil(res)
 }
 
 func (s *StateLeafTestSuite) Test_GetStateLeaves_NoLeaves() {
 	res, err := s.storage.GetStateLeaves(1)
-	s.Equal(err, NewNotFoundError("state leaves"))
+	s.Equal(NewNotFoundError("state leaves"), err)
 	s.Nil(res)
 }
 
