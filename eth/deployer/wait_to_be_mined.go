54ab32f284090b28805e6dad0931fe52d8b64d31
Michał Sieczkowski <michal@ethworks.io>
2021-04-15 13:40:47 +0200

    Refactor WaitToBeMined to use ticker

--- eth/deployer/wait_to_be_mined.go
+++ eth/deployer/wait_to_be_mined.go
@@ -9,21 +9,25 @@ import (
 	"github.com/pkg/errors"
 )
 
-var ChainTimeout = 5 * time.Minute
+var (
+	PollInterval = 500 * time.Millisecond
+	ChainTimeout = 5 * time.Minute
+)
 
 func WaitToBeMined(c ChainBackend, tx *types.Transaction) (*types.Receipt, error) {
-	begin := time.Now()
+	ticker := time.NewTicker(PollInterval)
+	defer ticker.Stop()
 	for {
-		receipt, err := c.TransactionReceipt(context.Background(), tx.Hash())
-		if err != nil && err != ethereum.NotFound {
-			return nil, errors.WithStack(err)
-		}
-
-		if receipt != nil && receipt.BlockNumber != nil {
-			return receipt, nil
-		}
-
-		if time.Since(begin) > ChainTimeout {
+		select {
+		case <-ticker.C:
+			receipt, err := c.TransactionReceipt(context.Background(), tx.Hash())
+			if err != nil && err != ethereum.NotFound {
+				return nil, errors.WithStack(err)
+			}
+			if receipt != nil && receipt.BlockNumber != nil {
+				return receipt, nil
+			}
+		case <-time.After(ChainTimeout):
 			return nil, errors.Errorf("timeout on waiting for transcation to be mined")
 		}
 	}
