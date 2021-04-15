037cb22dc08a891aad08cf02dd7918a2d0ebe219
Daniel Izdebski <spidiritsu@gmail.com>
2021-04-15 14:06:15 +0200

    Update create2transfer to use foreign key

--- db/migrations/000005_create2transfer.up.sql
+++ db/migrations/000005_create2transfer.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "create2transfer" (
-    tx_hash                BYTEA PRIMARY KEY,
+    tx_hash                BYTEA PRIMARY KEY REFERENCES transaction_base,
     to_state_id            BIGINT NOT NULL,
     to_pubkey_id           BIGINT NOT NULL
 );
