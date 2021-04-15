d9af024c01326b6de00d311245d0e19bb1142499
Dmytro Maretskyi <maretskii@gmail.com>
2021-04-15 12:36:19 +0200

    Extract wait to be mined

--- eth/deployer/account_registry.go
+++ eth/deployer/account_registry.go
@@ -1,14 +1,10 @@
 package deployer
 
 import (
-	"context"
 	"log"
-	"time"
 
 	"github.com/Worldcoin/hubble-commander/contracts/accountregistry"
-	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/pkg/errors"
 )
 
@@ -26,22 +22,4 @@ func DeployAccountRegistry(c ChainConnection) (*common.Address, *accountregistry
 	}
 
 	return &accountRegistryAddress, accountRegistry, nil
-}
-
-func WaitToBeMined(c ChainBackend, tx *types.Transaction) (*types.Receipt, error) {
-	begin := time.Now()
-	for {
-		receipt, err := c.TransactionReceipt(context.Background(), tx.Hash())
-		if err != nil && err != ethereum.NotFound {
-			return nil, errors.WithStack(err)
-		}
-
-		if receipt != nil && receipt.BlockNumber != nil {
-			return receipt, nil
-		}
-
-		if time.Since(begin) > 5*time.Minute {
-			return nil, errors.Errorf("timeout on waiting for transcation to be mined")
-		}
-	}
 }
---
+++ eth/deployer/wait_to_be_mined.go
@@ -0,0 +1,28 @@
+package deployer
+
+import (
+	"context"
+	"time"
+
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/pkg/errors"
+)
+
+func WaitToBeMined(c ChainBackend, tx *types.Transaction) (*types.Receipt, error) {
+	begin := time.Now()
+	for {
+		receipt, err := c.TransactionReceipt(context.Background(), tx.Hash())
+		if err != nil && err != ethereum.NotFound {
+			return nil, errors.WithStack(err)
+		}
+
+		if receipt != nil && receipt.BlockNumber != nil {
+			return receipt, nil
+		}
+
+		if time.Since(begin) > 5*time.Minute {
+			return nil, errors.Errorf("timeout on waiting for transcation to be mined")
+		}
+	}
+}
