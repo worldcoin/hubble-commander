bc5557e5379cac0494f44ef251d0d449abcd12f6
Daniel Izdebski <spidiritsu@gmail.com>
2021-04-15 13:48:00 +0200

    Adjust CreateCommitments tests

--- commander/create_commitments_test.go
+++ commander/create_commitments_test.go
@@ -69,11 +69,11 @@ func (s *CreateCommitmentsTestSuite) TearDownTest() {
 	s.NoError(err)
 }
 
-func (s *CreateCommitmentsTestSuite) Test_CreateCommitments_DoesNothingWhenThereAreNotEnoughPendingTxs() {
+func (s *CreateCommitmentsTestSuite) Test_CreateCommitments_DoesNothingWhenThereAreNotEnoughPendingTransfers() {
 	preRoot, err := storage.NewStateTree(s.storage).Root()
 	s.NoError(err)
 
-	commitments, err := createCommitments([]models.Transaction{}, s.storage, s.cfg)
+	commitments, err := createCommitments([]models.Transfer{}, s.storage, s.cfg)
 	s.NoError(err)
 	s.Len(commitments, 0)
 
@@ -83,19 +83,19 @@ func (s *CreateCommitmentsTestSuite) Test_CreateCommitments_DoesNothingWhenThere
 	s.Equal(preRoot, postRoot)
 }
 
-func (s *CreateCommitmentsTestSuite) Test_CreateCommitments_DoesNothingWhenThereAreNotEnoughValidTxs() {
-	txs := generateValidTransactions(2)
-	txs[1].Amount = models.MakeUint256(99999999999)
-	s.addTransactions(txs)
+func (s *CreateCommitmentsTestSuite) Test_CreateCommitments_DoesNothingWhenThereAreNotEnoughValidTransfers() {
+	transfers := generateValidTransfers(2)
+	transfers[1].Amount = models.MakeUint256(99999999999)
+	s.addTransfers(transfers)
 
-	pendingTransactions, err := s.storage.GetPendingTransactions()
+	pendingTransfers, err := s.storage.GetPendingTransfers()
 	s.NoError(err)
-	s.Len(pendingTransactions, 2)
+	s.Len(pendingTransfers, 2)
 
 	preRoot, err := storage.NewStateTree(s.storage).Root()
 	s.NoError(err)
 
-	commitments, err := createCommitments(pendingTransactions, s.storage, s.cfg)
+	commitments, err := createCommitments(pendingTransfers, s.storage, s.cfg)
 	s.NoError(err)
 	s.Len(commitments, 0)
 
@@ -106,12 +106,12 @@ func (s *CreateCommitmentsTestSuite) Test_CreateCommitments_DoesNothingWhenThere
 }
 
 func (s *CreateCommitmentsTestSuite) Test_CreateCommitments_StoresCorrectCommitment() {
-	pendingTransactions := s.prepareAndReturnPendingTransactions(3)
+	pendingTransfers := s.prepareAndReturnPendingTransfers(3)
 
 	preRoot, err := storage.NewStateTree(s.storage).Root()
 	s.NoError(err)
 
-	commitments, err := createCommitments(pendingTransactions, s.storage, s.cfg)
+	commitments, err := createCommitments(pendingTransfers, s.storage, s.cfg)
 	s.NoError(err)
 	s.Len(commitments, 1)
 	s.Len(commitments[0].Transactions, 24)
@@ -126,56 +126,56 @@ func (s *CreateCommitmentsTestSuite) Test_CreateCommitments_StoresCorrectCommitm
 }
 
 func (s *CreateCommitmentsTestSuite) Test_CreateCommitments_CreatesMaximallyAsManyCommitmentsAsSpecifiedInConfig() {
-	pendingTransactions := s.prepareAndReturnPendingTransactions(2)
+	pendingTransfers := s.prepareAndReturnPendingTransfers(2)
 
-	commitments, err := createCommitments(pendingTransactions, s.storage, s.cfg)
+	commitments, err := createCommitments(pendingTransfers, s.storage, s.cfg)
 	s.NoError(err)
 	s.Len(commitments, 1)
 }
 
-func (s *CreateCommitmentsTestSuite) Test_CreateCommitments_MarksTransactionsAsIncludedInCommitment() {
-	pendingTransactions := s.prepareAndReturnPendingTransactions(2)
+func (s *CreateCommitmentsTestSuite) Test_CreateCommitments_MarksTransfersAsIncludedInCommitment() {
+	pendingTransfers := s.prepareAndReturnPendingTransfers(2)
 
-	commitments, err := createCommitments(pendingTransactions, s.storage, s.cfg)
+	commitments, err := createCommitments(pendingTransfers, s.storage, s.cfg)
 	s.NoError(err)
 	s.Len(commitments, 1)
 
-	for i := range pendingTransactions {
-		tx, err := s.storage.GetTransaction(pendingTransactions[i].Hash)
+	for i := range pendingTransfers {
+		tx, err := s.storage.GetTransfer(pendingTransfers[i].Hash)
 		s.NoError(err)
 		s.Equal(*tx.IncludedInCommitment, int32(1))
 	}
 }
 
 func (s *CreateCommitmentsTestSuite) Test_removeTransactions() {
-	tx1 := models.Transaction{Hash: utils.RandomHash()}
-	tx2 := models.Transaction{Hash: utils.RandomHash()}
-	tx3 := models.Transaction{Hash: utils.RandomHash()}
+	transfer1 := models.Transfer{Hash: utils.RandomHash()}
+	transfer2 := models.Transfer{Hash: utils.RandomHash()}
+	transfer3 := models.Transfer{Hash: utils.RandomHash()}
 
-	txs := []models.Transaction{tx1, tx2, tx3}
-	toRemove := []models.Transaction{tx2}
+	transfers := []models.Transfer{transfer1, transfer2, transfer3}
+	toRemove := []models.Transfer{transfer2}
 
-	s.Equal([]models.Transaction{tx1, tx3}, removeTransactions(txs, toRemove))
+	s.Equal([]models.Transfer{transfer1, transfer3}, removeTransfer(transfers, toRemove))
 }
 
 func TestCreateCommitmentsTestSuite(t *testing.T) {
 	suite.Run(t, new(CreateCommitmentsTestSuite))
 }
 
-func (s *CreateCommitmentsTestSuite) addTransactions(txs []models.Transaction) {
-	for i := range txs {
-		err := s.storage.AddTransaction(&txs[i])
+func (s *CreateCommitmentsTestSuite) addTransfers(transfers []models.Transfer) {
+	for i := range transfers {
+		err := s.storage.AddTransfer(&transfers[i])
 		s.NoError(err)
 	}
 }
 
-func (s *CreateCommitmentsTestSuite) prepareAndReturnPendingTransactions(txAmount int) []models.Transaction {
-	txs := generateValidTransactions(txAmount)
-	s.addTransactions(txs)
+func (s *CreateCommitmentsTestSuite) prepareAndReturnPendingTransfers(transferAmount int) []models.Transfer {
+	transfers := generateValidTransfers(transferAmount)
+	s.addTransfers(transfers)
 
-	pendingTransactions, err := s.storage.GetPendingTransactions()
+	pendingTransfers, err := s.storage.GetPendingTransfers()
 	s.NoError(err)
-	s.Len(pendingTransactions, txAmount)
+	s.Len(pendingTransfers, transferAmount)
 
-	return pendingTransactions
+	return pendingTransfers
 }
