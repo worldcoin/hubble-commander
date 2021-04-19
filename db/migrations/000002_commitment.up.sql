2f9b356bcf71c1388e929a03a9dab9ba27175688
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-04-19 15:13:46 +0200

    Add type to commitment and batch

--- db/migrations/000002_commitment.up.sql
+++ db/migrations/000002_commitment.up.sql
@@ -1,9 +1,10 @@
 CREATE TABLE commitment (
     commitment_id      SERIAL PRIMARY KEY,
-    transactions       BYTEA  NOT NULL, -- 32 transactions encoded in compact format
-    fee_receiver       BIGINT NOT NULL, -- state index of fee receiver
-    combined_signature BYTEA  NOT NULL,
-    post_state_root    BYTEA  NOT NULL,
+    type               SMALLINT NOT NULL,
+    transactions       BYTEA    NOT NULL, -- 32 transactions encoded in compact format
+    fee_receiver       BIGINT   NOT NULL, -- state index of fee receiver
+    combined_signature BYTEA    NOT NULL,
+    post_state_root    BYTEA    NOT NULL,
     account_tree_root  BYTEA,
-    included_in_batch  BYTEA REFERENCES batch
+    included_in_batch  BYTEA REFERENCES batch,
 );
