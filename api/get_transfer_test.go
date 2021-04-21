0047ae6330fe7f9dae0b47460e1b7ef07e41d4e4
Bartlomiej Tarczynski <51199571+b-tarczynski@users.noreply.github.com>
2021-04-21 14:26:00 +0200

    Add flag -dev for disabling signature verification (#90)
    * Add dev flag for disabling signature validation
    
    * Fix getTransfer tests
    
    * Add make dev-run and readme description
    
    * Rename dev-run and dev-prune
    
    * Rename Dev -> DevMode
    
    * Rename phony commands
    
    * Update README.md
    
    Co-authored-by: Michał Sieczkowski <michal@ethworks.io>

--- api/get_transfer_test.go
+++ api/get_transfer_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/Worldcoin/hubble-commander/bls"
+	"github.com/Worldcoin/hubble-commander/config"
 	"github.com/Worldcoin/hubble-commander/db"
 	"github.com/Worldcoin/hubble-commander/models"
 	"github.com/Worldcoin/hubble-commander/models/dto"
@@ -30,7 +31,11 @@ func (s *GetTransferTestSuite) SetupTest() {
 	s.NoError(err)
 
 	storage := st.NewTestStorage(testDB.DB)
-	s.api = &API{nil, storage, nil}
+	s.api = &API{
+		cfg:     &config.APIConfig{},
+		storage: storage,
+		client:  nil,
+	}
 	s.db = testDB
 
 	s.wallet, err = bls.NewRandomWallet(mockDomain)
