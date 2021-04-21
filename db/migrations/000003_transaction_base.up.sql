9acb789da661045fae2b7aeff81430ee503be111
Bartlomiej Tarczynski <51199571+b-tarczynski@users.noreply.github.com>
2021-04-21 16:27:58 +0200

    Add FK for to_pub_key_id column in c2t table, remove unnecessary "" (#87)
    * Add FK for c2t, change migrations number
    
    * Fix create2Transfer test
    
    * Remove unnecessary "" from table names
    
    * Lint remaining tables
    
    * Revert FK addition in existing migrations
    
    * Add separate migration file for create2transfer FK addition
    
    Co-authored-by: Michał Sieczkowski <michal@ethworks.io>

--- db/migrations/000003_transaction_base.up.sql
+++ db/migrations/000003_transaction_base.up.sql
@@ -1,11 +1,11 @@
-CREATE TABLE "transaction_base" (
+CREATE TABLE transaction_base (
     tx_hash                BYTEA PRIMARY KEY,
-    tx_type                SMALLINT NOT NULL,
-    from_state_id          BIGINT NOT NULL,
+    tx_type                SMALLINT    NOT NULL,
+    from_state_id          BIGINT      NOT NULL,
     amount                 NUMERIC(78) NOT NULL,
     fee                    NUMERIC(78) NOT NULL,
     nonce                  NUMERIC(78) NOT NULL,
-    signature              BYTEA NOT NULL,
+    signature              BYTEA       NOT NULL,
     included_in_commitment INTEGER REFERENCES commitment,
     error_message          TEXT
 );
---
+++ db/migrations/000009_create2transfer_to_pub_key_id_fkey.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE create2transfer
+    DROP CONSTRAINT create2transfer_to_pub_key_id_fkey;
