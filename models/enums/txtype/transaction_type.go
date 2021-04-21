e577775e8b5da77eced94ddc2615d927b862bf29
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-04-21 15:45:21 +0200

    Add Marshaler interface for TransactionType

--- models/enums/txtype/transaction_type.go
+++ models/enums/txtype/transaction_type.go
@@ -1,5 +1,10 @@
 package txtype
 
+import (
+	"encoding/json"
+	"errors"
+)
+
 type TransactionType uint8
 
 const (
@@ -8,11 +13,15 @@ const (
 	MassMigration   TransactionType = 5
 )
 
-var TransactionTypes = map[TransactionType]string{
-	Transfer:        "TRANSFER",
-	Create2Transfer: "CREATE2TRANSFER",
-	MassMigration:   "MASS_MIGRATION",
-}
+var (
+	TransactionTypes = map[TransactionType]string{
+		Transfer:        "TRANSFER",
+		Create2Transfer: "CREATE2TRANSFER",
+		MassMigration:   "MASS_MIGRATION",
+	}
+
+	ErrUnsupportedTransactionType = errors.New("unsupported transaction type")
+)
 
 func (s TransactionType) Ref() *TransactionType {
 	return &s
@@ -24,4 +33,28 @@ func (s TransactionType) String() string {
 		return "UNKNOWN"
 	}
 	return msg
+}
+
+func (s *TransactionType) UnmarshalJSON(bytes []byte) error {
+	var strType string
+	err := json.Unmarshal(bytes, &strType)
+	if err != nil {
+		return err
+	}
+
+	for k, v := range TransactionTypes {
+		if v == strType {
+			*s = k
+			return nil
+		}
+	}
+	return ErrUnsupportedTransactionType
+}
+
+func (s TransactionType) MarshalJSON() ([]byte, error) {
+	msg, exists := TransactionTypes[s]
+	if !exists {
+		return nil, ErrUnsupportedTransactionType
+	}
+	return json.Marshal(msg)
 }
