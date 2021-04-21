b7b23218cda7e98636eb4070c7beabe683f4bece
Michał Sieczkowski <michal@ethworks.io>
2021-04-21 14:41:14 +0200

    Shorten GetBatches

--- api/get_batches.go
+++ api/get_batches.go
@@ -5,10 +5,5 @@ import (
 )
 
 func (a *API) GetBatches(from, to *models.Uint256) ([]models.Batch, error) {
-	batches, err := a.storage.GetBatchesInRange(from, to)
-	if err != nil {
-		return nil, err
-	}
-
-	return batches, nil
+	return a.storage.GetBatchesInRange(from, to)
 }
