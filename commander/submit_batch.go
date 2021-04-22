8852b15bcedfe733ffd2bada6b01a7885ee3a8e4
Daniel Izdebski <spidiritsu@gmail.com>
2021-04-22 15:23:54 +0200

    Add submitCreate2TransferBatch

--- commander/submit_batch.go
+++ commander/submit_batch.go
@@ -39,6 +39,31 @@ func submitTransferBatch(commitments []models.Commitment, storage *st.Storage, c
 	return nil
 }
 
+func submitCreate2TransferBatch(commitments []models.Commitment, storage *st.Storage, client *eth.Client, cfg *config.RollupConfig) error {
+	if len(commitments) < int(cfg.MinCommitmentsPerBatch) {
+		return ErrNotEnoughCommitments
+	}
+
+	batch, accountRoot, err := client.SubmitCreate2TransfersBatch(commitments)
+	if err != nil {
+		return err
+	}
+
+	err = storage.AddBatch(batch)
+	if err != nil {
+		return err
+	}
+
+	err = markCommitmentsAsIncluded(storage, commitments, &batch.Hash, accountRoot)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Submitted %d commitment(s) on chain. Batch ID: %d. Batch Hash: %v", len(commitments), batch.ID.Uint64(), batch.Hash)
+
+	return nil
+}
+
 func markCommitmentsAsIncluded(storage *st.Storage, commitments []models.Commitment, batchHash, accountRoot *common.Hash) error {
 	for i := range commitments {
 		err := storage.MarkCommitmentAsIncluded(commitments[i].ID, batchHash, accountRoot)
