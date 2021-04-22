d3d02936d3872fad3d63eeaa31e9ff2aa5491157
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-04-22 14:08:20 +0200

    Change dto batch structure

--- models/dto/batch.go
+++ models/dto/batch.go
@@ -1,8 +1,20 @@
 package dto
 
-import "github.com/Worldcoin/hubble-commander/models"
+import (
+	"github.com/Worldcoin/hubble-commander/models"
+	"github.com/Worldcoin/hubble-commander/models/enums/txtype"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+type Batch struct {
+	ID                models.Uint256
+	Hash              common.Hash
+	Type              txtype.TransactionType
+	AccountTreeRoot   *common.Hash
+	FinalisationBlock uint32
+}
 
 type BatchWithCommitments struct {
-	models.Batch
+	Batch
 	Commitments []models.Commitment
 }
