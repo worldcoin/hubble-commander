5a8183a68da4cbd9314a93a8692d8422875dbfab
Michał Sieczkowski <michal@ethworks.io>
2021-04-22 13:41:07 +0200

    Refactor functions a bit to shorten them

--- api/get_batch.go
+++ api/get_batch.go
@@ -12,15 +12,14 @@ func (a *API) GetBatchByHash(hash common.Hash) (*dto.BatchWithCommitments, error
 		return nil, err
 	}
 
-	batchWithCommitments := &dto.BatchWithCommitments{
-		Batch: *batch,
-	}
-
-	batchWithCommitments.Commitments, err = a.storage.GetCommitmentsByBatchHash(&hash)
+	commitments, err := a.storage.GetCommitmentsByBatchHash(&hash)
 	if err != nil {
 		return nil, err
 	}
-	return batchWithCommitments, nil
+	return &dto.BatchWithCommitments{
+		Batch:       *batch,
+		Commitments: commitments,
+	}, nil
 }
 
 func (a *API) GetBatchByID(id models.Uint256) (*dto.BatchWithCommitments, error) {
@@ -29,13 +28,12 @@ func (a *API) GetBatchByID(id models.Uint256) (*dto.BatchWithCommitments, error)
 		return nil, err
 	}
 
-	batchWithCommitments := &dto.BatchWithCommitments{
-		Batch: *batch,
-	}
-
-	batchWithCommitments.Commitments, err = a.storage.GetCommitmentsByBatchID(id)
+	commitments, err := a.storage.GetCommitmentsByBatchID(id)
 	if err != nil {
 		return nil, err
 	}
-	return batchWithCommitments, nil
+	return &dto.BatchWithCommitments{
+		Batch:       *batch,
+		Commitments: commitments,
+	}, nil
 }
