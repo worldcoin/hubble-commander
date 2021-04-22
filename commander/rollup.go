4ed1bf76a63bd5348f1fefeeba03bb34898f5781
Daniel Izdebski <spidiritsu@gmail.com>
2021-04-22 15:33:34 +0200

    Make rollup loop alternate between Transfer and Create2Transfer batches

--- commander/rollup.go
+++ commander/rollup.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Worldcoin/hubble-commander/config"
 	"github.com/Worldcoin/hubble-commander/eth"
+	"github.com/Worldcoin/hubble-commander/models/enums/txtype"
 	st "github.com/Worldcoin/hubble-commander/storage"
 )
 
@@ -18,12 +19,21 @@ func RollupLoop(storage *st.Storage, client *eth.Client, cfg *config.RollupConfi
 	ticker := time.NewTicker(cfg.BatchLoopInterval)
 	defer ticker.Stop()
 
+	currentBatchType := txtype.Transfer
+
 	for {
 		select {
 		case <-done:
 			return nil
 		case <-ticker.C:
-			err = createAndSubmitBatch(storage, client, cfg)
+			if currentBatchType == txtype.Transfer {
+				err = createAndSubmitTransferBatch(storage, client, cfg)
+				currentBatchType = txtype.Create2Transfer
+			} else {
+				err = createAndSubmitCreate2TransferBatch(storage, client, cfg)
+				currentBatchType = txtype.Transfer
+			}
+
 			if err != nil {
 				var e *RollupError
 				if errors.As(err, &e) {
@@ -35,7 +45,7 @@ func RollupLoop(storage *st.Storage, client *eth.Client, cfg *config.RollupConfi
 	}
 }
 
-func createAndSubmitBatch(storage *st.Storage, client *eth.Client, cfg *config.RollupConfig) (err error) {
+func createAndSubmitTransferBatch(storage *st.Storage, client *eth.Client, cfg *config.RollupConfig) (err error) {
 	tx, txStorage, err := storage.BeginTransaction()
 	if err != nil {
 		return err
@@ -53,6 +63,31 @@ func createAndSubmitBatch(storage *st.Storage, client *eth.Client, cfg *config.R
 	}
 
 	err = submitTransferBatch(commitments, txStorage, client, cfg)
+	if err != nil {
+		return err
+	}
+
+	return tx.Commit()
+}
+
+func createAndSubmitCreate2TransferBatch(storage *st.Storage, client *eth.Client, cfg *config.RollupConfig) (err error) {
+	tx, txStorage, err := storage.BeginTransaction()
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback(&err)
+
+	pendingTransfers, err := storage.GetPendingCreate2Transfers()
+	if err != nil {
+		return err
+	}
+
+	commitments, err := createCreate2TransferCommitments(pendingTransfers, txStorage, cfg)
+	if err != nil {
+		return err
+	}
+
+	err = submitCreate2TransferBatch(commitments, txStorage, client, cfg)
 	if err != nil {
 		return err
 	}
