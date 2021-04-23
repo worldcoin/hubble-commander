57bec6310ee7d839468a41cd509d659948a8500a
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-04-23 08:29:08 +0200

    Change GetCommitments storage methods

--- storage/commitment.go
+++ storage/commitment.go
@@ -82,23 +82,37 @@ func (s *Storage) GetPendingCommitments(maxFetched uint64) ([]models.Commitment,
 	return res, nil
 }
 
-func (s *Storage) GetCommitmentsByBatchHash(hash *common.Hash) ([]models.Commitment, error) {
-	res := make([]models.Commitment, 0, 32)
+func (s *Storage) GetCommitmentsByBatchHash(hash *common.Hash) ([]models.CommitmentWithTokenID, error) {
+	res := make([]models.CommitmentWithTokenID, 0, 32)
 	err := s.DB.Query(
-		s.QB.Select("*").
+		s.QB.Select("commitment.commitment_id",
+			"commitment.fee_receiver",
+			"commitment.combined_signature",
+			"commitment.post_state_root",
+			"state_leaf.token_index AS token_index",
+		).
 			From("commitment").
-			Where(squirrel.Eq{"included_in_batch": hash}),
+			Join("state_node ON lpad(state_node.merkle_path::text, 33, '0')::bit(33)::bigint = commitment.fee_receiver").
+			JoinClause("NATURAL JOIN state_leaf").
+			Where(squirrel.Eq{"commitment.included_in_batch": hash}),
 	).Into(&res)
 	return res, err
 }
 
-func (s *Storage) GetCommitmentsByBatchID(id models.Uint256) ([]models.Commitment, error) {
-	res := make([]models.Commitment, 0, 32)
+func (s *Storage) GetCommitmentsByBatchID(id models.Uint256) ([]models.CommitmentWithTokenID, error) {
+	res := make([]models.CommitmentWithTokenID, 0, 32)
 	err := s.DB.Query(
-		s.QB.Select("commitment.*").
+		s.QB.Select("commitment.commitment_id",
+			"commitment.fee_receiver",
+			"commitment.combined_signature",
+			"commitment.post_state_root",
+			"state_leaf.token_index AS token_index",
+		).
 			From("batch").
-			Join("commitment on commitment.included_in_batch = batch.batch_hash").
-			Where(squirrel.Eq{"batch_id": id}),
+			Join("commitment ON commitment.included_in_batch = batch.batch_hash").
+			Join("state_node ON lpad(state_node.merkle_path::text, 33, '0')::bit(33)::bigint = commitment.fee_receiver").
+			JoinClause("NATURAL JOIN state_leaf").
+			Where(squirrel.Eq{"batch.batch_id": id}),
 	).Into(&res)
 	return res, err
 }
