ce081fdb44cdb41548a1c13d47a05c695fb20e34
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-04-23 15:16:17 +0200

    Revert changes in TransactionBase model

--- models/transaction_base.go
+++ models/transaction_base.go
@@ -1,8 +1,6 @@
 package models
 
 import (
-	"encoding/json"
-
 	"github.com/Worldcoin/hubble-commander/models/enums/txtype"
 	"github.com/ethereum/go-ethereum/common"
 )
@@ -17,31 +15,4 @@ type TransactionBase struct {
 	Signature            []byte
 	IncludedInCommitment *int32  `db:"included_in_commitment"`
 	ErrorMessage         *string `db:"error_message"`
-}
-
-type Transaction struct {
-	Parsed interface{}
-}
-
-func (t *Transaction) Scan(src interface{}) error {
-	var rawTx struct {
-		Type *txtype.TransactionType
-	}
-	err := json.Unmarshal(bytes, &rawTx)
-	if err != nil {
-		return err
-	}
-
-	if rawTx.Type == nil {
-		return ErrMissingType
-	}
-
-	switch *rawTx.Type { // nolint:exhaustive
-	case txtype.Transfer:
-		return tx.unmarshalTransfer(bytes)
-	case txtype.Create2Transfer:
-		return tx.unmarshalCreate2Transfer(bytes)
-	default:
-		return ErrNotImplemented
-	}
 }
