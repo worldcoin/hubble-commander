95ad0d1d6e5e8963d9e3cc7d1a945e102b0f3acb
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-04-26 11:06:25 +0200

    Change API GetTransaction endpoint

--- api/get_transactions.go
+++ api/get_transactions.go
@@ -5,23 +5,38 @@ import (
 	"github.com/Worldcoin/hubble-commander/models/dto"
 )
 
-func (a *API) GetTransactions(publicKey *models.PublicKey) ([]dto.TransferReceipt, error) {
+func (a *API) GetTransactions(publicKey *models.PublicKey) ([]interface{}, error) {
 	transfers, err := a.storage.GetTransfersByPublicKey(publicKey)
 	if err != nil {
 		return nil, err
 	}
 
-	userTransfers := make([]dto.TransferReceipt, 0, len(transfers))
+	create2Transfers, err := a.storage.GetCreate2TransfersByPublicKey(publicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	userTransfers := make([]interface{}, 0, len(transfers)+len(create2Transfers))
 	for i := range transfers {
 		status, err := CalculateTransferStatus(a.storage, &transfers[i].TransactionBase, a.storage.GetLatestBlockNumber())
 		if err != nil {
 			return nil, err
 		}
-		returnTransfer := &dto.TransferReceipt{
+		userTransfers = append(userTransfers, &dto.TransferReceipt{
 			Transfer: transfers[i],
 			Status:   *status,
+		})
+	}
+
+	for i := range create2Transfers {
+		status, err := CalculateTransferStatus(a.storage, &create2Transfers[i], a.storage.GetLatestBlockNumber())
+		if err != nil {
+			return nil, err
 		}
-		userTransfers = append(userTransfers, *returnTransfer)
+		userTransfers = append(userTransfers, &dto.Create2TransferReceipt{
+			Create2Transfer: transfers[i],
+			Status:          *status,
+		})
 	}
 
 	return userTransfers, nil
