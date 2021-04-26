935375da6e9936a88648afe1c2b27e6391300c3d
Michał Sieczkowski <michal@ethworks.io>
2021-04-26 13:59:00 +0200

    Refactor and improve GetTransactions endpoint test

--- api/get_transactions_test.go
+++ api/get_transactions_test.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"math/big"
 	"testing"
 
 	"github.com/Worldcoin/hubble-commander/db"
@@ -10,7 +9,7 @@ import (
 	"github.com/Worldcoin/hubble-commander/models/enums/txstatus"
 	"github.com/Worldcoin/hubble-commander/models/enums/txtype"
 	st "github.com/Worldcoin/hubble-commander/storage"
-	"github.com/ethereum/go-ethereum/common"
+	"github.com/Worldcoin/hubble-commander/utils"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
@@ -43,142 +42,143 @@ func (s *GetTransactionsTestSuite) TearDownTest() {
 	s.NoError(err)
 }
 
-// nolint:funlen
-func (s *GetTransactionsTestSuite) TestGetTransactions() {
+func (s *GetTransactionsTestSuite) addAccounts() {
 	accounts := []models.Account{
 		{
+			PubKeyID:  0,
+			PublicKey: models.PublicKey{1, 1, 1},
+		},
+		{
 			PubKeyID:  1,
-			PublicKey: models.PublicKey{1, 2, 3},
+			PublicKey: models.PublicKey{1, 1, 1},
 		},
 		{
 			PubKeyID:  2,
-			PublicKey: models.PublicKey{2, 3, 4},
+			PublicKey: models.PublicKey{2, 2, 2},
+		},
+		{
+			PubKeyID:  3,
+			PublicKey: models.PublicKey{3, 3, 3},
 		},
 	}
 	for i := range accounts {
 		err := s.storage.AddAccountIfNotExists(&accounts[i])
 		s.NoError(err)
 	}
+}
 
-	userStates := []models.UserState{
-		{
-			PubKeyID:   accounts[0].PubKeyID,
-			TokenIndex: models.MakeUint256(1),
-			Balance:    models.MakeUint256(420),
-			Nonce:      models.MakeUint256(0),
-		},
-		{
-			PubKeyID:   accounts[1].PubKeyID,
-			TokenIndex: models.MakeUint256(2),
+func (s *GetTransactionsTestSuite) addUserStates() {
+	makeUserState := func(tokenIndex int64, pubKeyID uint32) models.UserState {
+		return models.UserState{
+			PubKeyID:   pubKeyID,
+			TokenIndex: models.MakeUint256(tokenIndex),
 			Balance:    models.MakeUint256(500),
 			Nonce:      models.MakeUint256(0),
-		},
-		{
-			PubKeyID:   accounts[0].PubKeyID,
-			TokenIndex: models.MakeUint256(25),
-			Balance:    models.MakeUint256(1),
-			Nonce:      models.MakeUint256(73),
-		},
+		}
 	}
 
-	err := s.tree.Set(0, &userStates[0])
-	s.NoError(err)
-	err = s.tree.Set(1, &userStates[1])
-	s.NoError(err)
-	err = s.tree.Set(2, &userStates[2])
-	s.NoError(err)
+	userStates := []models.UserState{
+		makeUserState(1, 0), // StateID = 0
+		makeUserState(1, 1), // StateID = 1
+		makeUserState(1, 2), // StateID = 2
+		makeUserState(2, 0), // StateID = 3
+		makeUserState(2, 2), // StateID = 4
+	}
+
+	for i := range userStates {
+		err := s.tree.Set(uint32(i), &userStates[i])
+		s.NoError(err)
+	}
+}
+
+func (s *GetTransactionsTestSuite) addTransfers() []models.Transfer {
+	makeTransfer := func(fromStateID, toStateID uint32) models.Transfer {
+		return models.Transfer{
+			TransactionBase: models.TransactionBase{
+				Hash:        utils.RandomHash(),
+				TxType:      txtype.Transfer,
+				FromStateID: fromStateID,
+				Amount:      models.MakeUint256(10),
+				Fee:         models.MakeUint256(1),
+				Nonce:       models.MakeUint256(0),
+				Signature:   []byte{1, 2, 3},
+			},
+			ToStateID: toStateID,
+		}
+	}
 
 	transfers := []models.Transfer{
-		{
-			TransactionBase: models.TransactionBase{
-				Hash:                 common.BigToHash(big.NewInt(1234)),
-				TxType:               txtype.Transfer,
-				FromStateID:          0,
-				Amount:               models.MakeUint256(1),
-				Fee:                  models.MakeUint256(5),
-				Nonce:                models.MakeUint256(0),
-				Signature:            []byte{1, 2, 3, 4, 5},
-				IncludedInCommitment: nil,
-			},
-			ToStateID: 1,
-		},
-		{
-			TransactionBase: models.TransactionBase{
-				Hash:                 common.BigToHash(big.NewInt(2345)),
-				TxType:               txtype.Transfer,
-				FromStateID:          0,
-				Amount:               models.MakeUint256(2),
-				Fee:                  models.MakeUint256(5),
-				Nonce:                models.MakeUint256(1),
-				Signature:            []byte{2, 3, 4, 5, 6},
-				IncludedInCommitment: nil,
-			},
-			ToStateID: 1,
-		},
-		{
-			TransactionBase: models.TransactionBase{
-				Hash:                 common.BigToHash(big.NewInt(3456)),
-				TxType:               txtype.Transfer,
-				FromStateID:          1,
-				Amount:               models.MakeUint256(3),
-				Fee:                  models.MakeUint256(5),
-				Nonce:                models.MakeUint256(0),
-				Signature:            []byte{3, 4, 5, 6, 7},
-				IncludedInCommitment: nil,
-			},
-			ToStateID: 0,
-		},
-		{
-			TransactionBase: models.TransactionBase{
-				Hash:                 common.BigToHash(big.NewInt(4567)),
-				TxType:               txtype.Transfer,
-				FromStateID:          0,
-				Amount:               models.MakeUint256(2),
-				Fee:                  models.MakeUint256(5),
-				Nonce:                models.MakeUint256(2),
-				Signature:            []byte{2, 3, 4, 5, 6},
-				IncludedInCommitment: nil,
-			},
-			ToStateID: 1,
-		},
+		makeTransfer(0, 2),
+		makeTransfer(1, 2),
+		makeTransfer(2, 0),
+		makeTransfer(3, 4),
+		makeTransfer(4, 3),
 	}
 
-	err = s.storage.AddTransfer(&transfers[0])
-	s.NoError(err)
-	err = s.storage.AddTransfer(&transfers[1])
-	s.NoError(err)
-	err = s.storage.AddTransfer(&transfers[2])
-	s.NoError(err)
-	err = s.storage.AddTransfer(&transfers[3])
-	s.NoError(err)
-
-	create2Transfer := models.Create2Transfer{
-		TransactionBase: models.TransactionBase{
-			Hash:        common.BigToHash(big.NewInt(1111)),
-			TxType:      txtype.Create2Transfer,
-			FromStateID: 0,
-			Amount:      models.MakeUint256(100),
-			Fee:         models.MakeUint256(5),
-			Nonce:       models.MakeUint256(1),
-		},
-		ToStateID:  1,
-		ToPubKeyID: accounts[1].PubKeyID,
+	for i := range transfers {
+		err := s.storage.AddTransfer(&transfers[i])
+		s.NoError(err)
 	}
-	err = s.storage.AddCreate2Transfer(&create2Transfer)
+	return transfers
+}
+
+func (s *GetTransactionsTestSuite) addCreate2Transfers() []models.Create2Transfer {
+	makeCreate2Transfer := func(fromStateID, toStateID, toPubKeyID uint32) models.Create2Transfer {
+		return models.Create2Transfer{
+			TransactionBase: models.TransactionBase{
+				Hash:        utils.RandomHash(),
+				TxType:      txtype.Create2Transfer,
+				FromStateID: fromStateID,
+				Amount:      models.MakeUint256(10),
+				Fee:         models.MakeUint256(1),
+				Nonce:       models.MakeUint256(1),
+				Signature:   []byte{1, 2, 3},
+			},
+			ToStateID:  toStateID,
+			ToPubKeyID: toPubKeyID,
+		}
+	}
+
+	transfers := []models.Create2Transfer{
+		makeCreate2Transfer(0, 5, 3),
+		makeCreate2Transfer(4, 6, 3),
+	}
+
+	for i := range transfers {
+		err := s.storage.AddCreate2Transfer(&transfers[i])
+		s.NoError(err)
+	}
+	return transfers
+}
+
+func (s *GetTransactionsTestSuite) TestGetTransactions() {
+	s.addAccounts()
+	s.addUserStates()
+	transfers := s.addTransfers()
+	create2Transfers := s.addCreate2Transfers()
+
+	txs, err := s.api.GetTransactions(&models.PublicKey{1, 1, 1})
 	s.NoError(err)
 
-	userTransfers, err := s.api.GetTransactions(&accounts[0].PublicKey)
-	s.NoError(err)
+	newTransferReceipt := func(transfer models.Transfer) *dto.TransferReceipt {
+		return &dto.TransferReceipt{
+			Transfer: transfer,
+			Status:   txstatus.Pending,
+		}
+	}
 
-	s.Len(userTransfers, 4)
-	s.Contains(userTransfers, &dto.TransferReceipt{
-		Transfer: transfers[0],
-		Status:   txstatus.Pending,
-	})
-	s.NotContains(userTransfers, &dto.TransferReceipt{
-		Transfer: transfers[2],
-		Status:   txstatus.Pending,
-	})
+	newCreate2Receipt := func(transfer models.Create2Transfer) *dto.Create2TransferReceipt {
+		return &dto.Create2TransferReceipt{
+			Create2Transfer: transfer,
+			Status:          txstatus.Pending,
+		}
+	}
+
+	s.Len(txs, 4)
+	s.Contains(txs, newTransferReceipt(transfers[0]))
+	s.Contains(txs, newTransferReceipt(transfers[1]))
+	s.Contains(txs, newTransferReceipt(transfers[3]))
+	s.Contains(txs, newCreate2Receipt(create2Transfers[0]))
 }
 
 func (s *GetTransactionsTestSuite) TestGetTransactions_NoTransactions() {
@@ -202,8 +202,8 @@ func (s *GetTransactionsTestSuite) TestGetTransactions_NoTransactions() {
 	userTransfers, err := s.api.GetTransactions(&account.PublicKey)
 	s.NoError(err)
 
-	s.Len(userTransfers, 0)
 	s.NotNil(userTransfers)
+	s.Len(userTransfers, 0)
 }
 
 func TestGetTransactionsTestSuite(t *testing.T) {
