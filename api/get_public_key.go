45f99348ef8a49bfd345b9783fdc019413bea11d
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-04-29 16:16:45 +0200

    Add GetPublicKeyByID API endpoint

--- api/get_public_key.go
+++ api/get_public_key.go
@@ -2,6 +2,6 @@ package api
 
 import "github.com/Worldcoin/hubble-commander/models"
 
-func (a *API) GetPublicKeyByID(id uint32) (*models.Account, error) {
-	return nil, nil
+func (a *API) GetPublicKeyByID(id uint32) (*models.PublicKey, error) {
+	return a.storage.GetPublicKey(id)
 }
