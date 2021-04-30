b0ab563aa04c2f5e54ec26d29760faf21a35976f
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-04-30 09:03:09 +0200

    Add GetUserState API endpoint

--- api/get_user_state.go
+++ api/get_user_state.go
@@ -1,1 +1,7 @@
 package api
+
+import "github.com/Worldcoin/hubble-commander/models"
+
+func (a *API) GetUserState(id uint32) (*models.UserStateWithID, error) {
+	return a.storage.GetUserStateByID(id)
+}
