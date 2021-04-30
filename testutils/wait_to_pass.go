d11bc40c210114fd9e35621cbb52cd61e406158a
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-04-30 13:47:02 +0200

    Change tryInterval to public const

--- testutils/wait_to_pass.go
+++ testutils/wait_to_pass.go
@@ -5,14 +5,14 @@ import (
 	"time"
 )
 
-var tryInterval = 250 * time.Millisecond
+const TryInterval = 250 * time.Millisecond
 
 func WaitToPass(t *testing.T, fn func() bool, timeout time.Duration) {
 	immediately := make(chan struct{}, 1)
 	immediately <- struct{}{}
 	defer close(immediately)
 
-	ticker := time.NewTicker(tryInterval)
+	ticker := time.NewTicker(TryInterval)
 	defer ticker.Stop()
 
 	timeoutC := time.After(timeout)
