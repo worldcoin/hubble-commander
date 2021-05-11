ef0f8c7b33d6af40648a103f3e55124e6c7eef8f
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-05-11 07:19:49 +0200

    Add domain to storage

--- storage/chain_state.go
+++ storage/chain_state.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"github.com/Masterminds/squirrel"
+	"github.com/Worldcoin/hubble-commander/bls"
 	"github.com/Worldcoin/hubble-commander/models"
 )
 
@@ -28,4 +29,24 @@ func (s *Storage) SetChainState(chainState *models.ChainState) error {
 			Values(chainState.ChainID, chainState.AccountRegistry, chainState.Rollup),
 	).Exec()
 	return err
+}
+
+func (s *Storage) GetDomain(chainID models.Uint256) (*bls.Domain, error) {
+	if s.domain != nil {
+		return s.domain, nil
+	}
+
+	res := make([]bls.Domain, 0, 1)
+	err := s.DB.Query(
+		s.QB.Select("rollup").
+			From("chain_state").
+			Where(squirrel.Eq{"chain_id": chainID}),
+	).Into(&res)
+	if err != nil {
+		return nil, err
+	}
+	if len(res) == 0 {
+		return nil, NewNotFoundError("domain")
+	}
+	return &res[0], nil
 }
