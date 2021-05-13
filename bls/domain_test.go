8b9947de3dd7ff03101f6e17f9433dd440ed9174
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-05-13 12:47:55 +0200

    Add test for Domain marshaller

--- bls/domain_test.go
+++ bls/domain_test.go
@@ -1,6 +1,9 @@
 package bls
 
 import (
+	"encoding/json"
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/ethereum/go-ethereum/crypto"
@@ -19,4 +22,12 @@ func TestDomainFromBytes_InvalidLength(t *testing.T) {
 	domain, err := DomainFromBytes(data)
 	require.Equal(t, ErrInvalidDomainLength, err)
 	require.Nil(t, domain)
+}
+
+func TestMarshalText(t *testing.T) {
+	domain := Domain{1, 2, 3}
+	expected := fmt.Sprintf("\"0x010203%s\"", strings.Repeat("0", DomainLength*2-6))
+	data, err := json.Marshal(&domain)
+	require.NoError(t, err)
+	require.Equal(t, expected, string(data))
 }
