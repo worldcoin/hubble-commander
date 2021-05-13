2dd233ed522ac7ca70451cedd1f73d97f9e0f901
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-05-13 15:04:40 +0200

    Add ToPublicKey to Create2Transfer model

--- db/postgres/migrations/000005_create2transfer.up.sql
+++ db/postgres/migrations/000005_create2transfer.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE create2transfer (
     tx_hash       BYTEA PRIMARY KEY REFERENCES transaction_base,
     to_state_id   BIGINT NOT NULL,
-    to_pub_key_id BIGINT NOT NULL
+    to_pub_key_id BIGINT NOT NULL,
+    to_public_key BYTEA
 );
 
--- models/create2transfer.go
+++ models/create2transfer.go
@@ -2,8 +2,9 @@ package models
 
 type Create2Transfer struct {
 	TransactionBase
-	ToStateID  uint32 `db:"to_state_id"`
-	ToPubKeyID uint32 `db:"to_pub_key_id"`
+	ToStateID   uint32     `db:"to_state_id"`
+	ToPubKeyID  uint32     `db:"to_pub_key_id"`
+	ToPublicKey *PublicKey `db:"to_public_key"`
 }
 
 type Create2TransferForCommitment struct {
