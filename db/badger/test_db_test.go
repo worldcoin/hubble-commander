508ef7d7a12ee627cc1fa99620c26ea0f2316591
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-05-18 12:04:54 +0200

    Add prune function to badger db

--- db/badger/test_db_test.go
+++ db/badger/test_db_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	bh "github.com/timshannon/badgerhold/v3"
 )
 
 type someStruct struct {
@@ -29,4 +30,25 @@ func TestNewTestDB(t *testing.T) {
 	require.NoError(t, err)
 
 	require.Equal(t, value, res)
+}
+
+func TestPrune(t *testing.T) {
+	bdg, err := NewTestDB()
+	require.NoError(t, err)
+
+	key := "Duck"
+	value := someStruct{
+		Name: key,
+		Age:  4,
+	}
+
+	err = bdg.DB.Insert(key, value)
+	require.NoError(t, err)
+
+	err = bdg.DB.Prune()
+	require.NoError(t, err)
+
+	var res someStruct
+	err = bdg.DB.Get(key, &res)
+	require.Equal(t, bh.ErrNotFound, err)
 }
