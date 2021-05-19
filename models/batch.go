ad3be94d995017806f0052135e2f1c601571e729
Bartlomiej Tarczynski <51199571+b-tarczynski@users.noreply.github.com>
2021-05-19 13:33:39 +0200

    Add missing indexes for postgres (#140)
    * Add missing indexes for postgres columns
    
    * Remove TODOs
    
    Co-authored-by: Michał Sieczkowski <michal@ethworks.io>

--- models/batch.go
+++ models/batch.go
@@ -8,7 +8,7 @@ import (
 type Batch struct {
 	Hash              common.Hash `db:"batch_hash"` // root of tree containing all commitments included in this batch
 	Type              txtype.TransactionType
-	ID                Uint256 `db:"batch_id"`           // TODO consider adding an index
+	ID                Uint256 `db:"batch_id"`
 	FinalisationBlock uint32  `db:"finalisation_block"` // nolint:misspell
 }
 
