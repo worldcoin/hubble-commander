6f8fee65f3ea012c3c62cdf1c36b1ca18a7bdd63
Daniel Izdebski <spidiritsu@gmail.com>
2021-05-24 16:54:07 +0200

    Add custom encoding for uint32 type (#152)
    * Add custom encoding for uint32
    
    * Move GetNextAvailableStateID to state_leaf.go
    
    * Don't recreate Postgres db in tests that don't need it
    
    * Pass state node merkle path by value
    
    Co-authored-by: Michał Sieczkowski <michal@ethworks.io>

--- storage/state_leaf_test.go
+++ storage/state_leaf_test.go
@@ -248,6 +248,48 @@ func (s *StateLeafTestSuite) TestGetFeeReceiverStateLeaf_WorkWithCachedValue() {
 	s.Equal(uint32(1), stateLeaf.StateID)
 }
 
+func (s *StateLeafTestSuite) TestGetNextAvailableStateID_NoLeavesInStateTree() {
+	path, err := s.storage.GetNextAvailableStateID()
+	s.NoError(err)
+	s.Equal(uint32(0), *path)
+}
+
+func (s *StateLeafTestSuite) TestGetNextAvailableStateID_OneBytes() {
+	err := s.storage.AddAccountIfNotExists(&account1)
+	s.NoError(err)
+	err = s.storage.AddAccountIfNotExists(&account2)
+	s.NoError(err)
+
+	tree := NewStateTree(s.storage.Storage)
+
+	err = tree.Set(0, userState1)
+	s.NoError(err)
+	err = tree.Set(2, userState2)
+	s.NoError(err)
+
+	path, err := s.storage.GetNextAvailableStateID()
+	s.NoError(err)
+	s.Equal(uint32(3), *path)
+}
+
+func (s *StateLeafTestSuite) TestGetNextAvailableStateID_TwoBytes() {
+	err := s.storage.AddAccountIfNotExists(&account1)
+	s.NoError(err)
+	err = s.storage.AddAccountIfNotExists(&account2)
+	s.NoError(err)
+
+	tree := NewStateTree(s.storage.Storage)
+
+	err = tree.Set(0, userState1)
+	s.NoError(err)
+	err = tree.Set(13456, userState2)
+	s.NoError(err)
+
+	path, err := s.storage.GetNextAvailableStateID()
+	s.NoError(err)
+	s.Equal(uint32(13457), *path)
+}
+
 func TestStateLeafTestSuite(t *testing.T) {
 	suite.Run(t, new(StateLeafTestSuite))
 }
