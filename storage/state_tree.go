eac6ce7cb5ca0ba4a129d061758b3a08e4e2dd43
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-05-25 14:44:12 +0200

    Remove unused decodeKey function

--- storage/state_tree.go
+++ storage/state_tree.go
@@ -1,8 +1,6 @@
 package storage
 
 import (
-	"bytes"
-	"encoding/gob"
 	"fmt"
 	"math/big"
 	"reflect"
@@ -139,11 +137,6 @@ func (s *StateTree) RevertTo(targetRootHash common.Hash) error {
 		return ErrNotExistentState
 	}
 	return txn.Commit()
-}
-
-func decodeKey(data []byte, key interface{}, prefix []byte) error {
-	return gob.NewDecoder(bytes.NewReader(data[len(prefix):])).
-		Decode(key)
 }
 
 func decodeStateUpdate(item *bdg.Item) (*models.StateUpdate, error) {
