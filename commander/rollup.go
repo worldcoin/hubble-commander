60828a77ee505947d3c38376489dc1bdaa032410
Dmytro Maretskyi <maretskii@gmail.com>
2021-05-25 15:52:36 +0200

    Introduce transactionExecutor struct

--- commander/rollup.go
+++ commander/rollup.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Worldcoin/hubble-commander/bls"
 	"github.com/Worldcoin/hubble-commander/config"
+	"github.com/Worldcoin/hubble-commander/db"
 	"github.com/Worldcoin/hubble-commander/eth"
 	"github.com/Worldcoin/hubble-commander/models"
 	"github.com/Worldcoin/hubble-commander/models/enums/txtype"
@@ -30,63 +31,86 @@ func (c *Commander) rollupLoop() (err error) {
 				}
 			}
 
+			var transactionExecutor *transactionExecutor
+			transactionExecutor, err = newTransactionExecutor(c.storage, c.client, &c.cfg.Rollup)
+			if err != nil {
+				return err
+			}
+
 			if currentBatchType == txtype.Transfer {
-				err = createAndSubmitBatch(currentBatchType, c.storage, c.client, &c.cfg.Rollup)
+				err = transactionExecutor.CreateAndSubmitBatch(currentBatchType)
 				currentBatchType = txtype.Create2Transfer
 			} else {
-				err = createAndSubmitBatch(currentBatchType, c.storage, c.client, &c.cfg.Rollup)
+				err = transactionExecutor.CreateAndSubmitBatch(currentBatchType)
 				currentBatchType = txtype.Transfer
 			}
 
 			if err != nil {
+				transactionExecutor.Rollback(&err)
+
 				var e *RollupError
 				if errors.As(err, &e) {
 					continue
 				}
 				return err
 			}
+
+			err = transactionExecutor.Commit()
+			if err != nil {
+				return err
+			}
 		}
 	}
 }
 
-func createAndSubmitBatch(batchType txtype.TransactionType, storage *st.Storage, client *eth.Client, cfg *config.RollupConfig) (err error) {
-	tx, txStorage, err := storage.BeginTransaction(st.TxOptions{Postgres: true, Badger: true})
-	if err != nil {
-		return err
-	}
-	defer tx.Rollback(&err)
-
-	err = unsafeCreateAndSubmitBatch(batchType, txStorage, client, cfg)
-	if err != nil {
-		return err
-	}
-
-	return tx.Commit()
+// transactionExecutor executes transactions & syncs batches. Manages a database transaction.
+type transactionExecutor struct {
+	cfg     *config.RollupConfig
+	storage *st.Storage
+	tx      *db.TxController
+	client  *eth.Client
 }
 
-func unsafeCreateAndSubmitBatch(
-	batchType txtype.TransactionType,
-	storage *st.Storage,
-	client *eth.Client,
-	cfg *config.RollupConfig,
-) (err error) {
+func newTransactionExecutor(storage *st.Storage, client *eth.Client, cfg *config.RollupConfig) (*transactionExecutor, error) {
+	tx, txStorage, err := storage.BeginTransaction(st.TxOptions{Postgres: true, Badger: true})
+	if err != nil {
+		return nil, err
+	}
+
+	return &transactionExecutor{
+		cfg:     cfg,
+		storage: txStorage,
+		tx:      tx,
+		client:  client,
+	}, nil
+}
+
+func (t *transactionExecutor) Commit() error {
+	return t.tx.Commit()
+}
+
+func (t *transactionExecutor) Rollback(cause *error) {
+	t.tx.Rollback(cause)
+}
+
+func (t *transactionExecutor) CreateAndSubmitBatch(batchType txtype.TransactionType) (err error) {
 	var commitments []models.Commitment
 
-	domain, err := storage.GetDomain(client.ChainState.ChainID)
+	domain, err := t.storage.GetDomain(t.client.ChainState.ChainID)
 	if err != nil {
 		return err
 	}
 
 	if batchType == txtype.Transfer {
-		commitments, err = buildTransferCommitments(storage, cfg, *domain)
+		commitments, err = buildTransferCommitments(t.storage, t.cfg, *domain)
 	} else {
-		commitments, err = buildCreate2TransfersCommitments(storage, client, cfg, *domain)
+		commitments, err = buildCreate2TransfersCommitments(t.storage, t.client, t.cfg, *domain)
 	}
 	if err != nil {
 		return err
 	}
 
-	err = submitBatch(batchType, commitments, storage, client, cfg)
+	err = submitBatch(batchType, commitments, t.storage, t.client, t.cfg)
 	if err != nil {
 		return err
 	}
