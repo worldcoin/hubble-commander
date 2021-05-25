6722ffe55631b9ff6ff40189916b5f0e3ffa5bc1
philsippl <philsippl@gmail.com>
2021-05-25 10:05:29 +0200

    added create2 (#155)

--- sdk/main.go
+++ sdk/main.go
@@ -90,13 +90,54 @@ func SignTransfer(from, to C.uint, amount, fee, nonce, privateKey, domain *C.cha
 	nonceBigInt := new(big.Int)
 	nonceBigInt.SetString(C.GoString(nonce), 10)
 
-	transfer, _ := api.SignTransfer(wallet, dto.Transfer{
+	transfer, err := api.SignTransfer(wallet, dto.Transfer{
 		FromStateID: ref.Uint32(uint32(from)),
 		ToStateID:   ref.Uint32(uint32(to)),
 		Amount:      models.NewUint256FromBig(*amountBigInt),
 		Fee:         models.NewUint256FromBig(*feeBigInt),
 		Nonce:       models.NewUint256FromBig(*nonceBigInt),
 	})
+	if err != nil {
+		return nil
+	}
+
+	return C.CString(hex.EncodeToString(transfer.Signature.Bytes()))
+}
+
+//export SignCreate2Transfer
+func SignCreate2Transfer(from C.uint, toPubKey, amount, fee, nonce, privateKey, domain *C.char) *C.char {
+	wallet, err := parseWallet(privateKey, domain)
+	if err != nil {
+		return nil
+	}
+
+	amountBigInt := new(big.Int)
+	amountBigInt.SetString(C.GoString(amount), 10)
+
+	feeBigInt := new(big.Int)
+	feeBigInt.SetString(C.GoString(fee), 10)
+
+	nonceBigInt := new(big.Int)
+	nonceBigInt.SetString(C.GoString(nonce), 10)
+
+	toPublicKeyBytes, err := hex.DecodeString(C.GoString(toPubKey))
+	if err != nil {
+		return nil
+	}
+
+	var toPublicKey models.PublicKey
+	copy(toPublicKey[:], toPublicKeyBytes)
+
+	transfer, err := api.SignCreate2Transfer(wallet, dto.Create2Transfer{
+		FromStateID: ref.Uint32(uint32(from)),
+		ToPublicKey: &toPublicKey,
+		Amount:      models.NewUint256FromBig(*amountBigInt),
+		Fee:         models.NewUint256FromBig(*feeBigInt),
+		Nonce:       models.NewUint256FromBig(*nonceBigInt),
+	})
+	if err != nil {
+		return nil
+	}
 
 	return C.CString(hex.EncodeToString(transfer.Signature.Bytes()))
 }
