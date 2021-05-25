c0b6f839d21ed48659c28128d20ec696c590c6f7
Dmytro Maretskyi <maretskii@gmail.com>
2021-05-25 12:56:45 +0200

    Parallelize tx submission in bench

--- e2e/bench_test.go
+++ e2e/bench_test.go
@@ -5,6 +5,8 @@ package e2e
 import (
 	"fmt"
 	"math/rand"
+	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -38,24 +40,6 @@ func TestBenchCommander(t *testing.T) {
 	require.Equal(t, config.GetConfig().API.Version, version)
 
 	stateIds := make([]uint32, 0)
-	nonces := map[uint32]*models.Uint256{}
-	walletForState := map[uint32]bls.Wallet{}
-	txsToWatch := map[uint32][]common.Hash{}
-
-	for i := 0; i < 6; i++ {
-		var userStates []dto.UserState
-		err = commander.Client().CallFor(&userStates, "hubble_getUserStates", []interface{}{wallets[i].PublicKey()})
-		require.NoError(t, err)
-		for _, state := range userStates {
-			stateIds = append(stateIds, state.StateID)
-			nonces[state.StateID] = models.NewUint256(0)
-			walletForState[state.StateID] = wallets[i]
-			txsToWatch[state.StateID] = make([]common.Hash, 0)
-		}
-	}
-
-	transfersSent := 0
-	startTime := time.Now()
 
 	// Total number of transactions to be sent.
 	const txCount = 10_000
@@ -66,44 +50,44 @@ func TestBenchCommander(t *testing.T) {
 	// Maximum number of tx batches in queue.
 	const maxQueuedBatchCount = 20
 
-	for transfersSent < txCount {
-		txSent := true
-		for txSent {
-			txSent = false
+	// Only use atomic operations to increment those two counters.
+	transfersSent := int64(0)
+	txsQueued := int64(0)
 
-			for _, stateId := range stateIds {
-				if len(txsToWatch[stateId]) > maxQueuedBatchCount { // max txs in queue
-					continue
-				}
+	startTime := time.Now()
 
-				var hash *common.Hash
+	var waitGroup sync.WaitGroup
+
+	runForWallet := func(wallet bls.Wallet, stateId uint32) {
+		fmt.Printf("Starting worker on stateId %d address=%s\n", stateId, wallet.PublicKey().String())
+
+		txsToWatch := make([]common.Hash, 0, maxQueuedBatchCount)
+		nonce := models.MakeUint256(0)
+
+		for transfersSent < txCount {
+
+			// Send phase
+			for len(txsToWatch) <= maxQueuedBatchCount {
+				var hash common.Hash
 				for i := 0; i < txBatchSize; i++ {
-					wallet := walletForState[stateId]
-					nonce := nonces[stateId]
-					to := stateId
 
 					// Pick random receiver id thats different from sender's.
+					to := stateIds[rand.Intn(len(stateIds))]
 					for to == stateId {
 						to = stateIds[rand.Intn(len(stateIds))]
 					}
 
-					hash = sendTransfer(t, commander, wallet, stateId, to, *nonce)
-					if hash != nil {
-						nonces[stateId] = nonces[stateId].AddN(1)
-						txSent = true
-					}
+					hash = sendTransfer(t, commander, wallet, stateId, to, nonce)
+					nonce = *nonce.AddN(1)
 				}
-				if hash != nil {
-					txsToWatch[stateId] = append(txsToWatch[stateId], *hash)
-				}
+				txsToWatch = append(txsToWatch, hash)
+				atomic.AddInt64(&txsQueued, txBatchSize)
 			}
-		}
 
-		txInQueue := 0
-		for _, stateId := range stateIds {
+			// Check phase
 			newTxsToWatch := make([]common.Hash, 0)
 			skip := false
-			for _, tx := range txsToWatch[stateId] {
+			for _, tx := range txsToWatch {
 				var sentTransfer dto.TransferReceipt
 				if !skip {
 					err = commander.Client().CallFor(&sentTransfer, "hubble_getTransaction", []interface{}{tx})
@@ -111,20 +95,45 @@ func TestBenchCommander(t *testing.T) {
 				}
 				if skip || sentTransfer.Status == txstatus.Pending {
 					newTxsToWatch = append(newTxsToWatch, tx)
-					txInQueue += txBatchSize
 					skip = true
 				} else {
-					transfersSent += txBatchSize
+					atomic.AddInt64(&transfersSent, txBatchSize)
+					atomic.AddInt64(&txsQueued, -txBatchSize)
+
 				}
 			}
-			txsToWatch[stateId] = newTxsToWatch
+			txsToWatch = newTxsToWatch
+
+			// Report phase
+			fmt.Printf("Transfers sent: %d, throughput: %f tx/s, txs in queue: %d\n", transfersSent, float64(transfersSent)/(time.Since(startTime).Seconds()), txsQueued)
 		}
 
-		fmt.Printf("Transfers sent: %d, throughput: %f tx/s, txs in queue: %d\n", transfersSent, float64(transfersSent)/(time.Since(startTime).Seconds()), txInQueue)
+		waitGroup.Done()
 	}
+
+	for _, wallet := range wallets {
+		var userStates []dto.UserState
+		err = commander.Client().CallFor(&userStates, "hubble_getUserStates", []interface{}{wallet.PublicKey()})
+		if err != nil {
+			continue
+		}
+
+		fmt.Printf("%d states found for wallet %s\n", len(userStates), wallet.PublicKey().String())
+
+		for _, state := range userStates {
+			stateIds = append(stateIds, state.StateID)
+
+			waitGroup.Add(1)
+			go runForWallet(wallet, state.StateID)
+		}
+	}
+
+	require.Greater(t, len(stateIds), 0)
+
+	waitGroup.Wait()
 }
 
-func sendTransfer(t *testing.T, commander Commander, wallet bls.Wallet, from uint32, to uint32, nonce models.Uint256) *common.Hash {
+func sendTransfer(t *testing.T, commander Commander, wallet bls.Wallet, from uint32, to uint32, nonce models.Uint256) common.Hash {
 	transfer, err := api.SignTransfer(&wallet, dto.Transfer{
 		FromStateID: &from,
 		ToStateID:   &to,
@@ -139,5 +148,5 @@ func sendTransfer(t *testing.T, commander Commander, wallet bls.Wallet, from uin
 	require.NoError(t, err)
 	require.NotNil(t, transferHash)
 
-	return &transferHash
+	return transferHash
 }
