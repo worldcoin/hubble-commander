91e370769fea027c72c4afd9a218f7de8f241248
Bartlomiej Tarczynski <51199571+b-tarczynski@users.noreply.github.com>
2021-05-25 16:28:49 +0200

    Add custom uint64 encoder (#158)
    * Add uint64 encoder and decoder
    
    * Move DecodeKey function to badger package
    
    * Remove unused decodeKey function

--- db/badger/encoder_test.go
+++ db/badger/encoder_test.go
@@ -27,3 +27,28 @@ func TestUint32_ByteEncoding(t *testing.T) {
 	_ = DecodeUint32(encodedDataHash, &decodedNumber)
 	require.Equal(t, number, decodedNumber)
 }
+
+func TestUint64_ByteEncoding(t *testing.T) {
+	value := uint64(123456789)
+
+	encoded, err := EncodeUint64(&value)
+	require.NoError(t, err)
+
+	var decoded uint64
+	err = DecodeUint64(encoded, &decoded)
+	require.NoError(t, err)
+	require.Equal(t, value, decoded)
+}
+
+func TestDecodeKey(t *testing.T) {
+	prefix := []byte("bh_prefix")
+	value := uint64(123456789)
+
+	encoded, err := EncodeUint64(&value)
+	require.NoError(t, err)
+
+	var decoded uint64
+	err = DecodeKey(append(prefix, encoded...), &decoded, prefix)
+	require.NoError(t, err)
+	require.Equal(t, value, decoded)
+}
