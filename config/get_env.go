12a0f63221de40d8ae522d8d7d40b3ec090a60ef
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-05-27 10:40:07 +0200

    Remov unused getEnv function

--- config/get_env.go
+++ config/get_env.go
@@ -1,19 +1,10 @@
 package config
 
 import (
-	"log"
 	"os"
 
 	"github.com/spf13/viper"
 )
-
-func getEnv(key string) string {
-	value := os.Getenv(key)
-	if value == "" {
-		log.Fatalf("%s env var not specified", key)
-	}
-	return value
-}
 
 func getEnvOrDefault(key string, fallback *string) *string {
 	value := os.Getenv(key)
