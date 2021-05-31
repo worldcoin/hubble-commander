a20193101d6f3ed766c961aeebb4a79beb0a260e
Michał Sieczkowski <michal@ethworks.io>
2021-05-31 12:42:57 +0200

    Merge 14th migration file into 7th

--- db/postgres/migrations/000007_chain_state.up.sql
+++ db/postgres/migrations/000007_chain_state.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE chain_state (
     chain_id         NUMERIC(78) PRIMARY KEY,
     account_registry BYTEA NOT NULL,
-    rollup           BYTEA NOT NULL
+    rollup           BYTEA NOT NULL,
+    genesis_accounts JSON  NOT NULL
 );
--- db/postgres/migrations/000014_chain_state_add_genesis_accounts.up.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE chain_state
-    ADD COLUMN genesis_accounts JSON NOT NULL;
