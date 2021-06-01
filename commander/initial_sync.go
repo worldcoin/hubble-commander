c4d624c7c10a1d2b02ca2d2ee09fb5a6084bb7b5
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-06-01 09:00:28 +0200

    Process initial sync in pararell

--- commander/initial_sync.go
+++ commander/initial_sync.go
@@ -1,9 +1,13 @@
 package commander
 
 import (
+	"context"
 	"log"
 
+	"github.com/Worldcoin/hubble-commander/utils/ref"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/pkg/errors"
+	"golang.org/x/sync/errgroup"
 )
 
 func (c *Commander) InitialSync() error {
@@ -17,41 +21,75 @@ func (c *Commander) InitialSync() error {
 	if err != nil {
 		return err
 	}
-	startBlock := *syncedBlock
-	endBlock := startBlock + uint64(c.cfg.Rollup.SyncSize)
 
-	for endBlock != uint64(*latestBlockNumber) {
-		//TODO: process accounts and batches parallel
-		err = c.RegisterAccounts(&bind.FilterOpts{
-			Start: startBlock,
-			End:   &endBlock,
-		})
-		if err != nil {
-			return err
-		}
+	group, ctx := errgroup.WithContext(context.Background())
+	boundaries := make(chan uint64)
 
-		err = c.SyncBatches(false, &endBlock)
-		if err != nil {
-			return err
-		}
-
-		err = c.storage.SetSyncedBlock(c.client.ChainState.ChainID, endBlock)
-		if err != nil {
-			return err
-		}
-
-		startBlock = endBlock
-		endBlock += uint64(c.cfg.Rollup.SyncSize)
-		if endBlock > uint64(*latestBlockNumber) {
-			endBlock = uint64(*latestBlockNumber)
-		}
+	group.Go(func() error {
+		return c.initialSyncAccounts(ctx, boundaries, *syncedBlock, uint64(*latestBlockNumber))
+	})
+	group.Go(func() error {
+		return c.initialSyncBatches(ctx, boundaries)
+	})
+	err = group.Wait()
+	if err != nil {
+		return errors.WithStack(err)
 	}
 
 	log.Println("Finished initial syncing")
 	return nil
 }
 
-func (c *Commander) RegisterAccounts(opts *bind.FilterOpts) error {
+func (c *Commander) initialSyncAccounts(ctx context.Context, boundaries chan<- uint64, startBlock, latestBlock uint64) error {
+	defer close(boundaries)
+	opts := &bind.FilterOpts{
+		Start: startBlock,
+		End:   ref.Uint64(startBlock + uint64(c.cfg.Rollup.SyncSize)),
+	}
+	for *opts.End != latestBlock {
+		err := c.registerAccounts(opts)
+		if err != nil {
+			return err
+		}
+		opts.Start = *opts.End
+		*opts.End = calculateEndBlock(*opts.End, latestBlock, c.cfg.Rollup.SyncSize)
+		boundaries <- *opts.End
+		select {
+		case <-ctx.Done():
+			return nil
+		default:
+			continue
+		}
+	}
+	return nil
+}
+
+func calculateEndBlock(endBlock, latestBlock uint64, syncSize uint32) uint64 {
+	endBlock += uint64(syncSize)
+	if endBlock > latestBlock {
+		return latestBlock
+	}
+	return endBlock
+}
+
+func (c *Commander) initialSyncBatches(ctx context.Context, boundaries <-chan uint64) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+		case endBlock, ok := <-boundaries:
+			if !ok {
+				return nil
+			}
+			err := c.SyncBatches(false, &endBlock)
+			if err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func (c *Commander) registerAccounts(opts *bind.FilterOpts) error {
 	it, err := c.client.AccountRegistry.FilterPubkeyRegistered(opts)
 	if err != nil {
 		return err
