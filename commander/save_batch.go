8ef9ee0a593fea98455ea636933574575e3079e8
Michał Sieczkowski <michal@ethworks.io>
2021-06-01 15:26:07 +0200

    Refactor saveBatch

--- commander/save_batch.go
+++ commander/save_batch.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Worldcoin/hubble-commander/eth"
 	st "github.com/Worldcoin/hubble-commander/storage"
+	"github.com/ethereum/go-ethereum"
 )
 
 func saveBatch(storage *st.Storage, client *eth.Client) error {
@@ -17,50 +18,51 @@ func saveBatch(storage *st.Storage, client *eth.Client) error {
 	}
 
 	batchTxReceipt, err := client.ChainConnection.GetBackend().TransactionReceipt(context.Background(), pendingBatch.TransactionHash)
+	if err == ethereum.NotFound {
+		// TODO - Have a discussion with the team on how to handle the situation when the sent transaction is stuck in the mempool
+		return nil
+	}
 	if err != nil {
 		return err
 	}
 
-	if batchTxReceipt != nil {
-		if batchTxReceipt.Status == 1 {
-			submissionBlock, latestBatchNumber, err := getLatestSubmissionBlockAndBatchNumber(storage, client)
-			if err != nil {
-				return err
-			}
-			newBatches, err := client.GetBatches(submissionBlock)
-			if err != nil {
-				return err
-			}
+	if batchTxReceipt.Status != 1 {
+		// TODO - Have a discussion with the team on how to handle the situation if transaction was mined unsuccessfully
+		return nil
+	}
 
-			for i := range newBatches {
-				batch := &newBatches[i]
-				if batch.Number.Cmp(latestBatchNumber) <= 0 {
-					continue
-				}
+	submissionBlock, latestBatchNumber, err := getLatestSubmissionBlockAndBatchNumber(storage, client)
+	if err != nil {
+		return err
+	}
+	newBatches, err := client.GetBatches(submissionBlock)
+	if err != nil {
+		return err
+	}
 
-				batch.TransactionHash = pendingBatch.TransactionHash
-				err := storage.MarkBatchAsSubmitted(&batch.Batch)
-				if err != nil {
-					return err
-				}
-
-				err = storage.UpdateCommitmentsAccountTreeRoot(batch.Batch.TransactionHash, batch.AccountRoot)
-				if err != nil {
-					return err
-				}
-
-				log.Printf(
-					"Submitted %d commitment(s) on chain. Batch ID: %d. Batch Hash: %v",
-					len(batch.Commitments),
-					batch.Number.Uint64(),
-					batch.Hash,
-				)
-			}
-		} else { // nolint:staticcheck
-			// TODO - Have a discussion with the team on how to handle the situation if transaction was mined unsuccessfully
+	for i := range newBatches {
+		batch := &newBatches[i]
+		if batch.Number.Cmp(latestBatchNumber) <= 0 {
+			continue
 		}
-	} else { // nolint:staticcheck
-		// TODO - Have a discussion with the team on how to handle the situation when the sent transaction is stuck in the mempool
+
+		batch.TransactionHash = pendingBatch.TransactionHash
+		err := storage.MarkBatchAsSubmitted(&batch.Batch)
+		if err != nil {
+			return err
+		}
+
+		err = storage.UpdateCommitmentsAccountTreeRoot(batch.Batch.TransactionHash, batch.AccountRoot)
+		if err != nil {
+			return err
+		}
+
+		log.Printf(
+			"Submitted %d commitment(s) on chain. Batch ID: %d. Batch Hash: %v",
+			len(batch.Commitments),
+			batch.Number.Uint64(),
+			batch.Hash,
+		)
 	}
 
 	return nil
