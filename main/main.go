cfbd377d85c827fda4ba6756a959996f57ddf63d
Daniel Izdebski <spidiritsu@gmail.com>
2021-06-09 13:31:06 +0200

    Make os.Signal channel buffered

--- main/main.go
+++ main/main.go
@@ -39,7 +39,7 @@ func getConfig() *config.Config {
 }
 
 func setupCloseHandler(cmd *commander.Commander) {
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
