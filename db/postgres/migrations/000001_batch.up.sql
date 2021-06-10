a43c807d52eaf0974e8a5e344fa4c5b8250c40b1
Michał Sieczkowski <michal@ethworks.io>
2021-06-10 21:15:05 +0200

    Squash account_tree_root migration file

--- db/postgres/migrations/000001_batch.up.sql
+++ db/postgres/migrations/000001_batch.up.sql
@@ -4,5 +4,6 @@ CREATE TABLE batch (
     transaction_hash   BYTEA       NOT NULL,
     batch_hash         BYTEA,
     batch_number       NUMERIC(78) NOT NULL,
-    finalisation_block BIGINT
+    finalisation_block BIGINT,
+    account_tree_root  BYTEA
 );
--- db/postgres/migrations/000012_account_root.down.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE batch DROP account_tree_root;
-ALTER TABLE commitment ADD COLUMN account_tree_root BYTEA;
