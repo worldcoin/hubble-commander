ffae3f577171a8c7e2f888ef9c7adc5a6b284637
Daniel Izdebski <spidiritsu@gmail.com>
2021-06-11 13:40:14 +0200

    Update migrations files

--- db/postgres/migrations/000002_commitment.up.sql
+++ db/postgres/migrations/000002_commitment.up.sql
@@ -5,5 +5,5 @@ CREATE TABLE commitment (
     fee_receiver       BIGINT   NOT NULL, -- state id of fee receiver
     combined_signature BYTEA    NOT NULL,
     post_state_root    BYTEA    NOT NULL,
-    included_in_batch  INT REFERENCES batch
+    included_in_batch  NUMERIC(78) REFERENCES batch
 );
