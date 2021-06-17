ae67702812a6e21e806d56204f8e71f43d3614f4
Michał Sieczkowski <michal@ethworks.io>
2021-06-17 14:41:37 +0200

    Cache AccountRegistryABI in client

--- commander/accounts.go
+++ commander/accounts.go
@@ -4,21 +4,12 @@ import (
 	"bytes"
 	"context"
 	"math/big"
-	"strings"
 
-	"github.com/Worldcoin/hubble-commander/contracts/accountregistry"
 	"github.com/Worldcoin/hubble-commander/models"
-	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
-	"github.com/pkg/errors"
 )
 
 func (c *Commander) syncAccounts(start, end uint64) error {
-	accountRegistryAbi, err := abi.JSON(strings.NewReader(accountregistry.AccountRegistryABI))
-	if err != nil {
-		return errors.WithStack(err)
-	}
-
 	it, err := c.client.AccountRegistry.FilterPubkeyRegistered(&bind.FilterOpts{
 		Start: start,
 		End:   &end,
@@ -34,11 +25,11 @@ func (c *Commander) syncAccounts(start, end uint64) error {
 		}
 
 		// TODO: Handle registerBatch.
-		if !bytes.Equal(tx.Data()[:4], accountRegistryAbi.Methods["register"].ID) {
+		if !bytes.Equal(tx.Data()[:4], c.client.AccountRegistryABI.Methods["register"].ID) {
 			continue // TODO handle internal transactions
 		}
 
-		unpack, err := accountRegistryAbi.Methods["register"].Inputs.Unpack(tx.Data()[4:])
+		unpack, err := c.client.AccountRegistryABI.Methods["register"].Inputs.Unpack(tx.Data()[4:])
 		if err != nil {
 			return err
 		}
