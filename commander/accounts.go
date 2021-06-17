f031152e5f0c9cf64deb81923a2567a0de05b4b9
Dmytro Maretskyi <maretskii@gmail.com>
2021-06-17 14:03:40 +0200

    Parse account public key from calldata

--- commander/accounts.go
+++ commander/accounts.go
@@ -1,13 +1,24 @@
 package commander
 
 import (
+	"bytes"
+	"context"
+	"math/big"
+	"strings"
+
 	"github.com/Worldcoin/hubble-commander/contracts/accountregistry"
 	"github.com/Worldcoin/hubble-commander/models"
-	st "github.com/Worldcoin/hubble-commander/storage"
+	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/pkg/errors"
 )
 
 func (c *Commander) syncAccounts(start, end uint64) error {
+	accountRegistryAbi, err := abi.JSON(strings.NewReader(accountregistry.AccountRegistryABI))
+	if err != nil {
+		return errors.WithStack(err)
+	}
+
 	it, err := c.client.AccountRegistry.FilterPubkeyRegistered(&bind.FilterOpts{
 		Start: start,
 		End:   &end,
@@ -17,23 +28,31 @@ func (c *Commander) syncAccounts(start, end uint64) error {
 	}
 	defer func() { _ = it.Close() }()
 	for it.Next() {
-		err = ProcessPubkeyRegistered(c.storage, it.Event)
+		tx, _, err := c.client.ChainConnection.GetBackend().TransactionByHash(context.Background(), it.Event.Raw.TxHash)
 		if err != nil {
 			return err
 		}
-	}
-	return nil
-}
 
-func ProcessPubkeyRegistered(storage *st.Storage, event *accountregistry.AccountRegistryPubkeyRegistered) error {
-	account := models.Account{
-		PubKeyID:  uint32(event.PubkeyID.Uint64()),
-		PublicKey: models.MakePublicKeyFromInts(event.Pubkey),
-	}
+		// TODO: Handle registerBatch.
+		if !bytes.Equal(tx.Data()[:4], accountRegistryAbi.Methods["register"].ID) {
+			continue // TODO handle internal transactions
+		}
 
-	err := storage.AddAccountIfNotExists(&account)
-	if err != nil {
-		return err
+		unpack, err := accountRegistryAbi.Methods["register"].Inputs.Unpack(tx.Data()[4:])
+		if err != nil {
+			return err
+		}
+
+		pubkey := unpack[0].([4]*big.Int)
+		account := models.Account{
+			PubKeyID:  uint32(it.Event.PubkeyID.Uint64()),
+			PublicKey: models.MakePublicKeyFromInts(pubkey),
+		}
+
+		err = c.storage.AddAccountIfNotExists(&account)
+		if err != nil {
+			return err
+		}
 	}
 	return nil
 }
