cc6366f44feeb37bc9505290ef27586b4c4e8caa
Michał Sieczkowski <michal@ethworks.io>
2021-06-22 17:12:23 +0200

    Inline prepareTransfers

--- commander/executor/create_transfer_commitments.go
+++ commander/executor/create_transfer_commitments.go
@@ -49,26 +49,71 @@ func (t *TransactionExecutor) createTransferCommitment(
 	pendingTransfers []models.Transfer,
 	domain *bls.Domain,
 ) (
-	[]models.Transfer,
-	*models.Commitment,
-	error,
+	newPendingTransfers []models.Transfer,
+	commitment *models.Commitment,
+	err error,
 ) {
 	startTime := time.Now()
 
-	preparedTransfers, err := t.prepareTransfers(pendingTransfers)
-	if err != nil {
-		return nil, nil, err
-	}
-	if preparedTransfers == nil {
-		return nil, nil, nil
-	}
-
-	commitment, err := t.prepareTransferCommitment(preparedTransfers, domain)
+	initialStateRoot, err := t.stateTree.Root()
 	if err != nil {
 		return nil, nil, err
 	}
 
-	err = t.markTransfersAsIncluded(preparedTransfers.appliedTransfers, commitment.ID)
+	appliedTransfers := make([]models.Transfer, 0, t.cfg.TxsPerCommitment)
+	invalidTransfers := make([]models.Transfer, 0, 1)
+	var feeReceiverStateID uint32
+
+	for {
+		if len(pendingTransfers) == 0 {
+			pendingTransfers, err = t.storage.GetPendingTransfers(pendingTxsCountMultiplier * t.cfg.TxsPerCommitment)
+			if err != nil || len(pendingTransfers) == 0 {
+				return nil, nil, err
+			}
+		}
+
+		var transfers *AppliedTransfers
+
+		maxAppliedTransfers := t.cfg.TxsPerCommitment - uint32(len(appliedTransfers))
+		transfers, err = t.ApplyTransfers(pendingTransfers, maxAppliedTransfers)
+		if err != nil {
+			return nil, nil, err
+		}
+		if transfers == nil {
+			return nil, nil, ErrNotEnoughTransfers
+		}
+
+		appliedTransfers = append(appliedTransfers, transfers.appliedTransfers...)
+		invalidTransfers = append(invalidTransfers, transfers.invalidTransfers...)
+
+		if len(appliedTransfers) == int(t.cfg.TxsPerCommitment) {
+			feeReceiverStateID = *transfers.feeReceiverStateID
+			break
+		}
+
+		limit := pendingTxsCountMultiplier*t.cfg.TxsPerCommitment + uint32(len(appliedTransfers)+len(invalidTransfers))
+		pendingTransfers, err = t.storage.GetPendingTransfers(limit)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		// TODO - instead of doing this use SQL Offset (needs proper mempool)
+		pendingTransfers = removeTransfer(pendingTransfers, append(appliedTransfers, invalidTransfers...))
+
+		if len(pendingTransfers) == 0 {
+			err = t.stateTree.RevertTo(*initialStateRoot)
+			return nil, nil, err
+		}
+	}
+
+	newPendingTransfers = removeTransfer(pendingTransfers, append(appliedTransfers, invalidTransfers...))
+
+	commitment, err = t.prepareTransferCommitment(appliedTransfers, feeReceiverStateID, domain)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	err = t.markTransfersAsIncluded(appliedTransfers, commitment.ID)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -76,11 +121,11 @@ func (t *TransactionExecutor) createTransferCommitment(
 	log.Printf(
 		"Created a %s commitment from %d transactions in %s",
 		txtype.Transfer,
-		len(preparedTransfers.appliedTransfers),
+		len(appliedTransfers),
 		time.Since(startTime).Round(time.Millisecond).String(),
 	)
 
-	return preparedTransfers.newPendingTransfers, commitment, nil
+	return newPendingTransfers, commitment, nil
 }
 
 func removeTransfer(transferList, toRemove []models.Transfer) []models.Transfer {
--- commander/executor/transfer_commitment.go
+++ commander/executor/transfer_commitment.go
@@ -7,88 +7,24 @@ import (
 	"github.com/Worldcoin/hubble-commander/models/enums/txtype"
 )
 
-type PreparedTransfers struct {
-	appliedTransfers    []models.Transfer
-	newPendingTransfers []models.Transfer
-	feeReceiverStateID  *uint32
-}
-
-func (t *TransactionExecutor) prepareTransfers(pendingTransfers []models.Transfer) (*PreparedTransfers, error) {
-	initialStateRoot, err := t.stateTree.Root()
-	if err != nil {
-		return nil, err
-	}
-
-	preparedTransfers := &PreparedTransfers{
-		appliedTransfers: make([]models.Transfer, 0, t.cfg.TxsPerCommitment),
-	}
-
-	invalidTransfers := make([]models.Transfer, 0, 1)
-
-	for {
-		if len(pendingTransfers) == 0 {
-			pendingTransfers, err = t.storage.GetPendingTransfers(pendingTxsCountMultiplier * t.cfg.TxsPerCommitment)
-			if err != nil || len(pendingTransfers) == 0 {
-				return nil, err
-			}
-		}
-
-		var transfers *AppliedTransfers
-
-		maxAppliedTransfers := t.cfg.TxsPerCommitment - uint32(len(preparedTransfers.appliedTransfers))
-		transfers, err = t.ApplyTransfers(pendingTransfers, maxAppliedTransfers)
-		if err != nil {
-			return nil, err
-		}
-		if transfers == nil {
-			return nil, ErrNotEnoughTransfers
-		}
-
-		preparedTransfers.appliedTransfers = append(preparedTransfers.appliedTransfers, transfers.appliedTransfers...)
-		invalidTransfers = append(invalidTransfers, transfers.invalidTransfers...)
-
-		if len(preparedTransfers.appliedTransfers) == int(t.cfg.TxsPerCommitment) {
-			preparedTransfers.feeReceiverStateID = transfers.feeReceiverStateID
-			break
-		}
-
-		limit := pendingTxsCountMultiplier*t.cfg.TxsPerCommitment + uint32(len(preparedTransfers.appliedTransfers)+len(invalidTransfers))
-		pendingTransfers, err = t.storage.GetPendingTransfers(limit)
-		if err != nil {
-			return nil, err
-		}
-
-		// TODO - instead of doing this use SQL Offset (needs proper mempool)
-		pendingTransfers = removeTransfer(pendingTransfers, append(preparedTransfers.appliedTransfers, invalidTransfers...))
-
-		if len(pendingTransfers) == 0 {
-			err = t.stateTree.RevertTo(*initialStateRoot)
-			return nil, err
-		}
-	}
-
-	preparedTransfers.newPendingTransfers = removeTransfer(pendingTransfers, append(preparedTransfers.appliedTransfers, invalidTransfers...))
-
-	return preparedTransfers, nil
-}
-
 func (t *TransactionExecutor) prepareTransferCommitment(
-	preparedTransfers *PreparedTransfers,
+	appliedTransfers []models.Transfer,
+	feeReceiverStateID uint32,
 	domain *bls.Domain,
 ) (*models.Commitment, error) {
-	serializedTxs, err := encoder.SerializeTransfers(preparedTransfers.appliedTransfers)
+	serializedTxs, err := encoder.SerializeTransfers(appliedTransfers)
 	if err != nil {
 		return nil, err
 	}
 
-	combinedSignature, err := combineTransferSignatures(preparedTransfers.appliedTransfers, domain)
+	combinedSignature, err := combineTransferSignatures(appliedTransfers, domain)
 	if err != nil {
 		return nil, err
 	}
 
 	commitment, err := t.createAndStoreCommitment(
 		txtype.Transfer,
-		*preparedTransfers.feeReceiverStateID,
+		feeReceiverStateID,
 		serializedTxs,
 		combinedSignature,
 	)
