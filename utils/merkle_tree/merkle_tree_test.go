06c806854b1ac642e53df49e840470e90c245646
Dmytro Maretskyi <maretskii@gmail.com>
2021-06-22 14:02:37 +0200

    More tests

--- utils/merkle_tree/merkle_tree_test.go
+++ utils/merkle_tree/merkle_tree_test.go
@@ -28,7 +28,14 @@ func TestNewMerkleTree_TwoNodes(t *testing.T) {
 }
 
 func TestNewMerkleTree_ThreeNodes(t *testing.T) {
-	// TODO
+	leaf1 := utils.RandomHash()
+	leaf2 := utils.RandomHash()
+	leaf3 := utils.RandomHash()
+
+	tree, err := NewMerkleTree([]common.Hash{leaf1, leaf2, leaf3})
+	require.NoError(t, err)
+
+	require.Equal(t, utils.HashTwo(utils.HashTwo(leaf1, leaf2), utils.HashTwo(leaf3, GetZeroHash(2))), tree.Root())
 }
 
 func TestMerkleTree_GetWitness_OnlyRoot(t *testing.T) {
@@ -53,5 +60,17 @@ func TestMerkleTree_GetWitness_TwoNodes(t *testing.T) {
 }
 
 func TestMerkleTree_GetWitness_ThreeNodes(t *testing.T) {
-	// TODO
+	leaf1 := utils.RandomHash()
+	leaf2 := utils.RandomHash()
+	leaf3 := utils.RandomHash()
+
+	h12 := utils.HashTwo(leaf1, leaf2)
+	h30 := utils.HashTwo(leaf3, GetZeroHash(2))
+
+	tree, err := NewMerkleTree([]common.Hash{leaf1, leaf2, leaf3})
+	require.NoError(t, err)
+
+	require.Equal(t, Witness{leaf2, h30}, tree.GetWitness(0))
+	require.Equal(t, Witness{leaf1, h30}, tree.GetWitness(1))
+	require.Equal(t, Witness{GetZeroHash(2), h12}, tree.GetWitness(2))
 }
