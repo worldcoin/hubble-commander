19629871f9aa62f6227b80144ef65b7dd2685a03
Bartlomiej Tarczynski <51199571+b-tarczynski@users.noreply.github.com>
2021-06-24 22:03:55 +0200

    Add DisputeTransition smart contracts wrappers (#221)
    * Add inclusion proof structs
    
    * Move Witness to models package
    
    * Add TransferBody struct for TransferCommitmentInclusionProof
    
    * Add DisputeTransitionTransfer function to eth.Client
    
    * Add DisputeTransitionCreate2Trasnfer function
    
    * Export GetRollbackStatus
    
    * Rename witness to witnesses
    
    * Revert rename witness to witnesses
    
    * Rename witnesses to witness file name

---
+++ models/witness.go
@@ -0,0 +1,14 @@
+package models
+
+import "github.com/ethereum/go-ethereum/common"
+
+type Witness []common.Hash
+
+func (w Witness) Bytes() [][32]byte {
+	result := make([][32]byte, 0, len(w))
+	for i := range w {
+		result = append(result, w[i])
+		w[i].Bytes()
+	}
+	return result
+}
