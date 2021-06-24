0c35f3e5053e6f822bece8aeb1fc32d1d2d74c98
Daniel Izdebski <spidiritsu@gmail.com>
2021-06-24 23:02:11 +0200

    Calculate commitment inclusion proof of previous commitment (#222)
    * Add inclusion proof structs
    
    * Add TransferBody struct for TransferCommitmentInclusionProof
    
    * Add DisputeTransitionTransfer function to eth.Client
    
    * Add DisputeTransitionCreate2Trasnfer function
    
    * Export GetRollbackStatus
    
    * Add BodyHash method to DecodedCommitment
    
    * Add method for calculating previous commitment inclusion proof
    
    Co-authored-by: Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>

---
+++ encoder/commitment.go
@@ -0,0 +1,50 @@
+package encoder
+
+import (
+	"encoding/binary"
+	"math/big"
+
+	"github.com/Worldcoin/hubble-commander/models"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+type DecodedCommitment struct {
+	StateRoot         common.Hash
+	CombinedSignature models.Signature
+	FeeReceiver       uint32
+	Transactions      []byte
+}
+
+func (c *DecodedCommitment) BodyHash(accountRoot common.Hash) common.Hash {
+	arr := make([]byte, 32+64+32+len(c.Transactions))
+
+	copy(arr[0:32], accountRoot.Bytes())
+	copy(arr[32:96], c.CombinedSignature.Bytes())
+	binary.BigEndian.PutUint32(arr[124:128], c.FeeReceiver)
+	copy(arr[128:], c.Transactions)
+
+	return crypto.Keccak256Hash(arr)
+}
+
+func CommitmentToCalldataFields(commitments []models.Commitment) (
+	stateRoots [][32]byte,
+	signatures [][2]*big.Int,
+	feeReceivers []*big.Int,
+	transactions [][]byte,
+) {
+	count := len(commitments)
+
+	stateRoots = make([][32]byte, 0, count)
+	signatures = make([][2]*big.Int, 0, count)
+	feeReceivers = make([]*big.Int, 0, count)
+	transactions = make([][]byte, 0, count)
+
+	for i := range commitments {
+		stateRoots = append(stateRoots, commitments[i].PostStateRoot)
+		signatures = append(signatures, commitments[i].CombinedSignature.BigInts())
+		feeReceivers = append(feeReceivers, new(big.Int).SetUint64(uint64(commitments[i].FeeReceiver)))
+		transactions = append(transactions, commitments[i].Transactions)
+	}
+	return
+}
