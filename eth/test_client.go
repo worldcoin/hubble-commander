5d558545867e044c8f19536c17bc8b1e44b1eecd
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-07-08 12:07:50 +0200

    Add NewConfiguredTestClient function

--- eth/test_client.go
+++ eth/test_client.go
@@ -1,6 +1,7 @@
 package eth
 
 import (
+	"github.com/Worldcoin/hubble-commander/eth/deployer"
 	"github.com/Worldcoin/hubble-commander/eth/rollup"
 	"github.com/Worldcoin/hubble-commander/models"
 	"github.com/Worldcoin/hubble-commander/testutils/simulator"
@@ -14,12 +15,21 @@ type TestClient struct {
 // NewTestClient Sets up a TestClient backed by automining simulator.
 // Remember to call Close() at the end of the test
 func NewTestClient() (*TestClient, error) {
+	return NewConfiguredTestClient(rollup.DeploymentConfig{})
+}
+
+func NewConfiguredTestClient(cfg rollup.DeploymentConfig) (*TestClient, error) {
 	sim, err := simulator.NewAutominingSimulator()
 	if err != nil {
 		return nil, err
 	}
 
-	contracts, err := rollup.DeployRollup(sim)
+	cfg.AccountRegistryAddress, _, _, err = deployer.DeployAccountRegistry(sim)
+	if err != nil {
+		return nil, err
+	}
+
+	contracts, err := rollup.DeployConfiguredRollup(sim, cfg)
 	if err != nil {
 		return nil, err
 	}
