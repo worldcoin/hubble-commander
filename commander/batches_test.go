58a391b53ac3139540e56f635d44135e6e1a11df
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-07-12 10:42:00 +0200

    Add syncRemoteBatch replace local batch with correct one from chain

--- commander/batches_test.go
+++ commander/batches_test.go
@@ -1,14 +1,20 @@
 package commander
 
 import (
+	"context"
 	"testing"
 
 	"github.com/Worldcoin/hubble-commander/bls"
+	"github.com/Worldcoin/hubble-commander/commander/executor"
 	"github.com/Worldcoin/hubble-commander/config"
 	"github.com/Worldcoin/hubble-commander/eth"
 	"github.com/Worldcoin/hubble-commander/models"
+	"github.com/Worldcoin/hubble-commander/models/enums/txtype"
 	st "github.com/Worldcoin/hubble-commander/storage"
 	"github.com/Worldcoin/hubble-commander/testutils"
+	"github.com/Worldcoin/hubble-commander/utils"
+	"github.com/Worldcoin/hubble-commander/utils/ref"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 )
@@ -16,11 +22,12 @@ import (
 type BatchesTestSuite struct {
 	*require.Assertions
 	suite.Suite
-	cmd        *Commander
-	testClient *eth.TestClient
-	cfg        *config.Config
-	teardown   func() error
-	wallets    []bls.Wallet
+	cmd                 *Commander
+	testClient          *eth.TestClient
+	transactionExecutor *executor.TransactionExecutor
+	cfg                 *config.Config
+	teardown            func() error
+	wallets             []bls.Wallet
 }
 
 func (s *BatchesTestSuite) SetupSuite() {
@@ -45,6 +52,8 @@ func (s *BatchesTestSuite) SetupTest() {
 	s.cmd.client = s.testClient.Client
 	s.cmd.storage = testStorage.Storage
 	s.cmd.stopChannel = make(chan bool)
+
+	s.transactionExecutor = executor.NewTestTransactionExecutor(testStorage.Storage, s.testClient.Client, s.cfg.Rollup, context.Background())
 
 	s.wallets = generateWallets(s.T(), s.testClient.ChainState.Rollup, 2)
 	seedDB(s.T(), testStorage.Storage, st.NewStateTree(testStorage.Storage), s.wallets)
@@ -90,9 +99,37 @@ func (s *BatchesTestSuite) TestUnsafeSyncBatches_DoesNotSyncExistingBatchTwice()
 }
 
 // TODO test syncRemoteBatch:
-//  - test that in case of race condition local batch(es) is reverted and remote one is synced
 //  - test that when syncing fraudulent batch dispute is made
 //  - test that both race condition and dispute can be handled
+
+func (s *BatchesTestSuite) TestSyncRemoteBatch_ReplaceLocalBatchWithRemoteOne() {
+	transfers := []models.Transfer{
+		testutils.MakeTransfer(0, 1, 0, 100),
+		testutils.MakeTransfer(0, 1, 0, 200),
+	}
+	for i := range transfers {
+		signTransfer(s.T(), &s.wallets[transfers[i].FromStateID], &transfers[i])
+	}
+
+	s.runInTransaction(func() {
+		s.createAndSubmitTransferBatch(&transfers[0])
+	})
+
+	s.createTransferBatch(&transfers[1])
+
+	remoteBatches, err := s.testClient.GetBatches(&bind.FilterOpts{})
+	s.NoError(err)
+	s.Len(remoteBatches, 1)
+
+	s.testClient.Account = s.testClient.Accounts[1]
+	err = s.cmd.syncRemoteBatch(&remoteBatches[0])
+	s.NoError(err)
+
+	localBatch, err := s.cmd.storage.GetBatch(remoteBatches[0].ID)
+	s.NoError(err)
+	localBatch.BlockTime = ref.Time(localBatch.BlockTime.UTC())
+	s.Equal(remoteBatches[0].Batch, *localBatch)
+}
 
 func (s *BatchesTestSuite) syncAllBlocks() {
 	latestBlockNumber, err := s.testClient.GetLatestBlockNumber()
@@ -100,6 +137,61 @@ func (s *BatchesTestSuite) syncAllBlocks() {
 
 	err = s.cmd.unsafeSyncBatches(0, *latestBlockNumber)
 	s.NoError(err)
+}
+
+func (s *BatchesTestSuite) createAndSubmitTransferBatch(tx *models.Transfer) *models.Batch {
+	_, err := s.cmd.storage.AddTransfer(tx)
+	s.NoError(err)
+
+	pendingBatch, err := s.transactionExecutor.NewPendingBatch(txtype.Transfer)
+	s.NoError(err)
+
+	commitments, err := s.transactionExecutor.CreateTransferCommitments(testDomain)
+	s.NoError(err)
+	s.Len(commitments, 1)
+
+	err = s.transactionExecutor.SubmitBatch(pendingBatch, commitments)
+	s.NoError(err)
+
+	s.testClient.Commit()
+	return pendingBatch
+}
+
+func (s *BatchesTestSuite) createTransferBatch(tx *models.Transfer) *models.Batch {
+	_, err := s.cmd.storage.AddTransfer(tx)
+	s.NoError(err)
+
+	pendingBatch, err := s.transactionExecutor.NewPendingBatch(txtype.Transfer)
+	s.NoError(err)
+
+	commitments, err := s.transactionExecutor.CreateTransferCommitments(testDomain)
+	s.NoError(err)
+	s.Len(commitments, 1)
+
+	pendingBatch.TransactionHash = utils.RandomHash()
+	err = s.cmd.storage.AddBatch(pendingBatch)
+	s.NoError(err)
+
+	err = s.cmd.storage.MarkCommitmentAsIncluded(commitments[0].ID, pendingBatch.ID)
+	s.NoError(err)
+
+	return pendingBatch
+}
+
+func (s *BatchesTestSuite) runInTransaction(handler func()) {
+	storage := *s.cmd.storage
+	txController, txStorage, err := s.cmd.storage.BeginTransaction(st.TxOptions{Postgres: true, Badger: true})
+	s.NoError(err)
+	s.cmd.storage = txStorage
+
+	defer func() {
+		txController.Rollback(nil)
+		s.cmd.storage = &storage
+		s.transactionExecutor = executor.NewTestTransactionExecutor(s.cmd.storage, s.testClient.Client, s.cfg.Rollup, context.Background())
+	}()
+
+	s.transactionExecutor = executor.NewTestTransactionExecutor(s.cmd.storage, s.testClient.Client, s.cfg.Rollup, context.Background())
+	handler()
 }
 
 func TestBatchesTestSuite(t *testing.T) {
