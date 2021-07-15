846dfed8d328b7d6626493cdf47dffea4edd0146
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-07-15 10:24:45 +0200

    Add Clone to TestStorage

--- storage/new_test_storage.go
+++ storage/new_test_storage.go
@@ -1,6 +1,7 @@
 package storage
 
 import (
+	"github.com/Worldcoin/hubble-commander/config"
 	"github.com/Worldcoin/hubble-commander/db/badger"
 	"github.com/Worldcoin/hubble-commander/db/postgres"
 )
@@ -68,6 +69,61 @@ func NewConfiguredTestStorage(cfg TestStorageConfig) (*TestStorage, error) {
 			return badgerTestDB.Teardown()
 		}
 	}
+
+	return &TestStorage{
+		Storage:  &storage,
+		Teardown: teardown,
+	}, nil
+}
+
+func (s *TestStorage) Clone(cfg *config.CloneConfig) (*TestStorage, error) {
+	// TODO-CLONE: rethink it
+	// - especially these ugly teardowns
+	storage := *s.Storage
+	var teardown = func() error {
+		return nil
+	}
+	var oldTeardown = func() error {
+		return nil
+	}
+
+	if s.Postgres != nil {
+		testPostgres := postgres.TestDB{DB: s.Postgres}
+		clonedPostgres, err := testPostgres.Clone(&cfg.PostgresConfig, cfg.PostgresSourceDB)
+		if err != nil {
+			return nil, err
+		}
+		storage.Postgres = clonedPostgres.DB
+		teardown = func() error {
+			return clonedPostgres.Teardown()
+		}
+		oldTeardown = testPostgres.Teardown
+	}
+
+	if s.Badger != nil {
+		testBadger := badger.TestDB{DB: s.Badger}
+		clonedBadger, err := testBadger.Clone()
+		if err != nil {
+			return nil, err
+		}
+		storage.Badger = clonedBadger.DB
+		parentTeardown := teardown
+		teardown = func() error {
+			if err := parentTeardown(); err != nil {
+				return err
+			}
+			return clonedBadger.Teardown()
+		}
+		oldParentTeardown := teardown
+		oldTeardown = func() error {
+			if err := oldParentTeardown(); err != nil {
+				return err
+			}
+			return s.Badger.Close()
+		}
+	}
+
+	s.Teardown = oldTeardown
 
 	return &TestStorage{
 		Storage:  &storage,
