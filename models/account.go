2adca3ac3ba7d81a8e9d7e32ffdc1c58b37adc64
Daniel Izdebski <spidiritsu@gmail.com>
2021-07-19 17:24:52 +0200

    Add AccountNode type

--- models/account.go
+++ models/account.go
@@ -1,6 +1,13 @@
 package models
 
+import "github.com/ethereum/go-ethereum/common"
+
 type Account struct {
 	PubKeyID  uint32
 	PublicKey PublicKey `badgerhold:"index"`
+}
+
+type AccountNode struct {
+	MerklePath MerklePath
+	DataHash   common.Hash
 }
