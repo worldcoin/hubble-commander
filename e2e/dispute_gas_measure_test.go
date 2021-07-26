98e14ada896600ef46e93e2d6b427d470b2dba09
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-07-26 12:48:06 +0200

    Measure C2T dispute gas usage for commitment with 32 txs

--- e2e/dispute_gas_measure_test.go
+++ e2e/dispute_gas_measure_test.go
@@ -4,12 +4,13 @@ import (
 	"bytes"
 	"testing"
 
+	"github.com/Worldcoin/hubble-commander/bls"
 	"github.com/Worldcoin/hubble-commander/contracts/rollup"
 	"github.com/Worldcoin/hubble-commander/e2e/setup"
 	"github.com/Worldcoin/hubble-commander/encoder"
 	"github.com/Worldcoin/hubble-commander/eth"
 	"github.com/Worldcoin/hubble-commander/models"
-	"github.com/Worldcoin/hubble-commander/utils"
+	"github.com/Worldcoin/hubble-commander/utils/ref"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/stretchr/testify/require"
 	"github.com/ybbus/jsonrpc/v2"
@@ -36,7 +37,7 @@ func TestMeasureDisputeGasUsage(t *testing.T) {
 	testSendTransferBatch(t, cmd.Client(), senderWallet, 0)
 
 	measureDisputeTransitionTransferInvalidStateRoot(t, cmd.Client(), ethClient)
-	//testDisputeTransitionC2TInvalidStateRoot(t, cmd.Client(), ethClient, wallets[len(wallets)-1])
+	measureTransitionDisputeC2TInvalidStateRoot(t, cmd.Client(), ethClient, wallets)
 }
 
 func measureDisputeTransitionTransferInvalidStateRoot(t *testing.T, client jsonrpc.RPCClient, ethClient *eth.Client) {
@@ -46,6 +47,18 @@ func measureDisputeTransitionTransferInvalidStateRoot(t *testing.T, client jsonr
 	defer subscription.Unsubscribe()
 
 	send32TransfersBatchWithInvalidStateRoot(t, ethClient)
+	testRollbackCompletion(t, ethClient, sink, subscription)
+
+	testBatchesAfterDispute(t, client, 1)
+}
+
+func measureTransitionDisputeC2TInvalidStateRoot(t *testing.T, client jsonrpc.RPCClient, ethClient *eth.Client, wallets []bls.Wallet) {
+	sink := make(chan *rollup.RollupRollbackStatus)
+	subscription, err := ethClient.Rollup.WatchRollbackStatus(&bind.WatchOpts{}, sink)
+	require.NoError(t, err)
+	defer subscription.Unsubscribe()
+
+	send32C2TBatchWithInvalidStateRoot(t, ethClient, wallets)
 	testRollbackCompletion(t, ethClient, sink, subscription)
 
 	testBatchesAfterDispute(t, client, 1)
@@ -64,22 +77,44 @@ func send32TransfersBatchWithInvalidStateRoot(t *testing.T, ethClient *eth.Clien
 	encodedTransfer, err := encoder.EncodeTransferForCommitment(&transfer)
 	require.NoError(t, err)
 
-	commitment := models.Commitment{
-		Transactions:      bytes.Repeat(encodedTransfer, 32),
-		FeeReceiver:       0,
-		CombinedSignature: models.Signature{},
-		PostStateRoot:     utils.RandomHash(),
-	}
-	commitments := make([]models.Commitment, 32)
-	for i := range commitments {
-		commitments[i] = commitment
-	}
-	submitTransfersBatch(t, ethClient, commitments, 2)
+	sendTransferCommitment(t, ethClient, bytes.Repeat(encodedTransfer, 32), 2)
 }
 
-func submitTransfersBatch(t *testing.T, ethClient *eth.Client, commitments []models.Commitment, batchID uint64) {
-	transaction, err := ethClient.SubmitTransfersBatch(commitments)
-	require.NoError(t, err)
+func send32C2TBatchWithInvalidStateRoot(t *testing.T, ethClient *eth.Client, wallets []bls.Wallet) {
+	transfer := models.Create2Transfer{
+		TransactionBase: models.TransactionBase{
+			FromStateID: 1,
+			Amount:      models.MakeUint256(90),
+			Fee:         models.MakeUint256(10),
+		},
+		ToStateID: ref.Uint32(38),
+	}
 
-	waitForSubmittedBatch(t, ethClient, transaction, batchID)
+	registrations, unsubscribe, err := ethClient.WatchBatchAccountRegistrations(&bind.WatchOpts{})
+	require.NoError(t, err)
+	defer unsubscribe()
+
+	publicKeyBatch := make([]models.PublicKey, 16)
+	registeredPubKeyIDs := make([]uint32, 0, 32)
+	walletIndex := len(wallets) - 32
+	for i := 0; i < 2; i++ {
+		for j := range publicKeyBatch {
+			publicKeyBatch[j] = *wallets[walletIndex].PublicKey()
+			walletIndex++
+		}
+		pubKeyIDs, err := ethClient.RegisterBatchAccount(publicKeyBatch, registrations)
+		require.NoError(t, err)
+		registeredPubKeyIDs = append(registeredPubKeyIDs, pubKeyIDs...)
+	}
+
+	encodedTransfers := make([]byte, 0, encoder.Create2TransferLength*32)
+	for i := range registeredPubKeyIDs {
+		transfer.ToStateID = ref.Uint32(uint32(38 + i))
+		encodedTx, err := encoder.EncodeCreate2TransferForCommitment(&transfer, registeredPubKeyIDs[i])
+		require.NoError(t, err)
+
+		encodedTransfers = append(encodedTransfers, encodedTx...)
+	}
+
+	sendC2TCommitment(t, ethClient, encodedTransfers, 2)
 }
