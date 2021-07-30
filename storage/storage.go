361faecec8ca6fb95989d721edbaccc1f4c429f5
Daniel Izdebski <spidiritsu@gmail.com>
2021-07-30 13:17:04 +0200

    Use new Database struct in StorageBase

--- storage/storage.go
+++ storage/storage.go
@@ -1,13 +1,7 @@
 package storage
 
 import (
-	"github.com/Masterminds/squirrel"
 	"github.com/Worldcoin/hubble-commander/config"
-	"github.com/Worldcoin/hubble-commander/db"
-	"github.com/Worldcoin/hubble-commander/db/badger"
-	"github.com/Worldcoin/hubble-commander/db/postgres"
-	"github.com/golang-migrate/migrate/v4"
-	log "github.com/sirupsen/logrus"
 )
 
 type Storage struct {
@@ -17,9 +11,7 @@ type Storage struct {
 }
 
 type StorageBase struct {
-	Postgres            *postgres.Database
-	Badger              *badger.Database
-	QB                  squirrel.StatementBuilderType
+	Database            *Database
 	feeReceiverStateIDs map[string]uint32 // token ID => state id
 	latestBlockNumber   uint32
 	syncedBlock         *uint64
@@ -31,21 +23,14 @@ type TxOptions struct {
 	ReadOnly bool
 }
 
-func NewStorage(postgresConfig *config.PostgresConfig, badgerConfig *config.BadgerConfig) (*Storage, error) {
-	postgresDB, err := postgres.NewDatabase(postgresConfig)
-	if err != nil {
-		return nil, err
-	}
-
-	badgerDB, err := badger.NewDatabase(badgerConfig)
+func NewConfiguredStorage(cfg *config.Config) (storage *Storage, err error) {
+	database, err := NewDatabase(cfg)
 	if err != nil {
 		return nil, err
 	}
 
 	storageBase := &StorageBase{
-		Postgres:            postgresDB,
-		Badger:              badgerDB,
-		QB:                  getQueryBuilder(),
+		Database:            database,
 		feeReceiverStateIDs: make(map[string]uint32),
 	}
 
@@ -56,92 +41,6 @@ func NewStorage(postgresConfig *config.PostgresConfig, badgerConfig *config.Badg
 	}, nil
 }
 
-func NewConfiguredStorage(cfg *config.Config) (storage *Storage, err error) {
-	err = postgres.CreateDatabaseIfNotExist(cfg.Postgres)
-	if err != nil {
-		return nil, err
-	}
-
-	migrator, err := postgres.GetMigrator(cfg.Postgres)
-	if err != nil {
-		return nil, err
-	}
-	defer func() {
-		srcErr, dbErr := migrator.Close()
-		if err == nil {
-			if srcErr != nil {
-				err = srcErr
-			} else {
-				err = dbErr
-			}
-		}
-	}()
-
-	storage, err = NewStorage(cfg.Postgres, cfg.Badger)
-	if err != nil {
-		return nil, err
-	}
-
-	if cfg.Bootstrap.Prune {
-		err = storage.Prune(migrator)
-		if err != nil {
-			return nil, err
-		}
-		log.Debug("Badger and Postgres databases were pruned")
-	}
-
-	err = migrator.Up()
-	if err != nil && err != migrate.ErrNoChange {
-		return nil, err
-	}
-
-	return storage, nil
-}
-
-// TODO after removing all state/account tree methods from StorageBase try to rewrite this method to Storage
-func (s *StorageBase) BeginTransaction(opts TxOptions) (*db.TxController, *StorageBase, error) {
-	var txController *db.TxController
-	storage := *s
-
-	if opts.Postgres && !opts.ReadOnly {
-		postgresTx, postgresDB, err := s.Postgres.BeginTransaction()
-		if err != nil {
-			return nil, nil, err
-		}
-		txController = postgresTx
-		storage.Postgres = postgresDB
-	}
-
-	if opts.Badger {
-		badgerTx, badgerDB := s.Badger.BeginTransaction(!opts.ReadOnly)
-		if txController != nil {
-			combinedController := NewCombinedController(txController, badgerTx)
-			txController = db.NewTxController(combinedController, txController.IsLocked())
-		} else {
-			txController = badgerTx
-		}
-		storage.Badger = badgerDB
-	}
-
-	return txController, &storage, nil
-}
-
-func (s *StorageBase) Close() error {
-	err := s.Postgres.Close()
-	if err != nil {
-		return err
-	}
-	return s.Badger.Close()
-}
-
-func (s *StorageBase) Prune(migrator *migrate.Migrate) error {
-	err := migrator.Down()
-	if err != nil && err != migrate.ErrNoChange {
-		return err
-	}
-	return s.Badger.Prune()
-}
-
-func getQueryBuilder() squirrel.StatementBuilderType {
-	return squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar)
+func (s *Storage) Close() error {
+	return s.Database.Close()
 }
