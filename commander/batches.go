d4bfa2eb3bc5a6cc98e4e5d04d79bb408731b062
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-08-03 08:29:59 +0200

    Move addGenesisBatch function to genesis.go

--- commander/batches.go
+++ commander/batches.go
@@ -149,30 +149,6 @@ func (c *Commander) getLatestBatchID() (*models.Uint256, error) {
 	return &latestBatch.ID, nil
 }
 
-func (c *Commander) addGenesisBatch() error {
-	nextBatchID, err := c.storage.GetNextBatchID()
-	if err != nil {
-		return err
-	}
-	if nextBatchID.CmpN(1) != 0 {
-		return nil
-	}
-
-	root, err := c.storage.StateTree.Root()
-	if err != nil {
-		return err
-	}
-
-	batch, err := c.client.GetBatch(models.NewUint256(0))
-	if err != nil {
-		return err
-	}
-
-	batch.PrevStateRoot = root
-
-	return c.storage.AddBatch(batch)
-}
-
 func logBatchesCount(newRemoteBatches []eth.DecodedBatch) {
 	newBatchesCount := len(newRemoteBatches)
 	if newBatchesCount > 0 {
