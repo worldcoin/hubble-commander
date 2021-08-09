112e8555e42f1609dd1c2c726ada7162f49bbede
Daniel Izdebski <spidiritsu@gmail.com>
2021-08-09 13:06:39 +0200

    Add method for reading chain spec file and loading it

--- commander/chain_spec.go
+++ commander/chain_spec.go
@@ -1,10 +1,51 @@
 package commander
 
 import (
+	"io/ioutil"
+	"strconv"
+
+	cfg "github.com/Worldcoin/hubble-commander/config"
 	"github.com/Worldcoin/hubble-commander/models"
 	"github.com/Worldcoin/hubble-commander/utils/ref"
 	"gopkg.in/yaml.v2"
 )
+
+func ReadChainSpecFile(path string) (*models.ChainSpec, error) {
+	yamlFile, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var chainSpec models.ChainSpec
+	err = yaml.Unmarshal(yamlFile, &chainSpec)
+	if err != nil {
+		return nil, err
+	}
+
+	return &chainSpec, nil
+}
+
+func LoadChainSpec(config *cfg.Config, chainSpec *models.ChainSpec) error {
+	chainState := newChainState(chainSpec)
+
+	storage, err := st.NewStorage(config)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		err = storage.Close()
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	err = storage.SetChainState(&chainState)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func GenerateChainSpec(chainState *models.ChainState) (*string, error) {
 	chainSpec := newChainSpec(chainState)
@@ -24,5 +65,16 @@ func newChainSpec(chainState *models.ChainState) models.ChainSpec {
 		DeploymentBlock: chainState.DeploymentBlock,
 		Rollup:          chainState.Rollup,
 		GenesisAccounts: chainState.GenesisAccounts,
+	}
+}
+
+func newChainState(chainSpec *models.ChainSpec) models.ChainState {
+	return models.ChainState{
+		ChainID:         chainSpec.ChainID,
+		AccountRegistry: chainSpec.AccountRegistry,
+		DeploymentBlock: chainSpec.DeploymentBlock,
+		Rollup:          chainSpec.Rollup,
+		GenesisAccounts: chainSpec.GenesisAccounts,
+		SyncedBlock:     getInitialSyncedBlock(chainSpec.DeploymentBlock),
 	}
 }
--- commander/chain_spec_test.go
+++ commander/chain_spec_test.go
@@ -1,6 +1,8 @@
 package commander
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	"github.com/Worldcoin/hubble-commander/models"
@@ -9,8 +11,28 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-func TestChainSpecTestSuite(t *testing.T) {
-	chainState := &models.ChainState{
+type ChainSpecTestSuite struct {
+	*require.Assertions
+	suite.Suite
+	storage    *st.TestStorage
+	config     *cfg.Config
+	chainState models.ChainState
+	chainSpec  models.ChainSpec
+}
+
+func (s *ChainSpecTestSuite) SetupSuite() {
+	s.Assertions = require.New(s.T())
+}
+
+func (s *ChainSpecTestSuite) SetupTest() {
+	var err error
+	s.storage, err = st.NewTestStorage()
+	s.NoError(err)
+	s.config = cfg.GetTestConfig()
+	s.config.Ethereum = &cfg.EthereumConfig{
+		ChainID: "1337",
+	}
+	s.chainState = models.ChainState{
 		ChainID:         models.MakeUint256(1337),
 		AccountRegistry: utils.RandomAddress(),
 		DeploymentBlock: 8837,
@@ -37,12 +59,63 @@ func TestChainSpecTestSuite(t *testing.T) {
 		},
 		SyncedBlock: 7738,
 	}
-	expectedChainSpec := newChainSpec(chainState)
+	err = s.storage.SetChainState(chainState)
+	s.NoError(err)
+	s.chainSpec = newChainSpec(chainState)
+}
 
-	yamlChainSpec, err := GenerateChainSpec(chainState)
-	require.NoError(t, err)
+func (s *ChainSpecTestSuite) TearDownTest() {
+	err := s.storage.Teardown()
+	s.NoError(err)
+}
+
+func (s *ChainSpecTestSuite) TestGenerateChainSpec() {
+	yamlChainSpec, err := GenerateChainSpec(&s.chainState)
+	s.NoError(err)
 	var chainSpec models.ChainSpec
 	err = yaml.Unmarshal([]byte(*yamlChainSpec), &chainSpec)
-	require.NoError(t, err)
-	require.EqualValues(t, expectedChainSpec, chainSpec)
+	s.NoError(err)
+	s.EqualValues(s.chainSpec, chainSpec)
+}
+
+func (s *ChainSpecTestSuite) TestLoadChainSpec() {
+	chainState1337, err := s.storage.GetChainState(s.chainSpec.ChainID)
+	s.NoError(err)
+
+	s.chainSpec.DeploymentBlock = 483924
+	s.chainSpec.ChainID = models.MakeUint256(4000)
+
+	err = LoadChainSpec(s.config, &s.chainSpec)
+	s.NoError(err)
+	chainState4000, err := s.storage.GetChainState(s.chainSpec.ChainID)
+	s.NoError(err)
+
+	s.NotEqualValues(chainState1337, chainState4000)
+
+	chainSpec := newChainSpec(chainState4000)
+	s.EqualValues(s.chainSpec, chainSpec)
+}
+
+func (s *ChainSpecTestSuite) TestReadChainSpecFile() {
+	yamlChainSpec, err := GenerateChainSpec(s.config)
+	s.NoError(err)
+
+	file, err := ioutil.TempFile("", "chain_state_test")
+	s.NoError(err)
+	defer func() {
+		err = os.Remove(file.Name())
+		s.NoError(err)
+	}()
+
+	_, err = file.Write([]byte(*yamlChainSpec))
+	s.NoError(err)
+
+	chainSpec, err := ReadChainSpecFile(file.Name())
+	s.NoError(err)
+
+	s.EqualValues(s.chainSpec, *chainSpec)
+}
+
+func TestChainSpecTestSuite(t *testing.T) {
+	suite.Run(t, new(ChainSpecTestSuite))
 }
