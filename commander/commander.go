5350ba95dbb8299741971643282d46d9b2d6573d
Michał Sieczkowski <michal@ethworks.io>
2021-08-24 12:16:32 +0200

    Make minor stylistic changes

--- commander/commander.go
+++ commander/commander.go
@@ -102,7 +102,7 @@ func (c *Commander) Start() (err error) {
 	})
 	c.startWorker(func() error { return c.newBlockLoop() })
 
-	log.Printf("Commander started and listening on port %s.", c.cfg.API.Port)
+	log.Printf("Commander started and listening on port %s", c.cfg.API.Port)
 
 	return nil
 }
