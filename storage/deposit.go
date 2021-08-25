4dcee256fc4f13ca9c7c4154d11872edd4eed132
Daniel Izdebski <spidiritsu@gmail.com>
2021-08-25 15:07:36 +0200

    Add AddDeposit and GetDeposit methods

--- storage/deposit.go
+++ storage/deposit.go
@@ -1,5 +1,10 @@
 package storage
 
+import (
+	"github.com/Worldcoin/hubble-commander/models"
+	bh "github.com/timshannon/badgerhold/v3"
+)
+
 type DepositStorage struct {
 	database *Database
 }
@@ -15,4 +20,21 @@ func (s *DepositStorage) copyWithNewDatabase(database *Database) *DepositStorage
 	newDepositStorage.database = database
 
 	return &newDepositStorage
+}
+
+func (s *DepositStorage) AddDeposit(deposit *models.Deposit) error {
+	return s.database.Badger.Upsert(deposit.ID, *deposit)
+}
+
+func (s *DepositStorage) GetDeposit(depositID *models.DepositID) (*models.Deposit, error) {
+	var deposit models.Deposit
+	err := s.database.Badger.Get(depositID, &deposit)
+	if err == bh.ErrNotFound {
+		return nil, NewNotFoundError("deposit")
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	return &deposit, nil
 }
---
+++ storage/deposit_test.go
@@ -0,0 +1,62 @@
+package storage
+
+import (
+	"testing"
+
+	"github.com/Worldcoin/hubble-commander/models"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+var (
+	deposit = models.Deposit{
+		ID: models.DepositID{
+			BlockNumber: 16,
+			LogIndex:    32,
+		},
+		ToPubKeyID:           4,
+		TokenID:              models.MakeUint256(4),
+		L2Amount:             models.MakeUint256(1024),
+		IncludedInCommitment: nil,
+	}
+)
+
+type DepositTestSuite struct {
+	*require.Assertions
+	suite.Suite
+	storage *TestStorage
+}
+
+func (s *DepositTestSuite) SetupSuite() {
+	s.Assertions = require.New(s.T())
+}
+
+func (s *DepositTestSuite) SetupTest() {
+	var err error
+	s.storage, err = NewTestStorageWithoutPostgres()
+	s.NoError(err)
+}
+
+func (s *DepositTestSuite) TearDownTest() {
+	err := s.storage.Teardown()
+	s.NoError(err)
+}
+
+func (s *DepositTestSuite) TestAddDeposit_AddAndRetrieve() {
+	err := s.storage.AddDeposit(&deposit)
+	s.NoError(err)
+
+	actual, err := s.storage.GetDeposit(&deposit.ID)
+	s.NoError(err)
+	s.Equal(deposit, *actual)
+}
+
+func (s *DepositTestSuite) TestGetDeposit_NotFound() {
+	_, err := s.storage.GetDeposit(&deposit.ID)
+	s.Equal(NewNotFoundError("deposit"), err)
+	s.True(IsNotFoundError(err))
+}
+
+func TestDepositTestSuite(t *testing.T) {
+	suite.Run(t, new(DepositTestSuite))
+}
