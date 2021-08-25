4a3de12dc540c70a197dad83b18b031fbd370e3a
Daniel Izdebski <spidiritsu@gmail.com>
2021-08-25 14:21:52 +0200

    Add Deposit model

---
+++ models/deposit.go
@@ -0,0 +1,14 @@
+package models
+
+type DepositID struct {
+	BlockNumber uint32
+	LogIndex    uint32
+}
+
+type Deposit struct {
+	ID                   DepositID
+	ToPubKeyID           uint32
+	TokenID              Uint256
+	L2Amount             Uint256
+	IncludedInCommitment *int32
+}
