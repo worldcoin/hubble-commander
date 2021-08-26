a7cac334c0548cbde3883a11d65907f7159da381
Daniel Izdebski <spidiritsu@gmail.com>
2021-08-26 12:06:52 +0200

    Add Bytes and SetBytes methods to Deposit and DepositID models

--- models/deposit.go
+++ models/deposit.go
@@ -1,5 +1,17 @@
 package models
 
+import (
+	"encoding/binary"
+
+	"github.com/Worldcoin/hubble-commander/utils"
+)
+
+const (
+	depositDataLength             = 68
+	depositInCommitmentDataLength = 101
+	depositIDDataLength           = 8
+)
+
 type DepositID struct {
 	BlockNumber uint32
 	LogIndex    uint32
@@ -11,4 +23,59 @@ type Deposit struct {
 	TokenID              Uint256
 	L2Amount             Uint256
 	IncludedInCommitment *CommitmentID
+}
+
+func (d *DepositID) Bytes() []byte {
+	b := make([]byte, depositIDDataLength)
+	binary.BigEndian.PutUint32(b[0:4], d.BlockNumber)
+	binary.BigEndian.PutUint32(b[4:8], d.LogIndex)
+	return b
+}
+
+func (d *DepositID) SetBytes(data []byte) error {
+	if len(data) != depositIDDataLength {
+		return ErrInvalidLength
+	}
+
+	d.BlockNumber = binary.BigEndian.Uint32(data[0:4])
+	d.LogIndex = binary.BigEndian.Uint32(data[4:8])
+
+	return nil
+}
+
+func (d *Deposit) Bytes() []byte {
+	var b []byte
+
+	if d.IncludedInCommitment != nil {
+		b = make([]byte, depositInCommitmentDataLength)
+		copy(b[68:101], d.IncludedInCommitment.Bytes())
+	} else {
+		b = make([]byte, depositDataLength)
+	}
+
+	binary.BigEndian.PutUint32(b[0:4], d.ToPubKeyID)
+	copy(b[4:36], utils.PadLeft(d.TokenID.Bytes(), 32))
+	copy(b[36:68], utils.PadLeft(d.L2Amount.Bytes(), 32))
+
+	return b
+}
+
+func (d *Deposit) SetBytes(data []byte) error {
+	if len(data) != depositDataLength && len(data) != depositInCommitmentDataLength {
+		return ErrInvalidLength
+	}
+
+	if len(data) == depositInCommitmentDataLength {
+		d.IncludedInCommitment = &CommitmentID{}
+		err := d.IncludedInCommitment.SetBytes(data[68:101])
+		if err != nil {
+			return err
+		}
+	}
+
+	d.ToPubKeyID = binary.BigEndian.Uint32(data[0:4])
+	d.TokenID.SetBytes(data[4:36])
+	d.L2Amount.SetBytes(data[36:68])
+
+	return nil
 }
---
+++ models/deposit_test.go
@@ -0,0 +1,68 @@
+package models
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDeposit_Bytes(t *testing.T) {
+	deposit := Deposit{
+		ID: DepositID{
+			BlockNumber: 4321,
+			LogIndex:    63452,
+		},
+		ToPubKeyID: 16,
+		TokenID:    MakeUint256(44),
+		L2Amount:   MakeUint256(235),
+	}
+
+	bytes := deposit.Bytes()
+
+	decodedDeposit := Deposit{
+		ToPubKeyID: 333,
+		TokenID:    MakeUint256(222),
+	}
+	err := decodedDeposit.SetBytes(bytes)
+	require.NoError(t, err)
+
+	require.Equal(t, DepositID{}, decodedDeposit.ID)
+	decodedDeposit.ID = deposit.ID
+	require.Equal(t, deposit, decodedDeposit)
+}
+
+func TestDepositInCommitment_Bytes(t *testing.T) {
+	deposit := Deposit{
+		ToPubKeyID: 33,
+		TokenID:    MakeUint256(234),
+		L2Amount:   MakeUint256(62346),
+		IncludedInCommitment: &CommitmentID{
+			BatchID:      MakeUint256(432),
+			IndexInBatch: 11,
+		},
+	}
+
+	bytes := deposit.Bytes()
+
+	decodedDeposit := Deposit{
+		ToPubKeyID: 555,
+		TokenID:    MakeUint256(555),
+	}
+	err := decodedDeposit.SetBytes(bytes)
+	require.NoError(t, err)
+	require.Equal(t, deposit, decodedDeposit)
+}
+
+func TestDepositID_Bytes(t *testing.T) {
+	depositID := DepositID{
+		BlockNumber: 68593,
+		LogIndex:    49102,
+	}
+
+	bytes := depositID.Bytes()
+
+	var decodedDepositID DepositID
+	err := decodedDepositID.SetBytes(bytes)
+	require.NoError(t, err)
+	require.Equal(t, depositID, decodedDepositID)
+}
