649c45972e3da750c7642e8a1d4ad9e7e289bfe6
Michał Sieczkowski <michal@ethworks.io>
2021-09-10 14:58:20 +0200

    Reorder SubmitBatch tests

--- commander/executor/submit_batch_test.go
+++ commander/executor/submit_batch_test.go
@@ -74,26 +74,6 @@ func (s *SubmitTransferBatchTestSuite) TestSubmitBatch_SubmitsCommitmentsOnChain
 	s.Equal(big.NewInt(2), nextBatchID)
 }
 
-func (s *SubmitC2TBatchTestSuite) TestSubmitBatch_SubmitsCommitmentsOnChain() {
-	nextBatchID, err := s.client.Rollup.NextBatchID(nil)
-	s.NoError(err)
-	s.Equal(big.NewInt(1), nextBatchID)
-
-	commitment := baseCommitment
-	commitment.ID.BatchID = models.MakeUint256FromBig(*nextBatchID)
-
-	pendingBatch, err := s.executionCtx.NewPendingBatch(txtype.Create2Transfer)
-	s.NoError(err)
-	err = s.executionCtx.SubmitBatch(pendingBatch, []models.Commitment{commitment})
-	s.NoError(err)
-
-	s.client.Commit()
-
-	nextBatchID, err = s.client.Rollup.NextBatchID(nil)
-	s.NoError(err)
-	s.Equal(big.NewInt(2), nextBatchID)
-}
-
 func (s *SubmitTransferBatchTestSuite) TestSubmitBatch_StoresPendingBatchRecord() {
 	pendingBatch, err := s.executionCtx.NewPendingBatch(txtype.Transfer)
 	s.NoError(err)
@@ -111,37 +91,6 @@ func (s *SubmitTransferBatchTestSuite) TestSubmitBatch_StoresPendingBatchRecord(
 	s.NotEqual(common.Hash{}, batch.TransactionHash)
 	s.Equal(pendingBatch.PrevStateRoot, batch.PrevStateRoot)
 	s.Nil(batch.Hash)
-}
-
-func (s *SubmitC2TBatchTestSuite) TestSubmitBatch_StoresPendingBatchRecord() {
-	pendingBatch, err := s.executionCtx.NewPendingBatch(txtype.Create2Transfer)
-	s.NoError(err)
-
-	commitment := baseCommitment
-	commitment.ID.BatchID = pendingBatch.ID
-
-	err = s.executionCtx.SubmitBatch(pendingBatch, []models.Commitment{commitment})
-	s.NoError(err)
-
-	batch, err := s.storage.GetBatch(models.MakeUint256(1))
-	s.NoError(err)
-	s.Equal(pendingBatch.Type, batch.Type)
-	s.NotEqual(common.Hash{}, batch.TransactionHash)
-	s.Equal(models.MakeUint256(1), batch.ID)
-	s.Equal(pendingBatch.PrevStateRoot, batch.PrevStateRoot)
-	s.Nil(batch.Hash)
-}
-
-func getCommitments(count int, batchID models.Uint256) []models.Commitment {
-	commitments := make([]models.Commitment, 0, count)
-	for i := 0; i < count; i++ {
-		commitment := baseCommitment
-		commitment.ID.BatchID = batchID
-		commitment.ID.IndexInBatch = uint8(i)
-
-		commitments = append(commitments, commitment)
-	}
-	return commitments
 }
 
 func (s *SubmitTransferBatchTestSuite) TestSubmitBatch_AddsCommitments() {
@@ -163,6 +112,45 @@ func (s *SubmitTransferBatchTestSuite) TestSubmitBatch_AddsCommitments() {
 	}
 }
 
+func (s *SubmitC2TBatchTestSuite) TestSubmitBatch_SubmitsCommitmentsOnChain() {
+	nextBatchID, err := s.client.Rollup.NextBatchID(nil)
+	s.NoError(err)
+	s.Equal(big.NewInt(1), nextBatchID)
+
+	commitment := baseCommitment
+	commitment.ID.BatchID = models.MakeUint256FromBig(*nextBatchID)
+
+	pendingBatch, err := s.executionCtx.NewPendingBatch(txtype.Create2Transfer)
+	s.NoError(err)
+	err = s.executionCtx.SubmitBatch(pendingBatch, []models.Commitment{commitment})
+	s.NoError(err)
+
+	s.client.Commit()
+
+	nextBatchID, err = s.client.Rollup.NextBatchID(nil)
+	s.NoError(err)
+	s.Equal(big.NewInt(2), nextBatchID)
+}
+
+func (s *SubmitC2TBatchTestSuite) TestSubmitBatch_StoresPendingBatchRecord() {
+	pendingBatch, err := s.executionCtx.NewPendingBatch(txtype.Create2Transfer)
+	s.NoError(err)
+
+	commitment := baseCommitment
+	commitment.ID.BatchID = pendingBatch.ID
+
+	err = s.executionCtx.SubmitBatch(pendingBatch, []models.Commitment{commitment})
+	s.NoError(err)
+
+	batch, err := s.storage.GetBatch(models.MakeUint256(1))
+	s.NoError(err)
+	s.Equal(pendingBatch.Type, batch.Type)
+	s.NotEqual(common.Hash{}, batch.TransactionHash)
+	s.Equal(models.MakeUint256(1), batch.ID)
+	s.Equal(pendingBatch.PrevStateRoot, batch.PrevStateRoot)
+	s.Nil(batch.Hash)
+}
+
 func (s *SubmitC2TBatchTestSuite) TestSubmitBatch_AddsCommitments() {
 	pendingBatch, err := s.executionCtx.NewPendingBatch(txtype.Create2Transfer)
 	s.NoError(err)
@@ -180,6 +168,18 @@ func (s *SubmitC2TBatchTestSuite) TestSubmitBatch_AddsCommitments() {
 		s.Equal(commitments[i], *commit)
 		s.Equal(batch.ID, commit.ID.BatchID)
 	}
+}
+
+func getCommitments(count int, batchID models.Uint256) []models.Commitment {
+	commitments := make([]models.Commitment, 0, count)
+	for i := 0; i < count; i++ {
+		commitment := baseCommitment
+		commitment.ID.BatchID = batchID
+		commitment.ID.IndexInBatch = uint8(i)
+
+		commitments = append(commitments, commitment)
+	}
+	return commitments
 }
 
 func TestSubmitTransferBatchTestSuite(t *testing.T) {
