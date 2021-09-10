4c909c8e6c5b27fec9e4e1bce0b7ced189c698e2
Michał Sieczkowski <michal@ethworks.io>
2021-09-10 20:09:47 +0200

    Split sync test suites across 3 files

---
+++ commander/executor/revert_batches_test.go
@@ -0,0 +1,79 @@
+package executor
+
+import (
+	"testing"
+
+	"github.com/Worldcoin/hubble-commander/models"
+	st "github.com/Worldcoin/hubble-commander/storage"
+	"github.com/Worldcoin/hubble-commander/testutils"
+	"github.com/stretchr/testify/suite"
+)
+
+type RevertBatchesTestSuite struct {
+	SyncTestSuite
+}
+
+func (s *RevertBatchesTestSuite) TestRevertBatches_RevertsState() {
+	initialStateRoot, err := s.storage.StateTree.Root()
+	s.NoError(err)
+
+	signTransfer(s.T(), &s.wallets[s.transfer.FromStateID], &s.transfer)
+	pendingBatch := createAndSubmitTransferBatch(s.Assertions, s.client, s.executionCtx, &s.transfer)
+
+	err = s.executionCtx.RevertBatches(pendingBatch)
+	s.NoError(err)
+
+	stateRoot, err := s.storage.StateTree.Root()
+	s.NoError(err)
+	s.Equal(*initialStateRoot, *stateRoot)
+
+	state0, err := s.storage.StateTree.Leaf(s.transfer.FromStateID)
+	s.NoError(err)
+	s.Equal(uint64(1000), state0.Balance.Uint64())
+
+	state1, err := s.storage.StateTree.Leaf(s.transfer.ToStateID)
+	s.NoError(err)
+	s.Equal(uint64(0), state1.Balance.Uint64())
+}
+
+func (s *RevertBatchesTestSuite) TestRevertBatches_ExcludesTransactionsFromCommitments() {
+	signTransfer(s.T(), &s.wallets[s.transfer.FromStateID], &s.transfer)
+	pendingBatch := createAndSubmitTransferBatch(s.Assertions, s.client, s.executionCtx, &s.transfer)
+
+	err := s.executionCtx.RevertBatches(pendingBatch)
+	s.NoError(err)
+
+	transfer, err := s.storage.GetTransfer(s.transfer.Hash)
+	s.NoError(err)
+	s.Nil(transfer.CommitmentID)
+}
+
+func (s *RevertBatchesTestSuite) TestRevertBatches_DeletesCommitmentsAndBatches() {
+	transfers := make([]models.Transfer, 2)
+	transfers[0] = s.transfer
+	transfers[1] = testutils.MakeTransfer(0, 1, 1, 200)
+
+	pendingBatches := make([]models.Batch, 2)
+	for i := range pendingBatches {
+		signTransfer(s.T(), &s.wallets[transfers[i].FromStateID], &transfers[i])
+		pendingBatches[i] = *createAndSubmitTransferBatch(s.Assertions, s.client, s.executionCtx, &transfers[i])
+	}
+
+	latestCommitment, err := s.executionCtx.storage.GetLatestCommitment()
+	s.NoError(err)
+	s.Equal(models.MakeUint256(2), latestCommitment.ID.BatchID)
+
+	err = s.executionCtx.RevertBatches(&pendingBatches[0])
+	s.NoError(err)
+
+	_, err = s.executionCtx.storage.GetLatestCommitment()
+	s.Equal(st.NewNotFoundError("commitment"), err)
+
+	batches, err := s.storage.GetBatchesInRange(nil, nil)
+	s.NoError(err)
+	s.Len(batches, 0)
+}
+
+func TestRevertBatchesTestSuite(t *testing.T) {
+	suite.Run(t, new(RevertBatchesTestSuite))
+}
