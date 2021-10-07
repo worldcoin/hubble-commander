b0ff0f85febdb38bf77ecb472856d076a395f689
Bartlomiej Tarczynski <51199571+b-tarczynski@users.noreply.github.com>
2021-10-07 14:54:11 +0200

    Store public keys in genesis.yaml (#367)
    * Store public keys in genesis.yaml (#356)
    
    * Add public key field to genesis accounts
    
    * Throw on invalid genesis account
    
    * Update genesis.yaml
    
    * Use PublicKey SetBytes in decodeRawGenesisAccounts
    
    * Validate that public keys match private keys when reading genesis.yml
    
    Co-authored-by: Daniel Izdebski <spidiritsu@gmail.com>
    Co-authored-by: Michał Sieczkowski <michal@ethworks.io>

--- config/genesis.go
+++ config/genesis.go
@@ -4,6 +4,7 @@ import (
 	"encoding/hex"
 	"io/ioutil"
 
+	"github.com/Worldcoin/hubble-commander/bls"
 	"github.com/Worldcoin/hubble-commander/models"
 	"gopkg.in/yaml.v2"
 )
@@ -27,19 +28,52 @@ func decodeRawGenesisAccounts(rawGenesisAccounts []models.RawGenesisAccount) ([]
 	genesisAccounts := make([]models.GenesisAccount, 0, len(rawGenesisAccounts))
 
 	for i := range rawGenesisAccounts {
-		var privateKey [32]byte
-
-		decodedPrivateKey, err := hex.DecodeString(rawGenesisAccounts[i].PrivateKey)
-		if err != nil {
-			return nil, err
-		}
-		copy(privateKey[:], decodedPrivateKey)
-
-		genesisAccounts = append(genesisAccounts, models.GenesisAccount{
-			PrivateKey: privateKey,
+		account := models.GenesisAccount{
+			PublicKey:  nil,
+			PrivateKey: nil,
 			Balance:    models.MakeUint256(rawGenesisAccounts[i].Balance),
-		})
+		}
+
+		if rawGenesisAccounts[i].PublicKey != "" {
+			decodedPublicKey, err := hex.DecodeString(rawGenesisAccounts[i].PublicKey)
+			if err != nil {
+				return nil, err
+			}
+			account.PublicKey = new(models.PublicKey)
+			err = account.PublicKey.SetBytes(decodedPublicKey)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		if rawGenesisAccounts[i].PrivateKey != "" {
+			decodedPrivateKey, err := hex.DecodeString(rawGenesisAccounts[i].PrivateKey)
+			if err != nil {
+				return nil, err
+			}
+			account.PrivateKey = &[32]byte{}
+			copy(account.PrivateKey[:], decodedPrivateKey)
+		}
+
+		if account.PrivateKey != nil && account.PublicKey != nil {
+			if err := validateKeysMatch(*account.PrivateKey, account.PublicKey); err != nil {
+				return nil, err
+			}
+		}
+
+		genesisAccounts = append(genesisAccounts, account)
 	}
 
 	return genesisAccounts, nil
+}
+
+func validateKeysMatch(privateKey [32]byte, publicKey *models.PublicKey) error {
+	derivedPublicKey, err := bls.PrivateToPublicKey(privateKey)
+	if err != nil {
+		return err
+	}
+	if *publicKey != *derivedPublicKey {
+		return NewErrNonMatchingKeys(publicKey.String())
+	}
+	return nil
 }
