c94c0cf98bba5503018ae0691e83763e378275a8
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-10-20 11:09:41 +0200

    Use proofer context in dipsuter

--- commander/disputer/context.go
+++ commander/disputer/context.go
@@ -1,15 +1,21 @@
 package disputer
 
 import (
+	"github.com/Worldcoin/hubble-commander/commander/proofer"
 	"github.com/Worldcoin/hubble-commander/eth"
 	st "github.com/Worldcoin/hubble-commander/storage"
 )
 
 type Context struct {
-	storage *st.Storage
-	client  *eth.Client
+	storage    *st.Storage
+	client     *eth.Client
+	prooferCtx *proofer.Context
 }
 
 func NewContext(storage *st.Storage, client *eth.Client) *Context {
-	return &Context{storage: storage, client: client}
+	return &Context{
+		storage:    storage,
+		client:     client,
+		prooferCtx: proofer.NewContext(storage),
+	}
 }
--- commander/disputer/dispute_signature.go
+++ commander/disputer/dispute_signature.go
@@ -3,13 +3,9 @@ package disputer
 import (
 	"fmt"
 
-	"github.com/Worldcoin/hubble-commander/encoder"
 	"github.com/Worldcoin/hubble-commander/eth"
 	"github.com/Worldcoin/hubble-commander/models"
 	"github.com/Worldcoin/hubble-commander/models/enums/batchtype"
-	st "github.com/Worldcoin/hubble-commander/storage"
-	"github.com/Worldcoin/hubble-commander/utils/merkletree"
-	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/pkg/errors"
 )
 
@@ -36,12 +32,12 @@ func (c *Context) disputeTransferSignature(
 	commitmentIndex int,
 	stateProofs []models.StateMerkleProof,
 ) error {
-	signatureProof, err := c.signatureProof(stateProofs)
+	signatureProof, err := c.prooferCtx.SignatureProof(stateProofs)
 	if err != nil {
 		return err
 	}
 
-	targetCommitmentProof, err := targetCommitmentInclusionProof(batch, uint32(commitmentIndex))
+	targetCommitmentProof, err := c.prooferCtx.TargetCommitmentInclusionProof(batch, uint32(commitmentIndex))
 	if err != nil {
 		return err
 	}
@@ -54,96 +50,15 @@ func (c *Context) disputeCreate2TransferSignature(
 	commitmentIndex int,
 	stateProofs []models.StateMerkleProof,
 ) error {
-	signatureProof, err := c.signatureProofWithReceiver(&batch.Commitments[commitmentIndex], stateProofs)
+	signatureProof, err := c.prooferCtx.SignatureProofWithReceiver(&batch.Commitments[commitmentIndex], stateProofs)
 	if err != nil {
 		return err
 	}
 
-	targetCommitmentProof, err := targetCommitmentInclusionProof(batch, uint32(commitmentIndex))
+	targetCommitmentProof, err := c.prooferCtx.TargetCommitmentInclusionProof(batch, uint32(commitmentIndex))
 	if err != nil {
 		return err
 	}
 
 	return c.client.DisputeSignatureCreate2Transfer(&batch.ID, batch.Hash, targetCommitmentProof, signatureProof)
-}
-
-func (c *Context) signatureProof(stateProofs []models.StateMerkleProof) (*models.SignatureProof, error) {
-	proof := &models.SignatureProof{
-		UserStates: stateProofs,
-		PublicKeys: make([]models.PublicKeyProof, 0, len(stateProofs)),
-	}
-
-	for i := range stateProofs {
-		publicKeyProof, err := c.publicKeyProof(stateProofs[i].UserState.PubKeyID)
-		if err != nil {
-			return nil, err
-		}
-		proof.PublicKeys = append(proof.PublicKeys, *publicKeyProof)
-	}
-	return proof, nil
-}
-
-func (c *Context) signatureProofWithReceiver(
-	commitment *encoder.DecodedCommitment,
-	stateProofs []models.StateMerkleProof,
-) (*models.SignatureProofWithReceiver, error) {
-	pubKeyIDs := encoder.DeserializeCreate2TransferPubKeyIDs(commitment.Transactions)
-
-	proof := &models.SignatureProofWithReceiver{
-		UserStates:         stateProofs,
-		SenderPublicKeys:   make([]models.PublicKeyProof, 0, len(stateProofs)),
-		ReceiverPublicKeys: make([]models.ReceiverPublicKeyProof, 0, len(stateProofs)),
-	}
-	for i := range stateProofs {
-		publicKeyProof, err := c.publicKeyProof(stateProofs[i].UserState.PubKeyID)
-		if err != nil {
-			return nil, err
-		}
-		receiverPublicKeyProof, err := c.receiverPublicKeyProof(pubKeyIDs[i])
-		if err != nil {
-			return nil, err
-		}
-
-		proof.SenderPublicKeys = append(proof.SenderPublicKeys, *publicKeyProof)
-		proof.ReceiverPublicKeys = append(proof.ReceiverPublicKeys, *receiverPublicKeyProof)
-	}
-	return proof, nil
-}
-
-func (c *Context) publicKeyProof(pubKeyID uint32) (*models.PublicKeyProof, error) {
-	account, err := c.storage.AccountTree.Leaf(pubKeyID)
-	if err != nil {
-		return nil, err
-	}
-	witness, err := c.storage.AccountTree.GetWitness(pubKeyID)
-	if err != nil {
-		return nil, err
-	}
-
-	return &models.PublicKeyProof{
-		PublicKey: &account.PublicKey,
-		Witness:   witness,
-	}, nil
-}
-
-func (c *Context) receiverPublicKeyProof(pubKeyID uint32) (*models.ReceiverPublicKeyProof, error) {
-	witness, err := c.storage.AccountTree.GetWitness(pubKeyID)
-	if err != nil {
-		return nil, err
-	}
-	account, err := c.storage.AccountTree.Leaf(pubKeyID)
-	if st.IsNotFoundError(err) {
-		return &models.ReceiverPublicKeyProof{
-			PublicKeyHash: merkletree.GetZeroHash(0),
-			Witness:       witness,
-		}, nil
-	}
-	if err != nil {
-		return nil, err
-	}
-
-	return &models.ReceiverPublicKeyProof{
-		PublicKeyHash: crypto.Keccak256Hash(account.PublicKey.Bytes()),
-		Witness:       witness,
-	}, nil
 }
