c6da0bb74069cf183bee08e56de66dc24f4433a9
Daniel Izdebski <spidiritsu@gmail.com>
2021-10-21 11:43:18 +0200

    Add comments to docker_init.sh

--- deployment/aws-staging/docker_init.sh
+++ deployment/aws-staging/docker_init.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Prepare directory for the AWS config and credentials
 mkdir /root/.aws
 
+# Create the AWS credentials file
 echo "[default]
 aws_access_key_id=${AWS_ACCESS_KEY_ID}
 aws_secret_access_key=${AWS_SECRET_ACCESS_KEY}
 " > /root/.aws/credentials
 
+# Create the AWS config file
 echo "[default]
 region=${AWS_REGION}
 output=json
@@ -14,6 +17,8 @@ output=json
 
 # Maybe verify that you have access to the aws bucket before doing anything further?
 
+# Run the script that initiates backups of the commander state data
 bash /root/scripts/start_commander_backups.sh
 
+# Run the cron daemon in the foreground
 crond -n
