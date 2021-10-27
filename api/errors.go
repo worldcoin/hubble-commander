2cdb5e88b79b00de554f1fbf35f9d047e5901a36
Michał Sieczkowski <michal@ethworks.io>
2021-10-27 17:43:01 +0200

    Extract sanitizeError to a separate file

--- api/errors.go
+++ api/errors.go
@@ -3,10 +3,7 @@ package api
 import (
 	"fmt"
 
-	"github.com/Worldcoin/hubble-commander/bls"
-	"github.com/Worldcoin/hubble-commander/db"
 	"github.com/pkg/errors"
-	log "github.com/sirupsen/logrus"
 )
 
 var (
@@ -129,63 +126,4 @@ func NewUnknownAPIError(err error) *APIError {
 	}
 
 	return NewAPIError(unknownAPIErrorCode, fmt.Sprintf("unknown error: %s", err.Error()))
-}
-
-type InternalToAPIError struct {
-	apiError        *APIError
-	commanderErrors []error
-}
-
-func NewInternalToAPIError(code int, message string, commanderErrors []error) *InternalToAPIError {
-	return &InternalToAPIError{
-		apiError:        NewAPIError(code, message),
-		commanderErrors: commanderErrors,
-	}
-}
-
-var commonErrors = []*InternalToAPIError{
-	// Badger
-	NewInternalToAPIError(
-		40000,
-		"an error occurred while saving data to the Badger database",
-		[]error{
-			db.ErrInconsistentItemsLength,
-			db.ErrInvalidKeyListLength,
-			db.ErrInvalidKeyListMetadataLength,
-		},
-	),
-	NewInternalToAPIError(40001, "an error occurred while iterating over Badger database", []error{db.ErrIteratorFinished}),
-	// BLS
-	NewInternalToAPIError(99004, "an error occurred while fetching the domain for signing", []error{bls.ErrInvalidDomainLength}),
-}
-
-func sanitizeError(err error, errMap map[error]*APIError) *APIError {
-	if log.IsLevelEnabled(log.DebugLevel) {
-		log.Debugf("Sanitizing error:\n%+v", err)
-	}
-
-	for k, v := range errMap {
-		if errors.Is(err, k) {
-			if log.IsLevelEnabled(log.DebugLevel) {
-				v.Data = fmt.Sprintf("%+v", err)
-			}
-			return v
-		}
-	}
-
-	return sanitizeCommonError(err, commonErrors)
-}
-
-func sanitizeCommonError(err error, errMap []*InternalToAPIError) *APIError {
-	for i := range errMap {
-		selectedErrMap := errMap[i]
-		for j := range selectedErrMap.commanderErrors {
-			commanderErr := selectedErrMap.commanderErrors[j]
-			if errors.Is(err, commanderErr) {
-				return errMap[i].apiError
-			}
-		}
-	}
-
-	return NewUnknownAPIError(err)
 }
