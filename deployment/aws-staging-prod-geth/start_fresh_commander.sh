030ee0560d10da6e104890ccc1b047a105c24813
Daniel Izdebski <spidiritsu@gmail.com>
2021-11-02 13:54:17 +0100

    Remove deployment directory

--- deployment/aws-staging-prod-geth/start_fresh_commander.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker-compose down -v
-docker-compose up mining-geth-init
-docker-compose up -d mining-geth
-docker-compose up api-geth-init
-docker-compose up -d api-geth
-docker-compose up deploy
-docker-compose up -d commander
--- deployment/aws-staging/check_data_usage.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo du -sh /var/lib/docker/volumes/aws-staging_badger-data-volume /var/lib/docker/volumes/aws-staging_postgres-data-volume /var/lib/docker/volumes/aws-staging_geth-data-volume
