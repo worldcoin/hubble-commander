2f6016883b55f1449a80ac5d2729e2f4fdc6b6ab
Michał Sieczkowski <michal@ethworks.io>
2021-11-04 14:45:22 +0100

    Simplify and remove a TODO

--- storage/deposit_sub_tree.go
+++ storage/deposit_sub_tree.go
@@ -12,7 +12,6 @@ func (s *DepositStorage) AddPendingDepositSubTree(subTree *models.PendingDeposit
 	return s.database.Badger.Upsert(subTree.ID, *subTree)
 }
 
-// TODO - replace with a proper getter when implementing submit deposit batches
 func (s *DepositStorage) GetPendingDepositSubTree(subTreeID models.Uint256) (*models.PendingDepositSubTree, error) {
 	var subTree models.PendingDepositSubTree
 	err := s.database.Badger.Get(subTreeID, &subTree)
@@ -39,10 +38,7 @@ func (s *DepositStorage) GetFirstPendingDepositSubTree() (subTree *models.Pendin
 	if err == db.ErrIteratorFinished {
 		return nil, errors.WithStack(NewNotFoundError("deposit sub tree"))
 	}
-	if err != nil {
-		return nil, err
-	}
-	return subTree, nil
+	return subTree, err
 }
 
 func (s *DepositStorage) DeletePendingDepositSubTrees(subTreeIDs ...models.Uint256) error {
