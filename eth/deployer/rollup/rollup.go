ce04e134ee519b02f86da238a84cf40e5f4efed9
Michał Sieczkowski <michal@ethworks.io>
2021-11-05 15:07:04 +0100

    Optimize DeployRollup to mine multiple txs in blocks

--- eth/deployer/rollup/rollup.go
+++ eth/deployer/rollup/rollup.go
@@ -20,6 +20,7 @@ import (
 	"github.com/Worldcoin/hubble-commander/eth/deployer"
 	"github.com/Worldcoin/hubble-commander/models"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 )
@@ -31,7 +32,7 @@ const (
 	DefaultMinGasLeft             = 10_000
 	DefaultMaxTxsPerCommit        = 32
 
-	costEstimatorAddress = "079d8077c465bd0bf0fc502ad2b846757e415661"
+	originalCostEstimatorAddress = "079d8077c465bd0bf0fc502ad2b846757e415661"
 )
 
 type DeploymentConfig struct {
@@ -76,6 +77,9 @@ type txHelperContracts struct {
 	TransferAddress        common.Address
 	MassMigrationAddress   common.Address
 	Create2TransferAddress common.Address
+	TransferTx             *types.Transaction
+	MassMigrationTx        *types.Transaction
+	Create2TransferTx      *types.Transaction
 	Transfer               *transfer.Transfer
 	MassMigration          *massmigration.MassMigration
 	Create2Transfer        *create2transfer.Create2Transfer
@@ -89,35 +93,45 @@ func DeployRollup(c chain.Connection) (*RollupContracts, error) {
 func DeployConfiguredRollup(c chain.Connection, cfg DeploymentConfig) (*RollupContracts, error) {
 	fillWithDefaults(&cfg.Params)
 
+	// Stage 1
 	err := deployMissing(&cfg.Dependencies, c)
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
 
+	// Stage 2
 	log.Println("Deploying TokenRegistry")
-	tokenRegistryAddress, tx, tokenRegistry, err := tokenregistry.DeployTokenRegistry(c.GetAccount(), c.GetBackend())
+	tokenRegistryAddress, tokenRegistryTx, tokenRegistry, err := tokenregistry.DeployTokenRegistry(c.GetAccount(), c.GetBackend())
 	if err != nil {
 		return nil, errors.WithStack(err)
-	}
-
-	_, err = chain.WaitToBeMined(c.GetBackend(), tx)
-	if err != nil {
-		return nil, err
 	}
 
 	log.Println("Deploying SpokeRegistry")
-	spokeRegistryAddress, tx, spokeRegistry, err := spokeregistry.DeploySpokeRegistry(c.GetAccount(), c.GetBackend())
+	spokeRegistryAddress, spokeRegistryTx, spokeRegistry, err := spokeregistry.DeploySpokeRegistry(c.GetAccount(), c.GetBackend())
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
 
-	_, err = chain.WaitToBeMined(c.GetBackend(), tx)
+	log.Println("Deploying BNPairingPrecompileCostEstimator")
+	costEstimatorAddress, costEstimatorDeployTx, costEstimator, err := estimator.DeployCostEstimator(c.GetAccount(), c.GetBackend())
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = chain.WaitForMultipleTxs(c.GetBackend(), *tokenRegistryTx, *spokeRegistryTx, *costEstimatorDeployTx)
 	if err != nil {
 		return nil, err
 	}
 
+	// Stage 3
+	log.Println("Initializing BNPairingPrecompileCostEstimator")
+	costEstimatorInitTx, err := costEstimator.Run(c.GetAccount())
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
 	log.Println("Deploying Vault")
-	vaultAddress, tx, vaultContract, err := vault.DeployVault(
+	vaultAddress, vaultTx, vaultContract, err := vault.DeployVault(
 		c.GetAccount(),
 		c.GetBackend(),
 		tokenRegistryAddress,
@@ -127,13 +141,14 @@ func DeployConfiguredRollup(c chain.Connection, cfg DeploymentConfig) (*RollupCo
 		return nil, errors.WithStack(err)
 	}
 
-	_, err = chain.WaitToBeMined(c.GetBackend(), tx)
+	_, err = chain.WaitForMultipleTxs(c.GetBackend(), *costEstimatorInitTx, *vaultTx)
 	if err != nil {
 		return nil, err
 	}
 
+	// Stage 4
 	log.Println("Deploying DepositManager")
-	depositManagerAddress, tx, depositManager, err := depositmanager.DeployDepositManager(
+	depositManagerAddress, depositManagerTx, depositManager, err := depositmanager.DeployDepositManager(
 		c.GetAccount(),
 		c.GetBackend(),
 		tokenRegistryAddress,
@@ -144,47 +159,26 @@ func DeployConfiguredRollup(c chain.Connection, cfg DeploymentConfig) (*RollupCo
 		return nil, errors.WithStack(err)
 	}
 
-	_, err = chain.WaitToBeMined(c.GetBackend(), tx)
-	if err != nil {
-		return nil, err
-	}
-
-	exampleTokenAddress, tx, _, err := exampletoken.DeployTestExampleToken(
-		c.GetAccount(),
-		c.GetBackend(),
-	)
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-
-	_, err = chain.WaitToBeMined(c.GetBackend(), tx)
-	if err != nil {
-		return nil, err
-	}
-
-	proofOfBurn, err := proofofburn.NewProofOfBurn(*cfg.Chooser, c.GetBackend())
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-
-	accountRegistry, err := accountregistry.NewAccountRegistry(*cfg.AccountRegistry, c.GetBackend())
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-
-	estimatorAddress, err := deployCostEstimator(c)
-	if err != nil {
-		return nil, err
-	}
-
 	var txHelpers *txHelperContracts
-	withReplacedCostEstimatorAddress(*estimatorAddress, func() {
+	withReplacedCostEstimatorAddress(costEstimatorAddress, func() {
 		txHelpers, err = deployTransactionHelperContracts(c)
 	})
 	if err != nil {
 		return nil, err
 	}
 
+	_, err = chain.WaitForMultipleTxs(
+		c.GetBackend(),
+		*depositManagerTx,
+		*txHelpers.TransferTx,
+		*txHelpers.MassMigrationTx,
+		*txHelpers.Create2TransferTx,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// Stage 5
 	log.Println("Deploying Rollup")
 	stateRoot := [32]byte{}
 	copy(stateRoot[:], cfg.GenesisStateRoot.Bytes())
@@ -212,14 +206,35 @@ func DeployConfiguredRollup(c chain.Connection, cfg DeploymentConfig) (*RollupCo
 		return nil, err
 	}
 
-	tx, err = depositManager.SetRollupAddress(c.GetAccount(), rollupAddress)
+	// Stage 6
+	log.Println("Setting Rollup address in DepositManager")
+	depositManagerInitTx, err := depositManager.SetRollupAddress(c.GetAccount(), rollupAddress)
 	if err != nil {
 		return nil, errors.WithStack(err)
 	}
 
-	_, err = chain.WaitToBeMined(c.GetBackend(), tx)
+	log.Println("Deploying TestExampleToken")
+	exampleTokenAddress, exampleTokenTx, _, err := exampletoken.DeployTestExampleToken(
+		c.GetAccount(),
+		c.GetBackend(),
+	)
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	_, err = chain.WaitForMultipleTxs(c.GetBackend(), *depositManagerInitTx, *exampleTokenTx)
 	if err != nil {
 		return nil, err
+	}
+
+	proofOfBurn, err := proofofburn.NewProofOfBurn(*cfg.Chooser, c.GetBackend())
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+
+	accountRegistry, err := accountregistry.NewAccountRegistry(*cfg.AccountRegistry, c.GetBackend())
+	if err != nil {
+		return nil, errors.WithStack(err)
 	}
 
 	return &RollupContracts{
@@ -242,64 +257,32 @@ func DeployConfiguredRollup(c chain.Connection, cfg DeploymentConfig) (*RollupCo
 	}, nil
 }
 
-func deployCostEstimator(c chain.Connection) (*common.Address, error) {
-	log.Println("Deploying BNPairingPrecompileCostEstimator")
-	estimatorAddress, tx, costEstimator, err := estimator.DeployCostEstimator(c.GetAccount(), c.GetBackend())
-	if err != nil {
-		return nil, errors.WithStack(err)
-	}
-
-	_, err = chain.WaitToBeMined(c.GetBackend(), tx)
-	if err != nil {
-		return nil, err
-	}
-
-	_, err = costEstimator.Run(c.GetAccount())
-	if err != nil {
-		return nil, err
-	}
-
-	return &estimatorAddress, nil
-}
-
 func deployTransactionHelperContracts(c chain.Connection) (*txHelperContracts, error) {
 	log.Println("Deploying Transfer")
-	transferAddress, tx, transferContract, err := transfer.DeployTransfer(c.GetAccount(), c.GetBackend())
+	transferAddress, transferTx, transferContract, err := transfer.DeployTransfer(c.GetAccount(), c.GetBackend())
 	if err != nil {
 		return nil, errors.WithStack(err)
-	}
-
-	_, err = chain.WaitToBeMined(c.GetBackend(), tx)
-	if err != nil {
-		return nil, err
 	}
 
 	log.Println("Deploying MassMigration")
-	massMigrationAddress, tx, massMigration, err := massmigration.DeployMassMigration(c.GetAccount(), c.GetBackend())
+	massMigrationAddress, massMigrationTx, massMigration, err := massmigration.DeployMassMigration(c.GetAccount(), c.GetBackend())
 	if err != nil {
 		return nil, errors.WithStack(err)
-	}
-
-	_, err = chain.WaitToBeMined(c.GetBackend(), tx)
-	if err != nil {
-		return nil, err
 	}
 
 	log.Println("Deploying Create2Transfer")
-	create2TransferAddress, tx, create2Transfer, err := create2transfer.DeployCreate2Transfer(c.GetAccount(), c.GetBackend())
+	create2TransferAddress, create2TransferTx, create2Transfer, err := create2transfer.DeployCreate2Transfer(c.GetAccount(), c.GetBackend())
 	if err != nil {
 		return nil, errors.WithStack(err)
-	}
-
-	_, err = chain.WaitToBeMined(c.GetBackend(), tx)
-	if err != nil {
-		return nil, err
 	}
 
 	return &txHelperContracts{
 		TransferAddress:        transferAddress,
 		MassMigrationAddress:   massMigrationAddress,
 		Create2TransferAddress: create2TransferAddress,
+		TransferTx:             transferTx,
+		MassMigrationTx:        massMigrationTx,
+		Create2TransferTx:      create2TransferTx,
 		Transfer:               transferContract,
 		MassMigration:          massMigration,
 		Create2Transfer:        create2Transfer,
@@ -354,9 +337,9 @@ func withReplacedCostEstimatorAddress(newCostEstimator common.Address, fn func()
 	originalCreate2TransferBin := create2transfer.Create2TransferBin
 	originalMassMigrationBin := massmigration.MassMigrationBin
 
-	transfer.TransferBin = strings.Replace(originalTransferBin, costEstimatorAddress, targetString, -1)
-	create2transfer.Create2TransferBin = strings.Replace(originalCreate2TransferBin, costEstimatorAddress, targetString, -1)
-	massmigration.MassMigrationBin = strings.Replace(originalMassMigrationBin, costEstimatorAddress, targetString, -1)
+	transfer.TransferBin = strings.Replace(originalTransferBin, originalCostEstimatorAddress, targetString, -1)
+	create2transfer.Create2TransferBin = strings.Replace(originalCreate2TransferBin, originalCostEstimatorAddress, targetString, -1)
+	massmigration.MassMigrationBin = strings.Replace(originalMassMigrationBin, originalCostEstimatorAddress, targetString, -1)
 
 	defer func() {
 		transfer.TransferBin = originalTransferBin
