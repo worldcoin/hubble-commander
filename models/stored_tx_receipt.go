8faec4c100be5d93125136591bfd2e8048e129d2
Michał Sieczkowski <michal@ethworks.io>
2021-11-18 18:38:40 +0100

    Make functions private

--- models/stored_tx_receipt.go
+++ models/stored_tx_receipt.go
@@ -51,7 +51,7 @@ func (t *StoredTxReceipt) SetBytes(data []byte) error {
 	if len(data) < storedTxReceiptBytesLength {
 		return ErrInvalidLength
 	}
-	commitmentID, err := DecodeCommitmentIDPointer(data[32:66])
+	commitmentID, err := decodeCommitmentIDPointer(data[32:66])
 	if err != nil {
 		return err
 	}
