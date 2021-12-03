150d7412e25acdb4877be8fc22746012a76702eb
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2021-12-03 08:23:34 +0100

    Add DecodedMassMigrationCommitment type

--- encoder/commitment.go
+++ encoder/commitment.go
@@ -32,3 +32,31 @@ func (c *DecodedCommitment) BodyHash(accountRoot common.Hash) *common.Hash {
 func (c *DecodedCommitment) LeafHash(accountRoot common.Hash) common.Hash {
 	return utils.HashTwo(c.StateRoot, *c.BodyHash(accountRoot))
 }
+
+type DecodedMassMigrationCommitment struct {
+	ID                models.CommitmentID
+	StateRoot         common.Hash
+	CombinedSignature models.Signature
+	Meta              models.MassMigrationMeta
+	WithdrawRoot      common.Hash
+	Transactions      []byte
+}
+
+func (c *DecodedMassMigrationCommitment) BodyHash(accountRoot common.Hash) *common.Hash {
+	arr := make([]byte, 32+64+32+32+32+32+32+len(c.Transactions))
+
+	copy(arr[0:32], accountRoot.Bytes())
+	copy(arr[32:96], c.CombinedSignature.Bytes())
+	binary.BigEndian.PutUint32(arr[124:128], c.Meta.SpokeID)
+	copy(arr[128:160], c.WithdrawRoot.Bytes())
+	copy(arr[160:192], c.Meta.TokenID.Bytes())
+	copy(arr[192:224], c.Meta.Amount.Bytes())
+	binary.BigEndian.PutUint32(arr[252:256], c.Meta.FeeReceiver)
+	copy(arr[256:], c.Transactions)
+
+	return ref.Hash(crypto.Keccak256Hash(arr))
+}
+
+func (c *DecodedMassMigrationCommitment) LeafHash(accountRoot common.Hash) common.Hash {
+	return utils.HashTwo(c.StateRoot, *c.BodyHash(accountRoot))
+}
