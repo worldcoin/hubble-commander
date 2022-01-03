8d8a0bca5ed01b9eaf7918a75785adf040be8336
Michał Sieczkowski <michal@ethworks.io>
2022-01-03 11:00:31 +0100

    Rename MM batch submission gas limit config option (#509)
    * Rename default gas limit const
    
    * Rename RollupConfig field
    
    * Rename ClientConfig field
    
    * Remove default value comments
    
    * Rename env var
    
    * Reformat

--- eth/client.go
+++ eth/client.go
@@ -31,15 +31,15 @@ type NewClientParams struct {
 }
 
 type ClientConfig struct {
-	TxTimeout                            *time.Duration  // default 60s
-	StakeAmount                          *models.Uint256 // default 0.1 ether
-	TransferBatchSubmissionGasLimit      *uint64         // default 400_000 gas
-	C2TBatchSubmissionGasLimit           *uint64         // default 500_000 gas
-	MassMigrationBatchSubmissionGasLimit *uint64         // default 550_000 gas
-	DepositBatchSubmissionGasLimit       *uint64         // default 220_000 gas
-	TransitionDisputeGasLimit            *uint64         // default 5_000_000 gas
-	SignatureDisputeGasLimit             *uint64         // default 7_500_000 gas
-	BatchAccountRegistrationGasLimit     *uint64         // default 8_000_000 gas
+	TxTimeout                        *time.Duration
+	StakeAmount                      *models.Uint256
+	TransferBatchSubmissionGasLimit  *uint64
+	C2TBatchSubmissionGasLimit       *uint64
+	MMBatchSubmissionGasLimit        *uint64
+	DepositBatchSubmissionGasLimit   *uint64
+	TransitionDisputeGasLimit        *uint64
+	SignatureDisputeGasLimit         *uint64
+	BatchAccountRegistrationGasLimit *uint64
 }
 
 type Client struct {
@@ -121,7 +121,7 @@ func fillWithDefaults(c *ClientConfig) {
 		c.TxTimeout = ref.Duration(60 * time.Second)
 	}
 	if c.StakeAmount == nil {
-		c.StakeAmount = models.NewUint256(1e17)
+		c.StakeAmount = models.NewUint256(1e17) // default 0.1 ether
 	}
 	if c.TransferBatchSubmissionGasLimit == nil {
 		c.TransferBatchSubmissionGasLimit = ref.Uint64(config.DefaultTransferBatchSubmissionGasLimit)
@@ -129,8 +129,8 @@ func fillWithDefaults(c *ClientConfig) {
 	if c.C2TBatchSubmissionGasLimit == nil {
 		c.C2TBatchSubmissionGasLimit = ref.Uint64(config.DefaultC2TBatchSubmissionGasLimit)
 	}
-	if c.MassMigrationBatchSubmissionGasLimit == nil {
-		c.MassMigrationBatchSubmissionGasLimit = ref.Uint64(config.DefaultMassMigrationBatchSubmissionGasLimit)
+	if c.MMBatchSubmissionGasLimit == nil {
+		c.MMBatchSubmissionGasLimit = ref.Uint64(config.DefaultMMBatchSubmissionGasLimit)
 	}
 	if c.DepositBatchSubmissionGasLimit == nil {
 		c.DepositBatchSubmissionGasLimit = ref.Uint64(config.DefaultDepositBatchSubmissionGasLimit)
