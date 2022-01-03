4ce1a2f79dccfacff5811fcf0aee9d920cea6482
Daniel Izdebski <spidiritsu@gmail.com>
2022-01-03 19:50:46 +0100

    Add MassMigration commitment type

---
+++ models/mm_commitment.go
@@ -0,0 +1,35 @@
+package models
+
+import (
+	"github.com/Worldcoin/hubble-commander/utils"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+type MMCommitment struct {
+	CommitmentBase
+	FeeReceiver       uint32
+	CombinedSignature Signature
+	BodyHash          *common.Hash
+	Meta              *MassMigrationMeta
+}
+
+func (c *MMCommitment) GetBodyHash() common.Hash {
+	return *c.BodyHash
+}
+
+func (c *MMCommitment) LeafHash() common.Hash {
+	return utils.HashTwo(c.PostStateRoot, *c.BodyHash)
+}
+
+type MMCommitmentWithTxs struct {
+	MMCommitment
+	Transactions []byte
+}
+
+func (c *MMCommitmentWithTxs) SetBodyHash(accountRoot common.Hash) {
+	c.BodyHash = calcBodyHash(c.FeeReceiver, c.CombinedSignature, c.Transactions, accountRoot.Bytes())
+}
+
+func (c *MMCommitmentWithTxs) CalcBodyHash(accountRoot common.Hash) *common.Hash {
+	return calcBodyHash(c.FeeReceiver, c.CombinedSignature, c.Transactions, accountRoot.Bytes())
+}
