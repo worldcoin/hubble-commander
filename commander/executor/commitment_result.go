6be4102818b9ce90411a87bf667270c37f34e8e6
Daniel Izdebski <spidiritsu@gmail.com>
2022-01-10 22:23:40 +0100

    Use new commitment type in CreateCommitmentResult and remove BatchData

--- commander/executor/commitment_result.go
+++ commander/executor/commitment_result.go
@@ -2,13 +2,12 @@ package executor
 
 import (
 	"github.com/Worldcoin/hubble-commander/models"
-	"github.com/ethereum/go-ethereum/common"
 )
 
 type CreateCommitmentResult interface {
 	AppliedTxs() models.GenericTransactionArray
 	PendingAccounts() []models.AccountLeaf
-	Commitment() *models.TxCommitmentWithTxs
+	Commitment() models.CommitmentWithTxs
 }
 
 type CreateTransferCommitmentResult struct {
@@ -24,7 +23,7 @@ func (c *CreateTransferCommitmentResult) PendingAccounts() []models.AccountLeaf 
 	return []models.AccountLeaf{}
 }
 
-func (c *CreateTransferCommitmentResult) Commitment() *models.TxCommitmentWithTxs {
+func (c *CreateTransferCommitmentResult) Commitment() models.CommitmentWithTxs {
 	return c.commitment
 }
 
@@ -42,13 +41,13 @@ func (c *CreateC2TCommitmentResult) PendingAccounts() []models.AccountLeaf {
 	return c.pendingAccounts
 }
 
-func (c *CreateC2TCommitmentResult) Commitment() *models.TxCommitmentWithTxs {
+func (c *CreateC2TCommitmentResult) Commitment() models.CommitmentWithTxs {
 	return c.commitment
 }
 
 type CreateMassMigrationCommitmentResult struct {
 	appliedTxs models.MassMigrationArray
-	commitment *models.TxCommitmentWithTxs
+	commitment *models.MMCommitmentWithTxs
 }
 
 func (c *CreateMassMigrationCommitmentResult) AppliedTxs() models.GenericTransactionArray {
@@ -59,82 +58,6 @@ func (c *CreateMassMigrationCommitmentResult) PendingAccounts() []models.Account
 	return []models.AccountLeaf{}
 }
 
-func (c *CreateMassMigrationCommitmentResult) Commitment() *models.TxCommitmentWithTxs {
+func (c *CreateMassMigrationCommitmentResult) Commitment() models.CommitmentWithTxs {
 	return c.commitment
-}
-
-type BatchData interface {
-	Commitments() []models.TxCommitmentWithTxs
-	Metas() []models.MassMigrationMeta
-	WithdrawRoots() []common.Hash
-	Len() int
-	AddCommitment(commitment *models.TxCommitmentWithTxs)
-	AddMeta(meta *models.MassMigrationMeta)
-	AddWithdrawRoot(withdrawRoot common.Hash)
-}
-
-type TxBatchData struct {
-	commitments []models.TxCommitmentWithTxs
-}
-
-func (c *TxBatchData) Commitments() []models.TxCommitmentWithTxs {
-	return c.commitments
-}
-
-func (c *TxBatchData) Metas() []models.MassMigrationMeta {
-	panic("Meta cannot be invoked on TxBatchData")
-}
-
-func (c *TxBatchData) WithdrawRoots() []common.Hash {
-	panic("WithdrawRoots cannot be invoked on TxBatchData")
-}
-
-func (c *TxBatchData) Len() int {
-	return len(c.commitments)
-}
-
-func (c *TxBatchData) AddCommitment(commitment *models.TxCommitmentWithTxs) {
-	c.commitments = append(c.commitments, *commitment)
-}
-
-func (c *TxBatchData) AddMeta(_ *models.MassMigrationMeta) {
-	panic("AddMeta cannot be invoked on TxBatchData")
-}
-
-func (c *TxBatchData) AddWithdrawRoot(_ common.Hash) {
-	panic("AddWithdrawRoot cannot be invoked on TxBatchData")
-}
-
-type MassMigrationBatchData struct {
-	commitments   []models.TxCommitmentWithTxs
-	metas         []models.MassMigrationMeta
-	withdrawRoots []common.Hash
-}
-
-func (c *MassMigrationBatchData) Commitments() []models.TxCommitmentWithTxs {
-	return c.commitments
-}
-
-func (c *MassMigrationBatchData) Metas() []models.MassMigrationMeta {
-	return c.metas
-}
-
-func (c *MassMigrationBatchData) WithdrawRoots() []common.Hash {
-	return c.withdrawRoots
-}
-
-func (c *MassMigrationBatchData) Len() int {
-	return len(c.commitments)
-}
-
-func (c *MassMigrationBatchData) AddCommitment(commitment *models.TxCommitmentWithTxs) {
-	c.commitments = append(c.commitments, *commitment)
-}
-
-func (c *MassMigrationBatchData) AddMeta(meta *models.MassMigrationMeta) {
-	c.metas = append(c.metas, *meta)
-}
-
-func (c *MassMigrationBatchData) AddWithdrawRoot(withdrawRoot common.Hash) {
-	c.withdrawRoots = append(c.withdrawRoots, withdrawRoot)
 }
