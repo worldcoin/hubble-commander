2a1d7f1de10b2b595f94963b035b9afc4c8a82b4
Remco Bloemen <remco@wicked.ventures>
2022-01-11 06:57:24 -0800

    Use go-ethereum RPC over LibP2P (#519)
    * Use gorpc
    
    * Refactor
    
    * Use libp2p-http
    
    * Use raw libp2p-gostreams
    
    * Fix test
    
    * RSA is deprecated
    
    * Fix close
    
    * Catch error in ServeListener

--- p2p/peer.go
+++ p2p/peer.go
@@ -2,45 +2,40 @@ package p2p
 
 import (
 	"context"
-	"crypto/rand"
 	"fmt"
+	"net"
+	"strconv"
+
 	"github.com/ethereum/go-ethereum/rpc"
-	"github.com/inconshreveable/muxado"
 	"github.com/libp2p/go-libp2p"
 	"github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/host"
-	"github.com/libp2p/go-libp2p-core/network"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/peerstore"
+	"github.com/libp2p/go-libp2p-core/protocol"
+	p2pstream "github.com/libp2p/go-libp2p-gostream"
 	"github.com/multiformats/go-multiaddr"
-	"io"
-	"log"
-	netRpc "net/rpc"
-	"net/rpc/jsonrpc"
-	"strconv"
-	"sync"
-	"time"
 )
 
-type Peer struct {
-	host             host.Host
-	handleConnection func(conn Connection)
-}
+var protocolID = protocol.ID("/worldcoin/rpc/1.0.0")
 
-type Connection struct {
-	server *rpc.Server
-	client *netRpc.Client
+type Peer struct {
+	host     host.Host
+	server   *rpc.Server
+	listener net.Listener
 }
 
 // NewPeer creates a new transaction exchange with P2P capabilities.
 // port - is the TCP port to listen for incoming P2P connections on. Pass 0 to let OS pick the port.
 // privateKey - is the identity of the P2P instance
-func NewPeer(port int, handleConnection func(conn Connection), privateKey crypto.PrivKey) (*Peer, error) {
+func NewPeer(port int, privateKey crypto.PrivKey) (*Peer, error) {
+	// Create a listening address
 	sourceMultiAddr, err := multiaddr.NewMultiaddr(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", port))
 	if err != nil {
 		return nil, err
 	}
 
+	// Create a libp2p Host
 	h, err := libp2p.New(
 		libp2p.ListenAddrs(sourceMultiAddr),
 		libp2p.Identity(privateKey),
@@ -49,135 +44,72 @@ func NewPeer(port int, handleConnection func(conn Connection), privateKey crypto
 		return nil, err
 	}
 
-	p := &Peer{
-		host:             h,
-		handleConnection: handleConnection,
-	}
+	// Start a libp2p-gostream based Geth JSON-RPC server
+	server := rpc.NewServer()
+	listener, _ := p2pstream.Listen(h, protocolID)
+	go func() {
+		err := server.ServeListener(listener)
+		if err != nil {
+			// TODO: How do we handle errors in threads?
+			panic(err)
+		}
+	}()
 
-	h.SetStreamHandler("/worldcoin/1.0.0", func(stream network.Stream) {
-		p.handleStream(stream)
-	})
+	p := &Peer{
+		host:     h,
+		server:   server,
+		listener: listener,
+	}
 
 	return p, nil
 }
 
-func NewPeerWithRandomKey(port int, handleConnection func(conn Connection)) (*Peer, error) {
-	// Creates a new RSA key pair for this host.
-	prvKey, _, err := crypto.GenerateKeyPairWithReader(crypto.RSA, 2048, rand.Reader)
+func NewPeerWithRandomKey(port int) (*Peer, error) {
+	// Creates a new key pair for this host.
+	prvKey, _, err := crypto.GenerateKeyPair(crypto.Ed25519, -1)
 	if err != nil {
 		return nil, err
 	}
 
-	return NewPeer(port, handleConnection, prvKey)
+	return NewPeer(port, prvKey)
 }
 
-type conn struct {
-	io.Reader
-	io.Writer
+func (p *Peer) Dial(destination string) (*rpc.Client, error) {
+	// Register remote peer and get url
+	dest, err := p.PeerID(destination)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create a libp2p-gostream connection
+	ctx := context.Background()
+	conn, err := p2pstream.Dial(ctx, p.host, *dest, protocolID)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create libp2p-gostream based Geth JSON-RPC client
+	return rpc.DialIO(ctx, conn, conn)
 }
 
-func (c conn) Close() error {
-	return nil
-}
-
-func (c conn) SetWriteDeadline(time time.Time) error {
-	return nil
-}
-
-func (p *Peer) handleStream(stream network.Stream) {
-	fmt.Println("handleStream")
-
-	mux := muxado.Server(stream, nil)
-
-	p.handleMuxed(mux)
-
-	println("handleStream end")
-}
-
-func (p *Peer) handleDial(stream network.Stream) {
-	fmt.Println("handleDial")
-
-	mux := muxado.Client(stream, nil)
-
-	time.Sleep(100 * time.Millisecond)
-
-	p.handleMuxed(mux)
-
-	println("handleDial end")
-}
-
-func (p *Peer) handleMuxed(mux muxado.Session) {
-
-	conn := Connection{}
-
-	wg := sync.WaitGroup{}
-	wg.Add(2)
-
-	go func() {
-		fmt.Println("Waiting to accept")
-		serverStream, err := mux.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println("Accepted")
-
-		conn.server = rpc.NewServer()
-
-		go conn.server.ServeCodec(rpc.NewCodec(serverStream), 0)
-		wg.Done()
-	}()
-
-	time.Sleep(200 * time.Millisecond)
-
-	go func() {
-		clientStream, err := mux.Open()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		fmt.Println("Opened")
-
-		conn.client = jsonrpc.NewClient(clientStream)
-		wg.Done()
-	}()
-
-	wg.Wait()
-
-	fmt.Println("handleConnection")
-
-	p.handleConnection(conn)
-
-	fmt.Println("handleConnection end")
-}
-
-func (p *Peer) Dial(destination string) error {
+func (p *Peer) PeerID(destination string) (*peer.ID, error) {
 	// Turn the destination into a multiaddr.
 	maddr, err := multiaddr.NewMultiaddr(destination)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// Extract the peer ID from the multiaddr.
 	info, err := peer.AddrInfoFromP2pAddr(maddr)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// Add the destination's peer multiaddress in the peerstore.
 	// This will be used during connection and stream creation by libp2p.
 	p.host.Peerstore().AddAddrs(info.ID, info.Addrs, peerstore.PermanentAddrTTL)
 
-	// Start a stream with the destination.
-	// Multiaddress of the destination peer is fetched from the peerstore using 'peerId'.
-	s, err := p.host.NewStream(context.Background(), info.ID, "/worldcoin/1.0.0")
-	if err != nil {
-		return err
-	}
-
-	go p.handleDial(s)
-
-	println("Dial end")
-	return nil
+	return &info.ID, nil
 }
 
 func (p *Peer) ListenAddr() string {
@@ -194,5 +126,11 @@ func (p *Peer) ListenAddr() string {
 }
 
 func (p *Peer) Close() error {
+	p.server.Stop()
+	// TODO: Closing the listener crashes the server thread, despite it being stopped
+	// err := p.listener.Close()
+	// if err != nil {
+	// 	return err
+	// }
 	return p.host.Close()
 }
--- p2p/peer_test.go
+++ p2p/peer_test.go
@@ -1,52 +1,47 @@
 package p2p
 
 import (
-	"fmt"
-	"github.com/stretchr/testify/require"
-	"log"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
-type TestRpc struct {
+type TestRPC struct {
 }
 
 type IntParam struct {
-	value int
+	// Note that Value needs to be uppercase so `json.Marshall` can see it
+	Value int
 }
 
-func (t *TestRpc) Double(arg IntParam) IntParam {
-	return IntParam{arg.value * 2}
+func (t *TestRPC) Double(arg IntParam) IntParam {
+	return IntParam{arg.Value * 2}
 }
 
 func TestPeer(t *testing.T) {
-	alice, err := NewPeerWithRandomKey(0, func(conn Connection) {
-		err := conn.server.RegisterName("test", &TestRpc{})
-		if err != nil {
-			panic(err)
-		}
-	})
+	// Create node Alice serving test_double
+	alice, err := NewPeerWithRandomKey(0)
+	require.NoError(t, err)
+	err = alice.server.RegisterName("test", &TestRPC{})
 	require.NoError(t, err)
 
-	done := make(chan bool)
-
-	bob, err := NewPeerWithRandomKey(0, func(conn Connection) {
-		var res IntParam
-		err := conn.client.Call("test_double", IntParam{3}, &res)
-		fmt.Println(res.value)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		done <- true
-	})
+	// Create node Bob
+	bob, err := NewPeerWithRandomKey(0)
 	require.NoError(t, err)
 
+	// Have Bob call Alice
+	var res IntParam
 	addr := alice.ListenAddr()
-	err = bob.Dial(addr)
+	client, err := bob.Dial(addr)
 	require.NoError(t, err)
+	err = client.Call(&res, "test_double", IntParam{3})
+	require.NoError(t, err)
+	require.Equal(t, 6, res.Value)
+	err = client.Call(&res, "test_double", IntParam{5})
+	require.NoError(t, err)
+	require.Equal(t, 10, res.Value)
 
-	<-done
-
-	//require.NoError(t, alice.Close())
-	//require.NoError(t, bob.Close())
+	// Tear down
+	require.NoError(t, alice.Close())
+	require.NoError(t, bob.Close())
 }
