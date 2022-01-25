5fcbee6d8d12b1dc0a0e9da1e36ba3f5dae7f854
Maxim Sidorov <86226467+max-sidorov@users.noreply.github.com>
2022-01-25 16:51:04 +0300

    Withdraw proposer stakes (#512)
    * add PendingStakeWithdrawalStorage
    
    * realise adding to the PendingStakeWithdrawal storage
    
    * add syncStakeWithdrawals WIP
    
    * update syncStakeWithdrawals and add tests
    
    * add committer to the batch struct
    
    * add GetPendingStakeWithdrawalByFinalisationBlock function to the storage
    
    * add committer comparing in syncBatches
    
    * fix test function TestGetPendingStakeWithdrawalByFinalisationBlock_AddAndGet
    
    * fix tests
    
    * fix SetupTest for NewBlockLoopTestSuite
    
    * fix linter
    
    * remove redundant error checking
    
    * add withdrawRemainingStakes function
    
    * fix lint
    
    * update data structures docs
    
    * create syncPendingStakeWithdrawal function and move it before workersContext.Done()
    
    * Extract and use a StakeWithdrawEvent constant
    
    * add stack trace info to the c.syncStakeWithdrawals error
    
    * add badgerhold index for the finalization block field
    
    * implement encode/decode methods for PendingStakeWithdrawal model
    
    * add GetReadyStateWithdrawals to the storage
    
    * update GetReadyStateWithdrawals method and a test function
    
    * remove committer field from the batch model
    
    * create new BatchWithMeta type and use it in getBatchDetails
    
    * fix linter issue
    
    * move withdrawRemainingStakes call
    
    * integration tests WIP
    
    * Draft test improvement
    
    * fix block number for  withdrawRemainingStake and RemovePendingStakeWithdrawal removing
    
    * move integration tests to another file
    
    * fix types in tests
    
    * change gasLimit for TestWithdrawProcess
    
    * add recreating storage for test
    
    * rename tests
    
    * update tests
    
    * Remove unused index
    
    * Replace BatchWithMeta type with ContractBatch that does not duplicate fields
    
    * Add a TODO
    
    * Rename a variable in tests
    
    * Move gas limit setting inside getTransactor method
    
    * Reorder functions in new block loop
    
    * Rename variable and avoid copying in a loop
    
    * Do small refactorings in tests
    
    * Rerun CI
    
    Co-authored-by: Michał Sieczkowski <michal@ethworks.io>

--- eth/get_batch.go
+++ eth/get_batch.go
@@ -6,6 +6,14 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
+type ContractBatch struct {
+	ID   models.Uint256
+	Hash common.Hash
+	models.BatchMeta
+}
+
+// TODO Replace usages of GetBatch with GetContractBatch
+
 func (c *Client) GetBatch(batchID *models.Uint256) (*models.Batch, error) {
 	batch, err := c.Rollup.GetBatch(nil, batchID.ToBig())
 	if err != nil {
@@ -18,5 +26,19 @@ func (c *Client) GetBatch(batchID *models.Uint256) (*models.Batch, error) {
 		Hash:              &hash,
 		Type:              meta.BatchType,
 		FinalisationBlock: &meta.FinaliseOn,
+	}, nil
+}
+
+func (c *Client) GetContractBatch(batchID *models.Uint256) (*ContractBatch, error) {
+	batch, err := c.Rollup.GetBatch(nil, batchID.ToBig())
+	if err != nil {
+		return nil, err
+	}
+	meta := encoder.DecodeMeta(batch.Meta)
+	hash := common.BytesToHash(batch.CommitmentRoot[:])
+	return &ContractBatch{
+		ID:        *batchID,
+		Hash:      hash,
+		BatchMeta: meta,
 	}, nil
 }
