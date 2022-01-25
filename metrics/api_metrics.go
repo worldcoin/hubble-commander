8d7fcceb95e66467eff907b09a172c7c3ce89528
Daniel Izdebski <spidiritsu@gmail.com>
2022-01-25 14:09:21 +0100

    Tweak metrics buckets

--- metrics/api_metrics.go
+++ metrics/api_metrics.go
@@ -19,6 +19,8 @@ func (c *CommanderMetrics) initializeAPIMetrics() {
 		Help:      "API requests duration",
 		Buckets: []float64{
 			0.0,
+			5.0,
+			10.0,
 			25.0,
 			50.0,
 			75.0,
@@ -26,16 +28,6 @@ func (c *CommanderMetrics) initializeAPIMetrics() {
 			150.0,
 			200.0,
 			250.0,
-			300.0,
-			350.0,
-			400.0,
-			450.0,
-			500.0,
-			600.0,
-			700.0,
-			800.0,
-			900.0,
-			1000.0,
 		},
 	})
 
