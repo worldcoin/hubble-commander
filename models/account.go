455dc63f93177b0ef0b3f7460efc94ce2dd60fad
Michał Sieczkowski <michal@ethworks.io>
2022-01-27 17:36:59 +0100

    Don't index AccountLeafs with zero public keys

--- models/account.go
+++ models/account.go
@@ -2,11 +2,18 @@ package models
 
 import (
 	"encoding/binary"
+	"fmt"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/pkg/errors"
+	bh "github.com/timshannon/badgerhold/v4"
 )
 
-var AccountLeafPrefix = GetBadgerHoldPrefix(AccountLeaf{})
+var (
+	AccountLeafName                = GetTypeName(AccountLeaf{})
+	AccountLeafPrefix              = GetBadgerHoldPrefix(AccountLeaf{})
+	errInvalidAccountLeafIndexType = fmt.Errorf("invalid models.AccountLeaf index type")
+)
 
 type AccountLeaf struct {
 	PubKeyID  uint32
@@ -16,6 +23,31 @@ type AccountLeaf struct {
 type AccountNode struct {
 	MerklePath MerklePath
 	DataHash   common.Hash
+}
+
+// Type implements badgerhold.Storer
+func (a AccountLeaf) Type() string {
+	return string(AccountLeafName)
+}
+
+// Indexes implements badgerhold.Storer
+// We're adding a lot of Accounts with ZeroPublicKey to the database as a result of the way C2T processing is currently implemented.
+// See the usages of ZeroPublicKey for more context.
+func (a AccountLeaf) Indexes() map[string]bh.Index {
+	return map[string]bh.Index{
+		"PublicKey": {
+			IndexFunc: func(_ string, value interface{}) ([]byte, error) {
+				v, err := interfaceToAccountLeaf(value)
+				if err != nil {
+					return nil, err
+				}
+				if v.PublicKey == ZeroPublicKey {
+					return nil, nil
+				}
+				return v.PublicKey.Bytes(), nil
+			},
+		},
+	}
 }
 
 func (a *AccountLeaf) Bytes() []byte {
@@ -28,4 +60,16 @@ func (a *AccountLeaf) Bytes() []byte {
 func (a *AccountLeaf) SetBytes(data []byte) error {
 	a.PubKeyID = binary.BigEndian.Uint32(data[0:4])
 	return a.PublicKey.SetBytes(data[4:132])
+}
+
+func interfaceToAccountLeaf(value interface{}) (*AccountLeaf, error) {
+	p, ok := value.(*AccountLeaf)
+	if ok {
+		return p, nil
+	}
+	v, ok := value.(AccountLeaf)
+	if ok {
+		return &v, nil
+	}
+	return nil, errors.WithStack(errInvalidAccountLeafIndexType)
 }
