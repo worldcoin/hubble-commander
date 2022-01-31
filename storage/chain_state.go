3e2de751152158db429b6c2ad47a78ecfad961e1
Remco Bloemen <remco@wicked.ventures>
2022-01-31 05:41:43 -0800

    Add ThreadSanitizer (#550)
    * Run tests with ThreadSanitizer
    
    * Fix race between .Commit() and .Close()
    
    * Add default path for E2E accounts file
    
    * Remove resetCommander from Stop function
    
    * Use atomic load and store in ChainStateStorage
    
    * Create new slice in newSeekPrefix
    
    * Fix race condition in workers
    
    * Use atomic values in commander
    
    * Rename running to active
    
    * Resolve race condition between StartAndWait and Stop
    
    * Disable lint for lifecycle
    
    * Remove tests that don't wait for batch to be mined
    
    * Add an explanatory comment
    
    * Shorten a usage of isRollupLoopActive() in test
    
    * Remove test that can introduce race conditions
    
    * Replace manualStop with closeOnce
    
    * Add a test for newSeekPrefix
    
    Co-authored-by: Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
    Co-authored-by: Michał Sieczkowski <michal@ethworks.io>

--- storage/chain_state.go
+++ storage/chain_state.go
@@ -1,6 +1,8 @@
 package storage
 
 import (
+	"sync/atomic"
+
 	"github.com/Worldcoin/hubble-commander/models"
 	"github.com/pkg/errors"
 	bh "github.com/timshannon/badgerhold/v4"
@@ -9,20 +11,23 @@ import (
 type ChainStateStorage struct {
 	database          *Database
 	latestBlockNumber uint32
-	syncedBlock       *uint64
+	syncedBlock       uint64
 }
 
 func NewChainStateStorage(database *Database) *ChainStateStorage {
-	return &ChainStateStorage{
-		database: database,
-	}
+	storage := &ChainStateStorage{database: database}
+	atomic.StoreUint32(&storage.latestBlockNumber, 0)
+	atomic.StoreUint64(&storage.syncedBlock, 0)
+
+	return storage
 }
 
 func (s *ChainStateStorage) copyWithNewDatabase(database *Database) *ChainStateStorage {
-	newChainStateStorage := *s
-	newChainStateStorage.database = database
+	newChainStateStorage := &ChainStateStorage{database: database}
+	atomic.StoreUint32(&newChainStateStorage.latestBlockNumber, s.GetLatestBlockNumber())
+	atomic.StoreUint64(&newChainStateStorage.syncedBlock, 0)
 
-	return &newChainStateStorage
+	return newChainStateStorage
 }
 
 func (s *ChainStateStorage) GetChainState() (*models.ChainState, error) {
