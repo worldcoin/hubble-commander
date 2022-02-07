d14a06577667d0b8e86a4ac32b994cf73b77b1e7
Maxim Sidorov <max.sidorov@ottofeller.com>
2022-02-07 16:55:02 +0300

    update sender

--- commander/tracker/sender.go
+++ commander/tracker/sender.go
@@ -4,7 +4,10 @@ import (
 	"github.com/Worldcoin/hubble-commander/eth"
 	"github.com/Worldcoin/hubble-commander/models"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/pkg/errors"
 )
+
+var ErrIncorrectTxParamsType = errors.New("incorrect tx params type")
 
 type TxsSender struct {
 	client       *eth.Client
@@ -14,25 +17,25 @@ type TxsSender struct {
 func newTxRequestsSender(ethClient *eth.Client) *TxsSender {
 	return &TxsSender{
 		client:       ethClient,
-		requestsChan: make(chan *txRequest),
+		requestsChan: make(chan *txRequest, 32),
 	}
 }
 
-func (t *TxsSender) SubmitTransfersBatchRequest(batchID *models.Uint256, commitments []models.CommitmentWithTxs) (*types.Transaction, error) {
+func (t *TxsSender) SubmitTransfersBatch(batchID *models.Uint256, commitments []models.CommitmentWithTxs) (*types.Transaction, error) {
 	return t.sendTxRequest(&submitTransfersBatchRequestParams{
 		batchID:     batchID,
 		commitments: commitments,
 	})
 }
 
-func (t *TxsSender) SubmitCreate2TransfersBatchRequest(batchID *models.Uint256, commitments []models.CommitmentWithTxs) (*types.Transaction, error) {
+func (t *TxsSender) SubmitCreate2TransfersBatch(batchID *models.Uint256, commitments []models.CommitmentWithTxs) (*types.Transaction, error) {
 	return t.sendTxRequest(&submitCreate2TransfersBatchRequestParams{
 		batchID:     batchID,
 		commitments: commitments,
 	})
 }
 
-func (t *TxsSender) SubmitMassMigrationsBatchRequest(batchID *models.Uint256, commitments []models.CommitmentWithTxs) (*types.Transaction, error) {
+func (t *TxsSender) SubmitMassMigrationsBatch(batchID *models.Uint256, commitments []models.CommitmentWithTxs) (*types.Transaction, error) {
 	return t.sendTxRequest(&submitMassMigrationsBatchRequestParams{
 		batchID:     batchID,
 		commitments: commitments,
@@ -45,23 +48,28 @@ func (t *TxsSender) WithdrawStakeRequest(batchID *models.Uint256) (*types.Transa
 	})
 }
 
-func (t *TxsSender) sendTransaction(request *txRequest) (tx *types.Transaction, err error) {
-	switch params := request.params.(type) {
-	case *submitTransfersBatchRequestParams:
-		tx, err = t.client.SubmitTransfersBatch(params.batchID, params.commitments)
-	case *submitCreate2TransfersBatchRequestParams:
-		tx, err = t.client.SubmitCreate2TransfersBatch(params.batchID, params.commitments)
-	case *submitMassMigrationsBatchRequestParams:
-		tx, err = t.client.SubmitMassMigrationsBatch(params.batchID, params.commitments)
-	case *withdrawStakeRequestParams:
-		tx, err = t.client.WithdrawStake(params.batchID)
-	}
+func (t *TxsSender) sendRequest(request *txRequest) (*types.Transaction, error) {
+	tx, err := t.sendTransaction(request.params)
 	request.responseChannel <- txRequestResponse{Tx: tx, Err: err}
 	return tx, err
 }
 
+func (t *TxsSender) sendTransaction(params interface{}) (*types.Transaction, error) {
+	switch typedParams := params.(type) {
+	case *submitTransfersBatchRequestParams:
+		return t.client.SubmitTransfersBatch(typedParams.batchID, typedParams.commitments)
+	case *submitCreate2TransfersBatchRequestParams:
+		return t.client.SubmitCreate2TransfersBatch(typedParams.batchID, typedParams.commitments)
+	case *submitMassMigrationsBatchRequestParams:
+		return t.client.SubmitMassMigrationsBatch(typedParams.batchID, typedParams.commitments)
+	case *withdrawStakeRequestParams:
+		return t.client.WithdrawStake(typedParams.batchID)
+	}
+	return nil, ErrIncorrectTxParamsType
+}
+
 func (t *TxsSender) sendTxRequest(params interface{}) (*types.Transaction, error) {
-	respChan := make(chan txRequestResponse)
+	respChan := make(chan txRequestResponse, 1)
 	t.requestsChan <- &txRequest{
 		params:          params,
 		responseChannel: respChan,
