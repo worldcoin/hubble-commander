6d3068b028d83ca6278b23d169e64a74f4c6bfa9
Maxim Sidorov <max.sidorov@ottofeller.com>
2022-02-08 15:04:03 +0300

    remove GetBatch method

--- eth/get_batch.go
+++ eth/get_batch.go
@@ -21,23 +21,6 @@ func (cb *ContractBatch) ToModelBatch() *models.Batch {
 	}
 }
 
-// TODO Replace usages of GetBatch with GetContractBatch
-
-func (c *Client) GetBatch(batchID *models.Uint256) (*models.Batch, error) {
-	batch, err := c.Rollup.GetBatch(nil, batchID.ToBig())
-	if err != nil {
-		return nil, err
-	}
-	meta := encoder.DecodeMeta(batch.Meta)
-	hash := common.BytesToHash(batch.CommitmentRoot[:])
-	return &models.Batch{
-		ID:                *batchID,
-		Hash:              &hash,
-		Type:              meta.BatchType,
-		FinalisationBlock: &meta.FinaliseOn,
-	}, nil
-}
-
 func (c *Client) GetContractBatch(batchID *models.Uint256) (*ContractBatch, error) {
 	batch, err := c.Rollup.GetBatch(nil, batchID.ToBig())
 	if err != nil {
