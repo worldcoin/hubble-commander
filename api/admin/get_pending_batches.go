d907ad0dd47ac6d023e956f2c8bc39e9cc686c08
Daniel Izdebski <spidiritsu@gmail.com>
2022-02-10 14:42:18 +0100

    Sort commitment txs in GetPendingBatches admin API method (#582)
    * Sort commitment txs in GetPendingBatches admin API method
    
    * Handle the case for deposit batches

--- api/admin/get_pending_batches.go
+++ api/admin/get_pending_batches.go
@@ -3,6 +3,7 @@ package admin
 import (
 	"context"
 
+	"github.com/Worldcoin/hubble-commander/commander/executor"
 	"github.com/Worldcoin/hubble-commander/models"
 	"github.com/Worldcoin/hubble-commander/models/dto"
 	"github.com/Worldcoin/hubble-commander/models/enums/batchtype"
@@ -48,6 +49,13 @@ func (a *API) getCommitments(batch *models.Batch) ([]dto.PendingCommitment, erro
 		if err != nil {
 			return nil, err
 		}
+
+		if txs != nil {
+			// TODO remove when new primary key for transactions with transaction index is implement
+			txQueue := executor.NewTxQueue(txs)
+			txs = txQueue.PickTxsForCommitment()
+		}
+
 		dtoCommitments = append(dtoCommitments, dto.PendingCommitment{
 			Commitment:   commitments[i],
 			Transactions: txs,
