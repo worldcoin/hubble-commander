840e9c68bd6ff4db843bc09a7df1112b1c270e48
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2022-02-14 16:34:55 +0200

    Add mempool (#579)
    * Add pseudocode
    
    * Add and test mempool creation
    
    * Test getExecutableTxs
    
    * Add and test transaction replacement
    
    * Read txs from storage in NewMempool
    
    * Add test functions that returns pointer to tx
    
    * Update NewMempool test
    
    * Test GetExecutableTxs
    
    * Reorder methods
    
    * Optimise NewMempool
    
    * Make GetExecutableTxs public
    
    * Extract nonExecutableIndex
    
    * Make AddOrReplace public
    
    * Work on AddOrReplace implementation
    
    * Add additional tests for AddOrReplace
    
    * Rename userTxsMap to buckets
    
    * Implement GetNextExecutableTx
    
    * Add test for IgnoreUserTxs
    
    * Improve existing tests
    
    * Optimize AddOrReplace to iterate once
    
    * Implement ResetExecutableIndices
    
    * Rename GetNextExecutableTx to GetExecutableTx
    
    * Extract common getExecutableTx
    
    * Implement RemoveTxs
    
    * Remove empty buckets
    
    * Lint
    
    Co-authored-by: Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
    Co-authored-by: Michał Sieczkowski <michal@ethworks.io>

---
+++ commander/mempool/mempool.go
@@ -0,0 +1,263 @@
+package mempool
+
+import (
+	"fmt"
+	"sort"
+
+	"github.com/Worldcoin/hubble-commander/models"
+	"github.com/Worldcoin/hubble-commander/models/enums/txtype"
+	st "github.com/Worldcoin/hubble-commander/storage"
+	"github.com/pkg/errors"
+)
+
+const nonExecutableIndex = -1
+
+var ErrTxReplacementFailed = fmt.Errorf("new transaction didn't meet replace condition")
+
+// Mempool is a data structure that queues pending transactions.
+//
+// Transactions in Mempool are tracked for each sender separately.
+// They can be divided into _executable_ and _non-executable_ categories.
+//
+// Mempool is persisted between Rollup Loop iterations.
+type Mempool struct {
+	buckets map[uint32]*txBucket // storing pointers in the map so that data is mutable
+}
+
+type txBucket struct {
+	txs             []models.GenericTransaction // "executable" and "non-executable" txs
+	nonce           uint64                      // user nonce
+	executableIndex int                         // index of next executable tx from txs
+}
+
+func NewMempool(storage *st.Storage) (*Mempool, error) {
+	txs, err := storage.GetAllPendingTransactions()
+	if err != nil {
+		return nil, err
+	}
+
+	mempool := &Mempool{
+		buckets: map[uint32]*txBucket{},
+	}
+
+	mempool.initTxs(txs)
+	err = mempool.initBuckets(storage)
+	if err != nil {
+		return nil, err
+	}
+
+	return mempool, nil
+}
+
+func (m *Mempool) initTxs(txs models.GenericTransactionArray) {
+	for i := 0; i < txs.Len(); i++ {
+		tx := txs.At(i)
+
+		bucket := m.getOrInitBucket(tx.GetFromStateID(), 0)
+		bucket.txs = append(bucket.txs, tx)
+	}
+}
+
+func (m *Mempool) initBuckets(storage *st.Storage) error {
+	for stateID, bucket := range m.buckets {
+		stateLeaf, err := storage.StateTree.Leaf(stateID)
+		if err != nil {
+			return err
+		}
+
+		bucket.nonce = stateLeaf.Nonce.Uint64()
+		sort.Slice(bucket.txs, func(i, j int) bool {
+			txA := bucket.txs[i].GetBase()
+			txB := bucket.txs[j].GetBase()
+			return txA.Nonce.Cmp(&txB.Nonce) < 0
+		})
+
+		firstNonce := bucket.txs[0].GetNonce()
+		if firstNonce.EqN(bucket.nonce) {
+			bucket.executableIndex = 0
+		}
+	}
+	return nil
+}
+
+func (m *Mempool) GetExecutableTx(txType txtype.TransactionType, stateID uint32) models.GenericTransaction {
+	return m.getExecutableTx(txType, m.buckets[stateID])
+}
+
+func (m *Mempool) GetExecutableTxs(txType txtype.TransactionType) []models.GenericTransaction {
+	result := make([]models.GenericTransaction, 0)
+	for _, bucket := range m.buckets {
+		tx := m.getExecutableTx(txType, bucket)
+		if tx != nil {
+			result = append(result, tx)
+		}
+	}
+	return result
+}
+
+func (m *Mempool) getExecutableTx(txType txtype.TransactionType, bucket *txBucket) models.GenericTransaction {
+	if bucket.executableIndex == nonExecutableIndex || bucket.executableIndex >= len(bucket.txs) {
+		return nil
+	}
+
+	currentTx := bucket.txs[bucket.executableIndex]
+	if currentTx.Type() != txType {
+		return nil
+	}
+
+	bucket.executableIndex++
+	if bucket.executableIndex >= len(bucket.txs) {
+		bucket.executableIndex = nonExecutableIndex
+		return currentTx
+	}
+
+	currentNonce := bucket.nonce + uint64(bucket.executableIndex)
+	nextTx := bucket.txs[bucket.executableIndex]
+	if !nextTx.GetBase().Nonce.EqN(currentNonce) {
+		bucket.executableIndex = nonExecutableIndex
+	}
+
+	return currentTx
+}
+
+func (m *Mempool) AddOrReplace(newTx models.GenericTransaction, senderNonce uint64) error {
+	bucket := m.getOrInitBucket(newTx.GetFromStateID(), senderNonce)
+	return bucket.addOrReplace(newTx)
+}
+
+func replaceCondition(previousTx, newTx models.GenericTransaction) bool {
+	return newTx.GetBase().Fee.Cmp(&previousTx.GetBase().Fee) > 0
+}
+
+func (m *Mempool) getOrInitBucket(stateID uint32, currentNonce uint64) *txBucket {
+	bucket, present := m.buckets[stateID]
+	if !present {
+		bucket = &txBucket{
+			txs:             make([]models.GenericTransaction, 0, 1),
+			nonce:           currentNonce,
+			executableIndex: nonExecutableIndex,
+		}
+		m.buckets[stateID] = bucket
+	}
+	return bucket
+}
+
+func (b *txBucket) addOrReplace(newTx models.GenericTransaction) error {
+	newTxNonce := &newTx.GetBase().Nonce
+	for i, tx := range b.txs {
+		if newTxNonce.Eq(&tx.GetBase().Nonce) {
+			return b.replace(i, newTx)
+		}
+
+		if newTxNonce.Cmp(&b.txs[i].GetBase().Nonce) < 0 {
+			b.insertAndSetIndex(i, newTx)
+			return nil
+		}
+	}
+	b.insertAndSetIndex(len(b.txs), newTx)
+	return nil
+}
+
+func (b *txBucket) replace(index int, newTx models.GenericTransaction) error {
+	if !replaceCondition(b.txs[index], newTx) {
+		return errors.WithStack(ErrTxReplacementFailed)
+	}
+	b.txs[index] = newTx
+	return nil
+}
+
+func (b *txBucket) insertAndSetIndex(index int, newTx models.GenericTransaction) {
+	b.insertAt(index, newTx)
+	nonce := &newTx.GetBase().Nonce
+	if index == 0 && nonce.EqN(b.nonce) {
+		b.executableIndex = 0
+	}
+}
+
+func (b *txBucket) insertAt(index int, tx models.GenericTransaction) {
+	if index == len(b.txs) {
+		b.txs = append(b.txs, tx)
+		return
+	}
+
+	b.txs = append(b.txs[:index+1], b.txs[index:]...)
+	b.txs[index] = tx
+}
+
+func (m *Mempool) IgnoreUserTxs(stateID uint32) {
+	m.buckets[stateID].executableIndex = nonExecutableIndex
+}
+
+func (m *Mempool) ResetExecutableIndices() {
+	for _, bucket := range m.buckets {
+		bucket.setExecutableIndex()
+	}
+}
+
+func (b *txBucket) setExecutableIndex() {
+	if len(b.txs) == 0 {
+		b.executableIndex = nonExecutableIndex
+		return
+	}
+
+	firstNonce := b.txs[0].GetNonce()
+	if firstNonce.EqN(b.nonce) {
+		b.executableIndex = 0
+		return
+	}
+	b.executableIndex = nonExecutableIndex
+}
+
+func (m *Mempool) RemoveTxs(successfulTxs, failedTxs []models.GenericTransaction) {
+	m.removeSuccessfulTxs(successfulTxs)
+	m.removeFailedTxs(failedTxs)
+}
+
+func (m *Mempool) removeSuccessfulTxs(txs []models.GenericTransaction) {
+	userNonces := collectNonces(txs)
+
+	for senderID, nonce := range userNonces {
+		bucket := m.buckets[senderID]
+		successfulTxs := nonce - bucket.nonce + 1
+		bucket.txs = bucket.txs[successfulTxs:]
+
+		if len(bucket.txs) == 0 {
+			delete(m.buckets, senderID)
+		}
+
+		bucket.nonce += successfulTxs
+		bucket.setExecutableIndex()
+	}
+}
+
+func (m *Mempool) removeFailedTxs(txs []models.GenericTransaction) {
+	userNonces := collectNonces(txs)
+
+	for senderID, nonce := range userNonces {
+		bucket := m.buckets[senderID]
+		failedTxs := nonce - bucket.nonce + 1
+		bucket.txs = bucket.txs[failedTxs:]
+
+		if len(bucket.txs) == 0 {
+			delete(m.buckets, senderID)
+		}
+
+		bucket.executableIndex = nonExecutableIndex
+	}
+}
+
+func collectNonces(txs []models.GenericTransaction) map[uint32]uint64 {
+	userNonces := map[uint32]uint64{}
+	for _, tx := range txs {
+		senderID := tx.GetBase().FromStateID
+		userNonces[senderID] = maxUint64(tx.GetBase().Nonce.Uint64(), userNonces[senderID])
+	}
+	return userNonces
+}
+
+func maxUint64(a, b uint64) uint64 {
+	if a > b {
+		return a
+	}
+	return b
+}
---
+++ commander/mempool/mempool_test.go
@@ -0,0 +1,312 @@
+package mempool
+
+import (
+	"testing"
+
+	"github.com/Worldcoin/hubble-commander/models"
+	"github.com/Worldcoin/hubble-commander/models/enums/txtype"
+	st "github.com/Worldcoin/hubble-commander/storage"
+	"github.com/Worldcoin/hubble-commander/testutils"
+	"github.com/Worldcoin/hubble-commander/utils/ref"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+)
+
+type MempoolTestSuite struct {
+	*require.Assertions
+	suite.Suite
+	storage             *st.TestStorage
+	initialTransactions []models.GenericTransaction
+}
+
+func (s *MempoolTestSuite) SetupSuite() {
+	s.Assertions = require.New(s.T())
+}
+
+func (s *MempoolTestSuite) SetupTest() {
+	var err error
+	s.storage, err = st.NewTestStorage()
+	s.NoError(err)
+
+	// no need to shuffle initial transactions as they are retrieved from DB sorted by tx hashes which are random
+	s.initialTransactions = []models.GenericTransaction{
+		s.newTransfer(0, 10), // 0 - executable
+		s.newTransfer(0, 11), // 1
+		s.newTransfer(0, 13), // 2 - non-executable
+
+		s.newTransfer(1, 11), // 3 - non-executable
+		s.newTransfer(1, 12), // 4
+
+		s.newTransfer(2, 15), // 5 - executable
+		s.newTransfer(2, 16), // 6
+
+		s.newC2T(3, 10), // 7 - executable
+		s.newC2T(3, 11), // 8
+	}
+
+	err = s.storage.BatchAddTransaction(models.GenericArray(s.initialTransactions))
+	s.NoError(err)
+
+	s.setUserStates(map[uint32]uint64{
+		0: 10,
+		1: 10,
+		2: 15,
+		3: 10,
+	})
+}
+
+func (s *MempoolTestSuite) TearDownTest() {
+	err := s.storage.Teardown()
+	s.NoError(err)
+}
+
+func (s *MempoolTestSuite) TestNewMempool_InitsBucketsCorrectly() {
+	mempool, err := NewMempool(s.storage.Storage)
+	s.NoError(err)
+
+	s.Len(mempool.buckets, 4)
+	s.Equal(mempool.buckets[0].txs, s.initialTransactions[0:3])
+	s.Equal(mempool.buckets[1].txs, s.initialTransactions[3:5])
+	s.Equal(mempool.buckets[2].txs, s.initialTransactions[5:7])
+	s.Equal(mempool.buckets[3].txs, s.initialTransactions[7:9])
+
+	s.EqualValues(mempool.buckets[0].nonce, 10)
+	s.EqualValues(mempool.buckets[1].nonce, 10)
+	s.EqualValues(mempool.buckets[2].nonce, 15)
+	s.EqualValues(mempool.buckets[3].nonce, 10)
+
+	s.Equal(mempool.buckets[0].executableIndex, 0)
+	s.Equal(mempool.buckets[1].executableIndex, nonExecutableIndex)
+	s.Equal(mempool.buckets[2].executableIndex, 0)
+	s.Equal(mempool.buckets[3].executableIndex, 0)
+}
+
+func (s *MempoolTestSuite) TestGetExecutableTx_ReturnsNextExecutableTxOfGivenStateID() {
+	mempool, err := NewMempool(s.storage.Storage)
+	s.NoError(err)
+
+	tx := mempool.GetExecutableTx(txtype.Transfer, 0)
+	s.Equal(s.initialTransactions[0], tx)
+}
+
+func (s *MempoolTestSuite) TestGetExecutableTx_NoMoreTxs() {
+	mempool, err := NewMempool(s.storage.Storage)
+	s.NoError(err)
+
+	_ = mempool.GetExecutableTx(txtype.Create2Transfer, 3)
+	_ = mempool.GetExecutableTx(txtype.Create2Transfer, 3)
+	tx := mempool.GetExecutableTx(txtype.Create2Transfer, 3)
+	s.Nil(tx)
+	s.Equal(nonExecutableIndex, mempool.buckets[3].executableIndex)
+}
+
+func (s *MempoolTestSuite) TestGetExecutableTx_NoMoreExecutableTxs() {
+	mempool, err := NewMempool(s.storage.Storage)
+	s.NoError(err)
+
+	_ = mempool.GetExecutableTx(txtype.Transfer, 0)
+	_ = mempool.GetExecutableTx(txtype.Transfer, 0)
+	tx := mempool.GetExecutableTx(txtype.Transfer, 0)
+	s.Nil(tx)
+	s.Equal(nonExecutableIndex, mempool.buckets[0].executableIndex)
+}
+
+func (s *MempoolTestSuite) TestGetExecutableTx_NoExecutableTxsOfGivenType() {
+	mempool, err := NewMempool(s.storage.Storage)
+	s.NoError(err)
+
+	tx := mempool.GetExecutableTx(txtype.Create2Transfer, 0)
+	s.Nil(tx)
+	s.Equal(0, mempool.buckets[0].executableIndex)
+}
+
+func (s *MempoolTestSuite) TestGetExecutableTxs_ReturnsExecutableTxsOfCorrectType() {
+	mempool, err := NewMempool(s.storage.Storage)
+	s.NoError(err)
+
+	executable := mempool.GetExecutableTxs(txtype.Transfer)
+	s.Len(executable, 2)
+	s.Contains(executable, s.initialTransactions[0])
+	s.Contains(executable, s.initialTransactions[5])
+
+	executable = mempool.GetExecutableTxs(txtype.Create2Transfer)
+	s.Len(executable, 1)
+	s.Contains(executable, s.initialTransactions[7])
+}
+
+func (s *MempoolTestSuite) TestGetExecutableTxs_UpdatesExecutableIndices() {
+	mempool, err := NewMempool(s.storage.Storage)
+	s.NoError(err)
+
+	mempool.GetExecutableTxs(txtype.Transfer)
+	s.Equal(1, mempool.buckets[0].executableIndex)
+	s.Equal(1, mempool.buckets[2].executableIndex)
+
+	mempool.GetExecutableTxs(txtype.Create2Transfer)
+	s.Equal(1, mempool.buckets[3].executableIndex)
+}
+
+func (s *MempoolTestSuite) TestAddOrReplace_AppendsNewTxToBucketList() {
+	mempool, err := NewMempool(s.storage.Storage)
+	s.NoError(err)
+
+	tx := s.newTransfer(0, 14)
+	err = mempool.AddOrReplace(tx, 10)
+	s.NoError(err)
+
+	bucket := mempool.buckets[0]
+	lastTxInBucket := bucket.txs[len(bucket.txs)-1]
+	s.Equal(tx, lastTxInBucket)
+}
+
+func (s *MempoolTestSuite) TestAddOrReplace_InsertsNewTxInTheMiddleOfBucketList() {
+	mempool, err := NewMempool(s.storage.Storage)
+	s.NoError(err)
+
+	tx := s.newTransfer(0, 12)
+	err = mempool.AddOrReplace(tx, 10)
+	s.NoError(err)
+
+	bucket := mempool.buckets[0]
+	s.Equal(tx, bucket.txs[2])
+}
+
+func (s *MempoolTestSuite) TestAddOrReplace_ReplacesTx() {
+	mempool, err := NewMempool(s.storage.Storage)
+	s.NoError(err)
+
+	tx := s.newTransfer(0, 11)
+	tx.Fee = models.MakeUint256(20)
+	err = mempool.AddOrReplace(tx, 10)
+	s.NoError(err)
+
+	bucket := mempool.buckets[0]
+	s.Equal(tx, bucket.txs[1])
+}
+
+func (s *MempoolTestSuite) TestAddOrReplace_ReturnsErrorOnFeeTooLowToReplace() {
+	mempool, err := NewMempool(s.storage.Storage)
+	s.NoError(err)
+
+	tx := s.newTransfer(0, 11)
+	err = mempool.AddOrReplace(tx, 10)
+	s.ErrorIs(err, ErrTxReplacementFailed)
+}
+
+func (s *MempoolTestSuite) TestIgnoreUserTxs() {
+	mempool, err := NewMempool(s.storage.Storage)
+	s.NoError(err)
+
+	mempool.IgnoreUserTxs(0)
+	s.Equal(nonExecutableIndex, mempool.buckets[0].executableIndex)
+}
+
+func (s *MempoolTestSuite) TestResetExecutableIndices() {
+	mempool, err := NewMempool(s.storage.Storage)
+	s.NoError(err)
+
+	mempool.buckets[0].executableIndex = 2
+	mempool.buckets[2].executableIndex = nonExecutableIndex
+
+	mempool.ResetExecutableIndices()
+	s.Equal(0, mempool.buckets[0].executableIndex)
+	s.Equal(0, mempool.buckets[2].executableIndex)
+}
+
+func (s *MempoolTestSuite) TestRemoveTxs_RemovesTxsFromMempool() {
+	mempool, err := NewMempool(s.storage.Storage)
+	s.NoError(err)
+
+	successfulTxs, failedTxs := s.modifyMempool(mempool)
+
+	mempool.RemoveTxs(successfulTxs, failedTxs)
+	s.Len(mempool.buckets[0].txs, 1)
+	s.Len(mempool.buckets[1].txs, 2)
+	s.Len(mempool.buckets[2].txs, 1)
+	s.Len(mempool.buckets[3].txs, 2)
+}
+
+func (s *MempoolTestSuite) TestRemoveTxs_IncrementsUserNonces() {
+	mempool, err := NewMempool(s.storage.Storage)
+	s.NoError(err)
+
+	successfulTxs, failedTxs := s.modifyMempool(mempool)
+
+	mempool.RemoveTxs(successfulTxs, failedTxs)
+	s.EqualValues(12, mempool.buckets[0].nonce)
+	s.EqualValues(10, mempool.buckets[1].nonce)
+	s.EqualValues(15, mempool.buckets[2].nonce)
+	s.EqualValues(10, mempool.buckets[3].nonce)
+}
+
+func (s *MempoolTestSuite) TestRemoveTxs_SetsExecutableIndices() {
+	mempool, err := NewMempool(s.storage.Storage)
+	s.NoError(err)
+
+	successfulTxs, failedTxs := s.modifyMempool(mempool)
+
+	mempool.RemoveTxs(successfulTxs, failedTxs)
+	s.EqualValues(nonExecutableIndex, mempool.buckets[0].executableIndex)
+	s.EqualValues(nonExecutableIndex, mempool.buckets[1].executableIndex)
+	s.EqualValues(nonExecutableIndex, mempool.buckets[2].executableIndex)
+	s.EqualValues(0, mempool.buckets[3].executableIndex)
+}
+
+func (s *MempoolTestSuite) TestRemoveTxs_RemovesEmptyBucket() {
+	mempool, err := NewMempool(s.storage.Storage)
+	s.NoError(err)
+
+	successfulTxs := []models.GenericTransaction{
+		mempool.GetExecutableTx(txtype.Create2Transfer, 3),
+		mempool.GetExecutableTx(txtype.Create2Transfer, 3),
+		mempool.GetExecutableTx(txtype.Transfer, 2),
+	}
+	failedTxs := []models.GenericTransaction{
+		mempool.GetExecutableTx(txtype.Transfer, 2),
+	}
+
+	mempool.RemoveTxs(successfulTxs, failedTxs)
+	s.NotContains(mempool.buckets, 3)
+	s.NotContains(mempool.buckets, 2)
+}
+
+func (s *MempoolTestSuite) modifyMempool(mempool *Mempool) (successfulTxs, failedTxs []models.GenericTransaction) {
+	txs := mempool.GetExecutableTxs(txtype.Transfer)
+	successfulTxs = make([]models.GenericTransaction, 0, 1)
+	failedTxs = make([]models.GenericTransaction, 0, 1)
+	for _, tx := range txs {
+		if tx.GetFromStateID() == 0 {
+			successfulTxs = append(successfulTxs, tx)
+		} else {
+			failedTxs = append(failedTxs, tx)
+		}
+	}
+
+	tx := mempool.GetExecutableTx(txtype.Transfer, 0)
+	successfulTxs = append(successfulTxs, tx)
+	return successfulTxs, failedTxs
+}
+
+func (s *MempoolTestSuite) setUserStates(nonces map[uint32]uint64) {
+	for stateID, nonce := range nonces {
+		_, err := s.storage.StateTree.Set(stateID, &models.UserState{
+			PubKeyID: 0,
+			TokenID:  models.MakeUint256(uint64(stateID)),
+			Balance:  models.MakeUint256(1000),
+			Nonce:    models.MakeUint256(nonce),
+		})
+		s.NoError(err)
+	}
+}
+
+func (s *MempoolTestSuite) newTransfer(from uint32, nonce uint64) *models.Transfer {
+	return testutils.NewTransfer(from, 1, nonce, 100)
+}
+
+func (s *MempoolTestSuite) newC2T(from uint32, nonce uint64) *models.Create2Transfer {
+	return testutils.NewCreate2Transfer(from, ref.Uint32(1), nonce, 100, nil)
+}
+
+func TestMempoolTestSuite(t *testing.T) {
+	suite.Run(t, new(MempoolTestSuite))
+}
