cde9964d2922f8d371f82c17d00456fff121fbd5
Michał Sieczkowski <michal@ethworks.io>
2022-02-15 13:21:24 +0100

    Log migration progress

--- commander/migrate.go
+++ commander/migrate.go
@@ -9,71 +9,87 @@ import (
 	"github.com/Worldcoin/hubble-commander/models"
 	"github.com/Worldcoin/hubble-commander/models/dto"
 	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 )
 
 var errMissingBootstrapNodeURL = fmt.Errorf("bootstrap node URL is required for migration mode")
 
 func (c *Commander) migrate() error {
-	if c.cfg.Bootstrap.BootstrapNodeURL == nil {
+	nodeURL := c.cfg.Bootstrap.BootstrapNodeURL
+	if nodeURL == nil {
 		return errors.WithStack(errMissingBootstrapNodeURL)
 	}
+	log.Printf("Migration mode is on, syncing data from commander instance running at %s\n", *nodeURL)
 
-	hubbleClient := client.NewHubble(*c.cfg.Bootstrap.BootstrapNodeURL, c.cfg.API.AuthenticationKey)
+	hubbleClient := client.NewHubble(*nodeURL, c.cfg.API.AuthenticationKey)
 	return c.migrateCommanderData(hubbleClient)
 }
 
 func (c *Commander) migrateCommanderData(hubble client.Hubble) error {
-	err := c.syncPendingTxs(hubble)
+	pendingTxsCount, err := c.syncPendingTxs(hubble)
 	if err != nil {
 		return err
 	}
+	log.Printf("Synced %d pending transaction(s)\n", pendingTxsCount)
 
-	err = c.syncFailedTxs(hubble)
+	failedTxsCount, err := c.syncFailedTxs(hubble)
 	if err != nil {
 		return err
 	}
+	log.Printf("Synced %d failed transaction(s)\n", failedTxsCount)
 
-	err = c.syncPendingBatches(hubble)
+	pendingBatchesCount, err := c.syncPendingBatches(hubble)
 	if err != nil {
 		return err
 	}
+	log.Printf("Synced %d pending batch(es)\n", pendingBatchesCount)
 
 	c.setMigrate(false)
 	return nil
 }
 
-func (c *Commander) syncPendingTxs(hubble client.Hubble) error {
+func (c *Commander) syncPendingTxs(hubble client.Hubble) (int, error) {
 	txs, err := hubble.GetPendingTransactions()
 	if err != nil {
-		return err
+		return 0, err
 	}
 
-	return c.storage.AddPendingTransactions(txs)
-}
-
-func (c *Commander) syncFailedTxs(hubble client.Hubble) error {
-	failedTxs, err := hubble.GetFailedTransactions()
+	err = c.storage.AddPendingTransactions(txs)
 	if err != nil {
-		return err
+		return 0, err
 	}
 
-	return c.storage.AddFailedTransactions(failedTxs)
+	return txs.Len(), nil
 }
 
-func (c *Commander) syncPendingBatches(hubble client.Hubble) error {
+func (c *Commander) syncFailedTxs(hubble client.Hubble) (int, error) {
+	txs, err := hubble.GetFailedTransactions()
+	if err != nil {
+		return 0, err
+	}
+
+	err = c.storage.AddFailedTransactions(txs)
+	if err != nil {
+		return 0, err
+	}
+
+	return txs.Len(), nil
+}
+
+func (c *Commander) syncPendingBatches(hubble client.Hubble) (int, error) {
 	pendingBatches, err := hubble.GetPendingBatches()
 	if err != nil {
-		return err
+		return 0, err
 	}
 
 	for i := range pendingBatches {
 		err = c.syncPendingBatch(dtoToModelsBatch(&pendingBatches[i]))
 		if err != nil {
-			return err
+			return 0, err
 		}
 	}
 
-	return nil
+	return len(pendingBatches), nil
 }
 
 func (c *Commander) syncPendingBatch(batch *models.PendingBatch) (err error) {
