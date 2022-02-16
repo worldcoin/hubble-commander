4fd03942f38592cb558dadeac7ecf481e45a325f
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-02-16 17:09:26 +0100

    Add TestTxPool

---
+++ mempool/test_tx_pool.go
@@ -0,0 +1,23 @@
+package mempool
+
+import (
+	"context"
+
+	"github.com/Worldcoin/hubble-commander/models"
+)
+
+type testTxPool struct{}
+
+func NewTestTxPool() TxPool {
+	return &testTxPool{}
+}
+
+func (p *testTxPool) Send(models.GenericTransaction) {}
+
+func (p *testTxPool) ReadTxs(context.Context) error {
+	return nil
+}
+
+func (p *testTxPool) Mempool() *Mempool {
+	return nil
+}
