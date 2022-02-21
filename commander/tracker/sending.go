55f5a41af42e31f8c83ad2f356572f8677998119
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-02-21 17:35:39 +0100

    Remove commented code

--- commander/tracker/sending.go
+++ commander/tracker/sending.go
@@ -12,8 +12,6 @@ func (t *Tracker) SendRequestedTxs(ctx context.Context) error {
 		case request := <-t.requestsChan:
 			err := request.Send()
 			if err != nil {
-				// nolint:gocritic
-				// close(t.requestsChan)
 				return err
 			}
 		}
