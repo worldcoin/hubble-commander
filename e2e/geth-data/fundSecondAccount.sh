66747bcd5a805fa460dafc20a37b9d95a5e1da4b
Michał Sieczkowski <michal@ethworks.io>
2022-02-24 16:41:02 +0100

    Increase amount funded

--- e2e/geth-data/fundSecondAccount.sh
+++ e2e/geth-data/fundSecondAccount.sh
@@ -2,7 +2,7 @@
 
 for i in {1..120} ; do
     echo "Trying to fund second account (attempt #$i)... "
-    response=$(geth attach http://localhost:8545 --exec "eth.sendTransaction({from: eth.accounts[0], to: eth.accounts[1], value: "100000000000000000000"})")
+    response=$(geth attach http://localhost:8545 --exec "eth.sendTransaction({from: eth.accounts[0], to: eth.accounts[1], value: 10e36})")
     res=$(echo "$response" | cut -c1-5)
     if [ "$res" != "Fatal" ]; then
         break
