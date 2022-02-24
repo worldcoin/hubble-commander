00b8a445c2934d37a18ea2f65d0b0850aa3d095d
Bartlomiej Tarczynski <bartlomiej.tarczynski1@gmail.com>
2022-02-24 14:43:56 +0100

    Fund second account in start-geth-locally

---
+++ utils/fundAccount.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+for i in {1..120} ; do
+    echo "Trying to fund second account (attempt #$i)... "
+    response=$(geth attach http://localhost:8545 --exec "eth.sendTransaction({from: eth.accounts[0], to: eth.accounts[1], value: "100000000000000000000"})")
+    res=$(echo "$response" | cut -c1-5)
+    if [ "$res" != "Fatal" ]; then
+        break
+    fi
+    sleep 0.5
+done
+
+echo "Second account funded!"
