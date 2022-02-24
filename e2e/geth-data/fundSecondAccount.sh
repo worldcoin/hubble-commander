8b5774c91508b775c0067397f6d9711d70415521
Michał Sieczkowski <michal@ethworks.io>
2022-02-24 18:34:37 +0100

    Adjust fundSecondAccount.sh script for use with dockerized geth

--- e2e/geth-data/fundSecondAccount.sh
+++ e2e/geth-data/fundSecondAccount.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
+if [ "$1" = "docker" ]; then
+    docker="docker exec hubble-geth"
+fi
 
-for i in {1..120} ; do
+for i in {1..120}; do
     echo "Trying to fund second account (attempt #$i)... "
-    response=$(geth attach http://localhost:8545 --exec "eth.sendTransaction({from: eth.accounts[0], to: eth.accounts[1], value: 10e36})")
-    res=$(echo "$response" | cut -c1-5)
-    if [ "$res" != "Fatal" ]; then
+    response=$($docker geth attach http://localhost:8545 --exec "eth.sendTransaction({from: eth.accounts[0], to: eth.accounts[1], value: 10e36})")
+    res=$(echo "$response" | cut -c1-3)
+    if [ "$res" = "\"0x" ]; then
         break
     fi
     sleep 0.5
