b03d3766cfcdaab4cc4115929a0f0ba681dd784f
Chris Kowalski <chrisk@worldcoin.org>
2022-07-05 18:02:09 -0700

    fix quote

--- e2e/geth-data/fundSecondAccount.sh
+++ e2e/geth-data/fundSecondAccount.sh
@@ -6,7 +6,7 @@ fi
 i=1
 while [ "$i" -le 120 ]; do
     echo "Trying to fund second account (attempt #$i)... "
-    response=$($docker geth attach --exec "eth.sendTransaction({from: eth.accounts[0], to: eth.accounts[1], value: 10e36}) http://localhost:8545")
+    response=$($docker geth attach --exec "eth.sendTransaction({from: eth.accounts[0], to: eth.accounts[1], value: 10e36})" http://localhost:8545)
     res=$(echo "$response" | cut -c1-3)
     if [ "$res" = "\"0x" ]; then
         break
