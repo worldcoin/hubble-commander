0ae12d6b1529aebbf05bbe6a5a49bb0c5b13416c
Chris Kowalski <chrisk@worldcoin.org>
2022-08-01 19:28:56 -0700

    more logging

--- commander/executor/create_commitments.go
+++ commander/executor/create_commitments.go
@@ -159,10 +159,13 @@ func (c *TxsContext) executeTxsForCommitment(batchMempool *mempool.TxMempool, fe
 	result ExecuteTxsForCommitmentResult,
 	err error,
 ) {
+
+	count := c.Mempool.TxCount(txtype.TransactionType(c.BatchType))
 	if c.Mempool.TxCount(txtype.TransactionType(c.BatchType)) < int(c.minTxsPerCommitment) {
-		log.Info("XXX:not enough tx point 1")
+		log.Infof("XXX:not enough tx point 1, count: %d", count)
 		return nil, errors.WithStack(ErrNotEnoughTxs)
 	}
+	log.Infof("XXX: transaction count: %d", c.Mempool.TxCount(txtype.TransactionType(c.BatchType)))
 
 	txController, commitmentMempool := batchMempool.BeginTransaction()
 	defer txController.Rollback()
